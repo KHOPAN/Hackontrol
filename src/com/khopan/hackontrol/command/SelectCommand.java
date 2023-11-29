405d546b3507130d8758e87336f0dcaf6c38d7d1
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-11-29 21:40:14 +0700

    Extract Common Code

--- src/com/khopan/hackontrol/command/SelectCommand.java
+++ src/com/khopan/hackontrol/command/SelectCommand.java
@@ -5,59 +5,60 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 
 public class SelectCommand implements Command {
 	@Override
 	public void register(CommandDispatcher<CommandSource> dispatcher) {
 		dispatcher.register(LiteralArgumentBuilder.<CommandSource>literal("select").then(RequiredArgumentBuilder.<CommandSource, String>argument("machineId", StringArgumentType.word()).executes(context -> {
-			CommandSource source = context.getSource();
-			String machineIdArgument = StringArgumentType.getString(context, "machineId");
-			String machineId = source.getMachineId();
-
-			if(machineId.equals(machineIdArgument)) {
-				source.setSelected(true);
-			}
-
+			this.selectSingle(context, false);
 			return 1;
 		})).then(LiteralArgumentBuilder.<CommandSource>literal("all").executes(context -> {
 			CommandSource source = context.getSource();
 			source.setSelected(true);
 			return 1;
 		})).then(LiteralArgumentBuilder.<CommandSource>literal("multiple").then(RequiredArgumentBuilder.<CommandSource, String>argument("machineIds", StringArgumentType.greedyString()).executes(context -> {
-			CommandSource source = context.getSource();
-			String machineIds = StringArgumentType.getString(context, "machineIds");
-			String machineId = source.getMachineId();
-			String[] machineIdList = machineIds.split("\\s+");
-
-			for(int i = 0; i < machineIdList.length; i++) {
-				if(machineId.equals(machineIdList[i])) {
-					source.setSelected(true);
-					break;
-				}
-			}
-
+			this.selectMultiple(context, false);
 			return 1;
 		}))).then(LiteralArgumentBuilder.<CommandSource>literal("only").then(RequiredArgumentBuilder.<CommandSource, String>argument("machineId", StringArgumentType.word()).executes(context -> {
-			CommandSource source = context.getSource();
-			String machineIdArgument = StringArgumentType.getString(context, "machineId");
-			String machineId = source.getMachineId();
-			source.setSelected(machineId.equals(machineIdArgument));
+			this.selectSingle(context, true);
 			return 1;
 		})).then(LiteralArgumentBuilder.<CommandSource>literal("multiple").then(RequiredArgumentBuilder.<CommandSource, String>argument("machineIds", StringArgumentType.greedyString()).executes(context -> {
-			CommandSource source = context.getSource();
-			String machineIds = StringArgumentType.getString(context, "machineIds");
-			String machineId = source.getMachineId();
-			String[] machineIdList = machineIds.split("\\s+");
-
-			for(int i = 0; i < machineIdList.length; i++) {
-				if(machineId.equals(machineIdList[i])) {
-					source.setSelected(true);
-					return 1;
-				}
-			}
-
-			source.setSelected(false);
+			this.selectMultiple(context, true);
 			return 1;
 		})))));
+	}
+
+	private void selectSingle(CommandContext<CommandSource> context, boolean only) {
+		CommandSource source = context.getSource();
+		String machineIdArgument = StringArgumentType.getString(context, "machineId");
+		String machineId = source.getMachineId();
+
+		if(machineId.equals(machineIdArgument)) {
+			source.setSelected(true);
+			return;
+		}
+
+		if(only) {
+			source.setSelected(false);
+		}
+	}
+
+	private void selectMultiple(CommandContext<CommandSource> context, boolean only) {
+		CommandSource source = context.getSource();
+		String machineIds = StringArgumentType.getString(context, "machineIds");
+		String machineId = source.getMachineId();
+		String[] machineIdList = machineIds.split("\\s+");
+
+		for(int i = 0; i < machineIdList.length; i++) {
+			if(machineId.equals(machineIdList[i])) {
+				source.setSelected(true);
+				return;
+			}
+		}
+
+		if(only) {
+			source.setSelected(false);
+		}
 	}
 }
