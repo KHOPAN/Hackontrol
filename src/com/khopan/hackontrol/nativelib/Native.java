955d546c2580f0f76c1ad73177c2d2417656966b
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-11-29 22:38:01 +0700

    Add Native Loader

---
+++ src/com/khopan/hackontrol/nativelib/Native.java
@@ -0,0 +1,11 @@
+package com.khopan.hackontrol.nativelib;
+
+public class Native {
+	static {
+		System.load("D:\\GitHub Repository\\Hackontrol\\Native Library\\x64\\Debug\\Native Library.dll");
+	}
+
+	public static void load() {
+		// Load the class
+	}
+}
