5278daec33b12948a349aba004d630057d09fcfb
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-01 20:25:19 +0700

    Add Installer Native Loader

---
+++ Installer/src/com/khopan/hackontrol/installer/nativelib/Native.java
@@ -0,0 +1,11 @@
+package com.khopan.hackontrol.installer.nativelib;
+
+public class Native {
+	static {
+		System.load("D:\\GitHub Repository\\Hackontrol\\Installer\\Native Library\\x64\\Debug\\Native Library.dll");
+	}
+
+	public static void load() {
+		// Load the class
+	}
+}
