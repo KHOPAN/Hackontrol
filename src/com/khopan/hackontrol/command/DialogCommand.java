8c2496889e775c9bd370d2efdafd15239e5a2642
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-06 21:16:48 +0700

    Add Dialog Command Native

--- src/com/khopan/hackontrol/command/DialogCommand.java
+++ src/com/khopan/hackontrol/command/DialogCommand.java
@@ -1,7 +1,6 @@
 package com.khopan.hackontrol.command;
 
 import com.khopan.hackontrol.source.CommandSource;
-import com.khopan.win32.Win32;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
@@ -12,32 +11,36 @@ public class DialogCommand implements Command {
 	@Override
 	public void register(CommandDispatcher<CommandSource> dispatcher) {
 		dispatcher.register(LiteralArgumentBuilder.<CommandSource>literal("dialog").requires(source -> source.isSelected()).then(LiteralArgumentBuilder.<CommandSource>literal("error").then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.string()).executes(context -> {
-			return this.message(context, Win32.MB_ICONERROR);
+			return this.message(context, 0x1010);
 		})).then(RequiredArgumentBuilder.<CommandSource, String>argument("title", StringArgumentType.string()).then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.string()).executes(context -> {
-			return this.title(context, Win32.MB_ICONERROR);
+			return this.title(context, 0x1010);
 		})))).then(LiteralArgumentBuilder.<CommandSource>literal("question").then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.string()).executes(context -> {
-			return this.message(context, Win32.MB_ICONQUESTION);
+			return this.message(context, 0x1020);
 		})).then(RequiredArgumentBuilder.<CommandSource, String>argument("title", StringArgumentType.string()).then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.string()).executes(context -> {
-			return this.title(context, Win32.MB_ICONQUESTION);
+			return this.title(context, 0x1020);
 		})))).then(LiteralArgumentBuilder.<CommandSource>literal("warning").then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.string()).executes(context -> {
-			return this.message(context, Win32.MB_ICONWARNING);
+			return this.message(context, 0x1030);
 		})).then(RequiredArgumentBuilder.<CommandSource, String>argument("title", StringArgumentType.string()).then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.string()).executes(context -> {
-			return this.title(context, Win32.MB_ICONWARNING);
+			return this.title(context, 0x1030);
 		})))).then(LiteralArgumentBuilder.<CommandSource>literal("information").then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.string()).executes(context -> {
-			return this.message(context, Win32.MB_ICONINFORMATION);
+			return this.message(context, 0x1040);
 		})).then(RequiredArgumentBuilder.<CommandSource, String>argument("title", StringArgumentType.string()).then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.string()).executes(context -> {
-			return this.title(context, Win32.MB_ICONINFORMATION);
+			return this.title(context, 0x1040);
 		})))));
 	}
 
-	private int message(CommandContext<CommandSource> context, long type) {
+	private int message(CommandContext<CommandSource> context, int type) {
 		String message = StringArgumentType.getString(context, "message");
-		return Win32.MessageBox(null, message, "", type | Win32.MB_SYSTEMMODAL);
+		DialogCommand.messageBox(message, null, type);
+		return 1;
 	}
 
-	private int title(CommandContext<CommandSource> context, long type) {
+	private int title(CommandContext<CommandSource> context, int type) {
 		String title = StringArgumentType.getString(context, "title");
 		String message = StringArgumentType.getString(context, "message");
-		return Win32.MessageBox(null, message, title, type | Win32.MB_SYSTEMMODAL);
+		DialogCommand.messageBox(message, title, type);
+		return 1;
 	}
+
+	public static native void messageBox(String message, String title, int type);
 }
