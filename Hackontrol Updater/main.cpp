be670cb75ba7b10ca6951b3a4852bcf8dc6848b5
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-09 18:21:18 +0700

    Add RapidJSON

--- Hackontrol Updater/main.cpp
+++ Hackontrol Updater/main.cpp
@@ -1,9 +1,15 @@
+#include <rapidjson/document.h>
+#include <rapidjson/reader.h>
+#include <string>
 #include <Windows.h>
 #include "HackontrolUpdater.h"
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR argument, int command) {
 	CURL* curl = HU_InitializeCURL();
 	const char* versionFile = HU_GetVersionFile(curl);
-	MessageBoxA(NULL, versionFile, NULL, MB_OK);
+	rapidjson::Document document;
+	document.Parse(versionFile);
+	rapidjson::Value& test = document["hash"];
+	MessageBoxA(NULL, test.GetString(), NULL, MB_OK);
 	return 0;
 }
