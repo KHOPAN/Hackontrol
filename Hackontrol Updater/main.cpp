15ef3314a8f4d98dbb77f422063010a4b16fea07
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-09 15:55:41 +0700

    Setup cURL

--- Hackontrol Updater/main.cpp
+++ Hackontrol Updater/main.cpp
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <Windows.h>
 
-int main(int argc, char** argv) {
-	printf("Hello, world!\n");
+#define CURL_STATICLIB
+#include <curl/curl.h>
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR argument, int command) {
+	curl_global_init(CURL_GLOBAL_ALL);
+	CURL* curl = curl_easy_init();
+	curl_easy_cleanup(curl);
+	curl_global_cleanup();
 	return 0;
 }
