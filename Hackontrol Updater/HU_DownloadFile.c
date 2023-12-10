f2e0fee8ef0ff854a61a214ff9530b62f3292a68
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-10 11:53:22 +0700

    Add HU_DownloadFile

---
+++ Hackontrol Updater/HU_DownloadFile.c
@@ -0,0 +1,36 @@
+#include "HackontrolUpdater.h"
+
+void HU_DownloadFile(CURL* curl, const char* url, const wchar_t* filePath) {
+	CURLcode code = curl_easy_setopt(curl, CURLOPT_URL, url);
+
+	if(code != CURLE_OK) {
+		ExitProcess(code);
+		return;
+	}
+
+	FILE* file = NULL;
+	errno_t errorCode = _wfopen_s(&file, filePath, L"wb");
+	
+	if(errorCode != NULL || file == NULL) {
+		ExitProcess(errorCode);
+		return;
+	}
+
+	code = curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+
+	if(code != CURLE_OK) {
+		fclose(file);
+		ExitProcess(code);
+		return;
+	}
+
+	code = curl_easy_perform(curl);
+
+	if(code != CURLE_OK) {
+		fclose(file);
+		ExitProcess(code);
+		return;
+	}
+
+	fclose(file);
+}
--- Hackontrol Updater/HackontrolUpdater.h
+++ Hackontrol Updater/HackontrolUpdater.h
@@ -11,6 +11,7 @@ const char* HU_GetVersionFile(CURL*);
 BOOL HU_IsFileExists(const wchar_t*);
 const char* HU_Hash(BYTE*, size_t);
 const wchar_t* HU_GetSystemDirectory(const wchar_t*);
+void HU_DownloadFile(CURL*, const char*, const wchar_t*);
 #ifdef __cplusplus
 }
 #endif
