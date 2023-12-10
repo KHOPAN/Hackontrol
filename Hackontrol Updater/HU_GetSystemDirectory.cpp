acefe753b091db9346cc66e2188c5b416a065ae5
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-10 12:58:21 +0700

    Add HU_GetSystemDirectory Error Handling

--- Hackontrol Updater/HU_GetSystemDirectory.cpp
+++ Hackontrol Updater/HU_GetSystemDirectory.cpp
@@ -3,8 +3,29 @@
 
 const wchar_t* HU_GetSystemDirectory(const wchar_t* fileName) {
 	INT size = GetWindowsDirectoryW(NULL, 0);
+
+	if(size == NULL) {
+		HU_DisplayError(GetLastError(), L"GetWindowsDirectoryW()");
+		ExitProcess(-1);
+		return NULL;
+	}
+
 	LPWSTR buffer = static_cast<LPWSTR>(malloc(sizeof(wchar_t) * size));
-	GetWindowsDirectoryW(buffer, size);
+
+	if(buffer == NULL) {
+		HU_DisplayError(ERROR_NOT_ENOUGH_MEMORY, L"malloc()");
+		ExitProcess(-2);
+		return NULL;
+	}
+
+	size = GetWindowsDirectoryW(buffer, size);
+
+	if(size == NULL) {
+		HU_DisplayError(GetLastError(), L"GetWindowsDirectoryW()");
+		ExitProcess(-1);
+		return NULL;
+	}
+
 	std::wstring string(buffer);
 	free(buffer);
 
