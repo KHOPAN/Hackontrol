3667faa170b8a8511a5bb246ffffc6f1626fbd5a
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-12 23:12:10 +0700

    Remove Old Installer

--- Installer/src/com/khopan/hackontrol/installer/nativelib/Administrator.java
+++
@@ -1,9 +0,0 @@
-package com.khopan.hackontrol.installer.nativelib;
-
-public class Administrator {
-	static {
-		Native.load();
-	}
-
-	public static native boolean hasAdministratorPrivileges();
-}
--- Installer/src/com/khopan/hackontrol/installer/nativelib/TaskScheduler.java
+++
@@ -1,9 +0,0 @@
-package com.khopan.hackontrol.installer.nativelib;
-
-public class TaskScheduler {
-	static {
-		Native.load();
-	}
-
-	public static native boolean registerTask(String taskName, String executable);
-}
