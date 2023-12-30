c4fe5fe7b67a1edaacf2998b7f4be3976eb3cc8b
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-30 19:32:29 +0700

    Delete Hackontrol Updater

--- Hackontrol Updater/HU_IsFileExists.c
+++
@@ -1,7 +0,0 @@
-#include <Windows.h>
-#include "HackontrolUpdater.h"
-
-BOOL HU_IsFileExists(const wchar_t* fileName) {
-	DWORD attributes = GetFileAttributesW(fileName);
-	return attributes != INVALID_FILE_ATTRIBUTES && !(attributes & FILE_ATTRIBUTE_DIRECTORY);
-}
