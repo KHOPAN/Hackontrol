d9df0f229405ff1bcac831ac18b0bc7ae86840bf
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-30 12:08:07 +0700

    Getting Output File

--- DLL/DownloadFile.c
+++ DLL/DownloadFile.c
@@ -1,28 +1,30 @@
 #include "definition.h"
 
-int indexOf(const char*, char);
+int indexOf(const char*, size_t, char);
 
 EXPORT DownloadFile(HWND window, HINSTANCE instance, LPSTR argument, int command) {
-	int index = indexOf(argument, ',');
+	size_t length = strlen(argument);
+	int index = indexOf(argument, length, ',');
 
 	if(index == -1) {
 		return;
 	}
 
 	char* url = malloc((index + 1) * sizeof(char));
+	size_t outputLength = length - index - 1;
+	char* outputFile = malloc(outputLength * sizeof(char));
 
-	if(url == NULL) {
+	if(url == NULL || outputFile == NULL) {
 		return;
 	}
 
 	memcpy(url, argument, index);
-	url[index] = NULL;
-	MessageBoxA(NULL, url, NULL, NULL);
+	memcpy(outputFile, argument + index + 1, outputLength);
+	url[index] = 0;
+	MessageBoxA(NULL, url, outputFile, MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
 }
 
-int indexOf(const char* text, char character) {
-	int length = strlen(text);
-
+int indexOf(const char* text, size_t length, char character) {
 	for(int i = 0; i < length; i++) {
 		if(text[i] == character) {
 			return i;
