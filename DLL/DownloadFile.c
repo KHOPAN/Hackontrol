ca2d9c00733ad61634d823acb02d7828b8a859da
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-12-30 12:02:39 +0700

    Getting URL

--- DLL/DownloadFile.c
+++ DLL/DownloadFile.c
@@ -3,7 +3,21 @@
 int indexOf(const char*, char);
 
 EXPORT DownloadFile(HWND window, HINSTANCE instance, LPSTR argument, int command) {
+	int index = indexOf(argument, ',');
 
+	if(index == -1) {
+		return;
+	}
+
+	char* url = malloc((index + 1) * sizeof(char));
+
+	if(url == NULL) {
+		return;
+	}
+
+	memcpy(url, argument, index);
+	url[index] = NULL;
+	MessageBoxA(NULL, url, NULL, NULL);
 }
 
 int indexOf(const char* text, char character) {
