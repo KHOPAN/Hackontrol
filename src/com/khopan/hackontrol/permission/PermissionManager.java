f4d8b5f43a21fd4426fbae4ecc2a0745aa190917
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-01-22 22:06:25 +0700

    Add Permission Manager

---
+++ src/com/khopan/hackontrol/permission/PermissionManager.java
@@ -0,0 +1,11 @@
+package com.khopan.hackontrol.permission;
+
+import net.dv8tion.jda.api.entities.User;
+
+public class PermissionManager {
+	private PermissionManager() {}
+
+	public static boolean checkPermission(User user) {
+		return false;
+	}
+}
