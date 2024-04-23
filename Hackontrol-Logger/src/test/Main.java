857b611a497f83c0144b13b01b4147876dd6f805
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-04-23 14:00:40 +0700

    Create Hackontrol Logger

---
+++ Hackontrol-Logger/src/test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Main");
+
+	public static void main(String[] args) {
+		Main.LOGGER.info("Hello, world!");
+	}
+}
