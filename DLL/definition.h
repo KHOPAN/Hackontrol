10071645d6cc0ae043732b6f702c2337f98695ab
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-04-24 23:14:33 +0700

    Remove Inlined Code

--- DLL/definition.h
+++ DLL/definition.h
@@ -19,10 +19,6 @@ const char* HU_Hash(BYTE*, size_t);
 CURL* HU_InitializeCURL();
 BOOL HU_IsFileExists(const wchar_t*);
 
-void HI_InitializeComAPI();
-ITaskService* HI_CreateTaskService();
-ITaskFolder* HI_CreateFolder(ITaskService*, const wchar_t*);
-ITaskDefinition* HI_NewTask(ITaskService*, ITaskFolder*);
 void HI_SetPrincipal(ITaskFolder*, ITaskDefinition*);
 void HI_SetTriggers(ITaskFolder*, ITaskDefinition*);
 void HI_SetActions(ITaskFolder*, ITaskDefinition*, const wchar_t*, const wchar_t*);
