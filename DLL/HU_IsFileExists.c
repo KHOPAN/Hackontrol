66a718d9d11eda4ca9cfa9a80bc35667fa7ed9a9
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-04-25 17:55:31 +0700

    Remove Unused Functions

--- DLL/HU_IsFileExists.c
+++
@@ -1,7 +0,0 @@
-#include <Windows.h>
-#include "definition.h"
-
-BOOL HU_IsFileExists(const wchar_t* fileName) {
-	DWORD attributes = GetFileAttributesW(fileName);
-	return attributes != INVALID_FILE_ATTRIBUTES && !(attributes & FILE_ATTRIBUTE_DIRECTORY);
-}
--- DLL/definition.h
+++ DLL/definition.h
@@ -8,7 +8,4 @@ EXPORT(DownloadFile); // DONE
 EXPORT(Install); // DONE
 EXPORT(Execute); // IN PROGRESS
 
-BOOL downloadFileInternal(CURL*, const char*, const void*, BOOL);
-
 char* hashSHA512(BYTE*, size_t);
-BOOL HU_IsFileExists(const wchar_t*);
