1528087de933da30e31324f51f96e1f571d8565c
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-04-25 15:53:03 +0700

    Change Compile As To C

--- DLL/definition.h
+++ DLL/definition.h
@@ -4,17 +4,11 @@
 #include <taskschd.h>
 #include "exportMacro.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 EXPORT(DownloadFile); // DONE
 EXPORT(Install); // DONE
-EXPORT(Execute);
+EXPORT(Execute); // IN PROGRESS
 
 BOOL downloadFileInternal(CURL*, const char*, const void*, BOOL);
 
 const char* HU_Hash(BYTE*, size_t);
 BOOL HU_IsFileExists(const wchar_t*);
-#ifdef __cplusplus
-}
-#endif
--- DLL/errorLog.h
+++ DLL/errorLog.h
@@ -3,12 +3,6 @@
 #include <Windows.h>
 #include "curlLib.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 void curlError(CURLcode, const wchar_t*);
 void dialogError(DWORD, const wchar_t*);
 void HU_DisplayError(DWORD, const wchar_t*);
-#ifdef __cplusplus
-}
-#endif
