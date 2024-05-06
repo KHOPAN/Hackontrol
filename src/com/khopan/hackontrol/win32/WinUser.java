3bef1315770d3d192d539efe395eddd48769811c
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-05-06 14:56:00 +0700

    Remove Unnecessary Flags

--- src/com/khopan/hackontrol/win32/WinUser.java
+++ src/com/khopan/hackontrol/win32/WinUser.java
@@ -1,43 +1,22 @@
 package com.khopan.hackontrol.win32;
 
 public interface WinUser {
-	public static final int MB_OK                        = 0x00000000;
-	public static final int MB_OKCANCEL                  = 0x00000001;
-	public static final int MB_ABORTRETRYIGNORE          = 0x00000002;
-	public static final int MB_YESNOCANCEL               = 0x00000003;
-	public static final int MB_YESNO                     = 0x00000004;
-	public static final int MB_RETRYCANCEL               = 0x00000005;
-	public static final int MB_CANCELTRYCONTINUE         = 0x00000006;
-	public static final int MB_ICONHAND                  = 0x00000010;
-	public static final int MB_ICONQUESTION              = 0x00000020;
-	public static final int MB_ICONEXCLAMATION           = 0x00000030;
-	public static final int MB_ICONASTERISK              = 0x00000040;
-	public static final int MB_USERICON                  = 0x00000080;
-	public static final int MB_ICONWARNING               = 0x00000030;
-	public static final int MB_ICONERROR                 = 0x00000010;
-	public static final int MB_ICONINFORMATION           = 0x00000040;
-	public static final int MB_ICONSTOP                  = 0x00000010;
-	public static final int MB_DEFBUTTON1                = 0x00000000;
-	public static final int MB_DEFBUTTON2                = 0x00000100;
-	public static final int MB_DEFBUTTON3                = 0x00000200;
-	public static final int MB_DEFBUTTON4                = 0x00000300;
-	public static final int MB_APPLMODAL                 = 0x00000000;
-	public static final int MB_SYSTEMMODAL               = 0x00001000;
-	public static final int MB_TASKMODAL                 = 0x00002000;
-	public static final int MB_HELP                      = 0x00004000;
-	public static final int MB_NOFOCUS                   = 0x00008000;
-	public static final int MB_SETFOREGROUND             = 0x00010000;
-	public static final int MB_DEFAULT_DESKTOP_ONLY      = 0x00020000;
-	public static final int MB_TOPMOST                   = 0x00040000;
-	public static final int MB_RIGHT                     = 0x00080000;
-	public static final int MB_RTLREADING                = 0x00100000;
-	public static final int MB_SERVICE_NOTIFICATION      = 0x00200000;
-	public static final int MB_SERVICE_NOTIFICATION_NT3X = 0x00040000;
-	public static final int MB_TYPEMASK                  = 0x0000000F;
-	public static final int MB_ICONMASK                  = 0x000000F0;
-	public static final int MB_DEFMASK                   = 0x00000F00;
-	public static final int MB_MODEMASK                  = 0x00003000;
-	public static final int MB_MISCMASK                  = 0x0000C000;
+	public static final int MB_ICONINFORMATION = 0x40;
+	public static final int MB_ICONQUESTION    = 0x20;
+	public static final int MB_ICONWARNING     = 0x30;
+	public static final int MB_ICONERROR       = 0x10;
+
+	public static final int MB_ABORTRETRYIGNORE  = 0x02;
+	public static final int MB_CANCELTRYCONTINUE = 0x06;
+	public static final int MB_OK                = 0x00;
+	public static final int MB_OKCANCEL          = 0x01;
+	public static final int MB_RETRYCANCEL       = 0x05;
+	public static final int MB_YESNO             = 0x04;
+	public static final int MB_YESNOCANCEL       = 0x03;
+
+	public static final int MB_DEFBUTTON1  = 0x0000;
+	public static final int MB_SYSTEMMODAL = 0x1000;
+	public static final int MB_TASKMODAL   = 0x2000;
 
 	public static final int IDOK       = 0x01;
 	public static final int IDCANCEL   = 0x02;
