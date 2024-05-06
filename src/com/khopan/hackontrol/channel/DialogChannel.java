15974f1f21f5de864d041aa011cc12479263d243
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-05-06 14:32:53 +0700

    Add Icon/OptionType Enum

--- src/com/khopan/hackontrol/channel/DialogChannel.java
+++ src/com/khopan/hackontrol/channel/DialogChannel.java
@@ -1,15 +1,18 @@
 package com.khopan.hackontrol.channel;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 import com.khopan.hackontrol.HackontrolChannel;
-import com.khopan.hackontrol.manager.interaction.ButtonContext;
 import com.khopan.hackontrol.manager.interaction.ButtonManager;
 import com.khopan.hackontrol.manager.interaction.ButtonManager.ButtonType;
 import com.khopan.hackontrol.manager.interaction.InteractionManager;
+import com.khopan.hackontrol.manager.interaction.ModalContext;
 import com.khopan.hackontrol.manager.interaction.ModalManager;
+import com.khopan.hackontrol.manager.interaction.StringSelectContext;
 import com.khopan.hackontrol.manager.interaction.StringSelectManager;
 import com.khopan.hackontrol.registry.Registry;
 import com.khopan.hackontrol.utils.interaction.HackontrolButton;
-import com.khopan.hackontrol.utils.interaction.HackontrolStringSelectMenu;
 
 import net.dv8tion.jda.api.interactions.callbacks.IModalCallback;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
@@ -30,7 +33,7 @@ public class DialogChannel extends HackontrolChannel {
 
 	@Override
 	public void preInitialize(Registry registry) {
-		registry.register(InteractionManager.BUTTON_REGISTRY, DialogChannel.BUTTON_NEW_DIALOG, this :: buttonNewDialog);
+		registry.register(InteractionManager.BUTTON_REGISTRY, DialogChannel.BUTTON_NEW_DIALOG, context -> this.replyDialogModal(context, false, null));
 	}
 
 	@Override
@@ -38,46 +41,73 @@ public class DialogChannel extends HackontrolChannel {
 		this.channel.sendMessageComponents(ActionRow.of(DialogChannel.BUTTON_NEW_DIALOG)).queue();
 	}
 
-	private void buttonNewDialog(ButtonContext context) {
-		this.replNewEditDialogModal(context, false, null, null);
-	}
+	private void replyDialogModal(IModalCallback callback, boolean edit, DialogInstance instance) {
+		TextInput.Builder titleInput   = TextInput.create("title",   "Title",   TextInputStyle.SHORT).setMinLength(-1).setMaxLength(-1).setRequired(false).setPlaceholder("Error");
+		TextInput.Builder messageInput = TextInput.create("message", "Message", TextInputStyle.PARAGRAPH).setMinLength(-1).setMaxLength(-1).setRequired(false).setPlaceholder("Type a message...");
 
-	private void replNewEditDialogModal(IModalCallback callback, boolean edit, String title, String messageContent) {
-		TextInput titleInput = TextInput.create("title", "Title", TextInputStyle.SHORT)
-				.setMinLength(-1)
-				.setMaxLength(-1)
-				.setRequired(false)
-				.setPlaceholder("Error")
-				.setValue(title)
-				.build();
+		if(instance != null) {
+			if(instance.title != null && !instance.title.isBlank()) {
+				titleInput.setValue(instance.title);
+			}
 
-		TextInput messageInput = TextInput.create("message", "Message", TextInputStyle.PARAGRAPH)
-				.setMinLength(-1)
-				.setMaxLength(-1)
-				.setRequired(false)
-				.setPlaceholder("Type a message...")
-				.setValue(messageContent)
-				.build();
+			if(instance.message != null && !instance.message.isBlank()) {
+				messageInput.setValue(instance.message);
+			}
+		}
 
 		callback.replyModal(ModalManager.dynamicModal(edit ? "Edit Dialog" : "New Dialog", context -> {
-			DialogInstance instance = new DialogInstance();
+			if(!edit) {
+				this.modalNewDialog(context);
+				return;
+			}
+
 			instance.title = context.getValue("title").getAsString();
 			instance.message = context.getValue("message").getAsString();
-			StringSelectMenu menu = StringSelectManager.dynamicMenu(HackontrolStringSelectMenu :: saveDefault)
-					.addOption("Icon Information", "iconInformation", "An icon consisting of a lowercase letter i in a circle appears in the message box.")
-					.addOption("Icon Question", "iconQuestion", "A question-mark icon appears in the message box.")
-					.addOption("Icon Warning", "iconWarning", "An exclamation-point icon appears in the message box.")
-					.addOption("Icon Error", "iconError", "A stop-sign icon appears in the message box.")
-					.setMaxValues(1)
-					.build();
-
-			context.reply(instance.toString()).addActionRow(menu).addActionRow(HackontrolButton.delete()).queue(InteractionManager :: callback);
-		}).addActionRow(titleInput).addActionRow(messageInput).build()).queue();
+			context.editMessage(instance.toString()).queue();
+		}).addActionRow(titleInput.build()).addActionRow(messageInput.build()).build()).queue();
 	}
 
-	private class DialogInstance {
+	private void modalNewDialog(ModalContext context) {
+		DialogInstance instance = new DialogInstance();
+		instance.title = context.getValue("title").getAsString();
+		instance.message = context.getValue("message").getAsString();
+		StringSelectMenu iconMenu = StringSelectManager.dynamicMenu(Event -> this.storeDefault(Event, DialogIcon.class, icon -> instance.icon = icon))
+				.addOption("Icon Information", DialogIcon.ICON_INFORMATION.name(), "An icon consisting of a lowercase letter i in a circle appears in the message box.")
+				.addOption("Icon Question",    DialogIcon.ICON_QUESTION.name(),    "A question-mark icon appears in the message box.")
+				.addOption("Icon Warning",     DialogIcon.ICON_WARNING.name(),     "An exclamation-point icon appears in the message box.")
+				.addOption("Icon Error",       DialogIcon.ICON_ERROR.name(),       "A stop-sign icon appears in the message box.")
+				.setMaxValues(1)
+				.build();
+
+		StringSelectMenu optionMenu = StringSelectManager.dynamicMenu(Event -> this.storeDefault(Event, DialogOptionType.class, optionType -> instance.optionType = optionType))
+				.addOption("Abort, Retry, Ignore",        DialogOptionType.OPTION_ABORT_RETRY_IGNORE.name(),       "The message box contains three push buttons: Abort, Retry, and Ignore.")
+				.addOption("Cancel, Try Again, Continue", DialogOptionType.OPTION_CANCEL_TRYAGAIN_CONTINUE.name(), "The message box contains three push buttons: Cancel, Try Again, Continue.")
+				.addOption("Ok",                          DialogOptionType.OPTION_OK.name(),                       "The message box contains one push button: OK.")
+				.addOption("Ok, Cancel",                  DialogOptionType.OPTION_OK_CANCEL.name(),                "The message box contains two push buttons: OK and Cancel.")
+				.addOption("Retry, Cancel",               DialogOptionType.OPTION_RETRY_CANCEL.name(),             "The message box contains two push buttons: Retry and Cancel.")
+				.addOption("Yes, No",                     DialogOptionType.OPTION_YES_NO.name(),                   "The message box contains two push buttons: Yes and No.")
+				.addOption("Yes, No, Cancel",             DialogOptionType.OPTION_YES_NO_CANCEL.name(),            "The message box contains three push buttons: Yes, No, and Cancel.")
+				.setMaxValues(1)
+				.build();
+
+		context.reply(instance.toString()).addActionRow(iconMenu).addActionRow(optionMenu).addActionRow(ButtonManager.dynamicButton(ButtonType.SUCCESS, "Edit", Event -> this.replyDialogModal(Event, true, instance)), HackontrolButton.delete()).queue(InteractionManager :: callback);
+	}
+
+	private <T extends Enum<T>> void storeDefault(StringSelectContext context, Class<T> enumClass, Consumer<T> consumer) {
+		List<String> values = context.getValues();
+		StringSelectMenu menu = context.getSelectMenu().createCopy().setDefaultValues(values).build();
+		context.editSelectMenu(menu).queue();
+
+		if(!values.isEmpty()) {
+			consumer.accept(Enum.valueOf(enumClass, values.get(0)));
+		}
+	}
+
+	private static class DialogInstance {
 		private String title;
 		private String message;
+		private DialogIcon icon;
+		private DialogOptionType optionType;
 
 		@Override
 		public String toString() {
@@ -95,5 +125,22 @@ public class DialogChannel extends HackontrolChannel {
 
 			return builder.toString().trim();
 		}
+	}
+
+	private static enum DialogIcon {
+		ICON_INFORMATION,
+		ICON_QUESTION,
+		ICON_WARNING,
+		ICON_ERROR;
+	}
+
+	private static enum DialogOptionType {
+		OPTION_ABORT_RETRY_IGNORE,
+		OPTION_CANCEL_TRYAGAIN_CONTINUE,
+		OPTION_OK,
+		OPTION_OK_CANCEL,
+		OPTION_RETRY_CANCEL,
+		OPTION_YES_NO,
+		OPTION_YES_NO_CANCEL;
 	}
 }
