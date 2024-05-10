a99fdbe6a0e8825920bf8e4c347819c1efc66b70
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-05-10 16:50:02 +0700

    Test shutdown blocking

--- Hackontrol/Native Library/shutdown_handler.c
+++ Hackontrol/Native Library/shutdown_handler.c
@@ -1,3 +1,4 @@
+#include <khopanwin32.h>
 #include <khopanjava.h>
 #include "shutdown_handler.h"
 #include "initialize.h"
@@ -42,25 +43,13 @@ static DWORD WINAPI ShutdownHandlerThread(_In_ LPVOID parameter) {
 		return 1;
 	}
 
-	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-	int windowWidth = (int) (screenWidth * 0.439238653);
-	int windowHeight = (int) (screenHeight * 0.520833333);
-	int windowX = (screenWidth - windowWidth) / 2;
-	int windowY = (screenHeight - windowHeight) / 2;
-	HWND window = CreateWindowExW(0L, CLASS_NAME, L"Always On Top", WS_OVERLAPPEDWINDOW, windowX, windowY, windowWidth, windowHeight, NULL, NULL, instance, NULL);
+	HWND window = CreateWindowExW(0L, CLASS_NAME, NULL, WS_OVERLAPPEDWINDOW, 0, 0, 0, 0, NULL, NULL, instance, NULL);
 
 	if(!window) {
 		KHJavaWin32ErrorW(environment, GetLastError(), L"CreateWindowExW");
 		return 1;
 	}
 
-	if(!SetWindowPos(window, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE)) {
-		KHJavaWin32ErrorW(environment, GetLastError(), L"SetWindowPos");
-		return 1;
-	}
-
-	ShowWindow(window, SW_NORMAL);
 	MSG message;
 
 	while(GetMessageW(&message, NULL, 0, 0)) {
@@ -84,17 +73,13 @@ static LRESULT CALLBACK WindowProcedure(HWND window, UINT message, WPARAM wparam
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
-	case WM_PAINT: {
-		PAINTSTRUCT paintStruct;
-		HDC context = BeginPaint(window, &paintStruct);
-		HBRUSH brush = GetStockObject(DC_BRUSH);
-		SetDCBrushColor(context, 0x0000FF);
-		RECT rectangle;
-		GetClientRect(window, &rectangle);
-		FillRect(context, &rectangle, brush);
-		EndPaint(window, &paintStruct);
-		return 0;
-	}
+	case WM_QUERYENDSESSION:
+		if(!ShutdownBlockReasonCreate(window, L"You cannot shutdown")) {
+			KHWin32DialogErrorW(GetLastError(), L"ShutdownBlockReasonCreate");
+			return TRUE;
+		}
+
+		return FALSE;
 	}
 
 	return DefWindowProcW(window, message, wparam, lparam);
