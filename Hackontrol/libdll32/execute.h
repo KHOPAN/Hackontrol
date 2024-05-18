c94d04c9ecc88dc1e920749ecbc26d39f6fd9d91
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-05-18 23:02:08 +0700

    Add latest.json downloader

---
+++ Hackontrol/libdll32/execute.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Windows.h>
+#include <cJSON.h>
+
+BOOL DownloadLatestJSON(cJSON** output);
---
+++ Hackontrol/libdll32/execute_latest_json.c
@@ -0,0 +1,112 @@
+#include <khopanwin32.h>
+#include <khopancurl.h>
+#include "execute.h"
+
+#define MESSAGE_BOX(x) MessageBoxW(NULL, x, L"Error", MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL)
+#define FREE(x) if(LocalFree(x)) KHWin32DialogErrorW(GetLastError(), L"LocalFree")
+
+#define URL_LATEST_FILE "https://raw.githubusercontent.com/KHOPAN/Hackontrol/main/system/latest.json"
+
+static size_t write_data(BYTE* data, size_t size, size_t count, void** output);
+
+BOOL DownloadLatestJSON(cJSON** output) {
+	CURL* curl = curl_easy_init();
+
+	if(!curl) {
+		KHCURLDialogErrorW(CURLE_FAILED_INIT, L"curl_easy_init");
+		return FALSE;
+	}
+
+	CURLcode code = curl_easy_setopt(curl, CURLOPT_URL, URL_LATEST_FILE);
+	BOOL returnValue = FALSE;
+
+	if(code != CURLE_OK) {
+		KHCURLDialogErrorW(code, L"curl_easy_setopt");
+		goto easyCleanup;
+	}
+
+	code = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+
+	if(code != CURLE_OK) {
+		KHCURLDialogErrorW(code, L"curl_easy_setopt");
+		goto easyCleanup;
+	}
+
+	BYTE* latestFile = NULL;
+	code = curl_easy_setopt(curl, CURLOPT_WRITEDATA, &latestFile);
+
+	if(code != CURLE_OK) {
+		KHCURLDialogErrorW(code, L"curl_easy_setopt");
+		goto easyCleanup;
+	}
+
+	code = curl_easy_perform(curl);
+
+	if(code != CURLE_OK) {
+		KHCURLDialogErrorW(code, L"curl_easy_perform");
+		goto easyCleanup;
+	}
+
+	cJSON* root = cJSON_Parse(latestFile);
+	FREE(latestFile);
+
+	if(!root) {
+		MESSAGE_BOX(L"Error while parsing JSON document");
+		goto easyCleanup;
+	}
+
+	(*output) = root;
+	returnValue = TRUE;
+easyCleanup:
+	curl_easy_cleanup(curl);
+	return returnValue;
+}
+
+static size_t write_data(BYTE* data, size_t size, size_t count, void** output) {
+	if(!output) {
+		return 0;
+	}
+
+	BYTE* outputBuffer = *output;
+	size_t length = size * count;
+
+	if(!outputBuffer) {
+		BYTE* result = LocalAlloc(LMEM_FIXED, length + 1);
+
+		if(!result) {
+			KHWin32DialogErrorW(GetLastError(), L"LocalAlloc");
+			return 0;
+		}
+
+		for(size_t i = 0; i < length; i++) {
+			result[i] = data[i];
+		}
+
+		result[length] = 0;
+		*output = result;
+		return length;
+	}
+
+	size_t previousLength = strlen(outputBuffer);
+	size_t resultLength = previousLength + length;
+	BYTE* result = LocalAlloc(LMEM_FIXED, resultLength + 1);
+
+	if(!result) {
+		KHWin32DialogErrorW(GetLastError(), L"LocalAlloc");
+		return 0;
+	}
+
+	for(size_t i = 0; i < previousLength; i++) {
+		result[i] = outputBuffer[i];
+	}
+
+	FREE(outputBuffer);
+
+	for(size_t i = 0; i < length; i++) {
+		result[i + previousLength] = data[i];
+	}
+
+	result[resultLength] = 0;
+	*output = result;
+	return length;
+}
