af07a17510bd38f03b0d82d08f8f0e2d1fd42dff
KHOPAN <puthipnakchan@gmail.com>
2024-05-19 14:13:01 +0700

    Convert functions to macro

--- Hackontrol/libdll32/execute_check_hash.c
+++ Hackontrol/libdll32/execute_check_hash.c
@@ -5,14 +5,9 @@
 #include <openssl/sha.h>
 #include <openssl/md5.h>
 
-#define HASH_CHECK(x,y) do{if(cJSON_HasObjectItem(root,x)){cJSON*object=cJSON_GetObjectItem(root,x);if(!cJSON_IsString(object)){goto freeBuffer;}match=y(object,buffer,fileSize.QuadPart);goto freeBuffer;}}while(0)
+#define HASH_CHECK(x,y,z) do{if(cJSON_HasObjectItem(root,x)){cJSON*object=cJSON_GetObjectItem(root,x);if(!cJSON_IsString(object)){goto freeBuffer;}unsigned char hash[z];y(buffer,fileSize.QuadPart,hash);char output[z*2+1];HexDump(output,z,hash);match=!strcmp(output,cJSON_GetStringValue(object));goto freeBuffer;}}while(0)
 
-static BOOL SHA512Check(const cJSON* stringNode, const BYTE* buffer, size_t size);
-static BOOL SHA384Check(const cJSON* stringNode, const BYTE* buffer, size_t size);
-static BOOL SHA256Check(const cJSON * stringNode, const BYTE * buffer, size_t size);
-static BOOL SHA224Check(const cJSON * stringNode, const BYTE * buffer, size_t size);
-static BOOL SHA1Check(const cJSON * stringNode, const BYTE * buffer, size_t size);
-static BOOL MD5Check(const cJSON * stringNode, const BYTE * buffer, size_t size);
+static void HexDump(char* output, unsigned long size, unsigned char* input);
 
 BOOL CheckFileHash(cJSON* root, LPWSTR filePath) {
 	HANDLE file = CreateFileW(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -44,12 +39,12 @@ BOOL CheckFileHash(cJSON* root, LPWSTR filePath) {
 		goto freeBuffer;
 	}
 
-	HASH_CHECK("sha512", SHA512Check);
-	HASH_CHECK("sha384", SHA384Check);
-	HASH_CHECK("sha256", SHA256Check);
-	HASH_CHECK("sha224", SHA224Check);
-	HASH_CHECK("sha1", SHA1Check);
-	HASH_CHECK("md5", MD5Check);
+	HASH_CHECK("sha512", SHA512, SHA512_DIGEST_LENGTH);
+	HASH_CHECK("sha384", SHA384, SHA384_DIGEST_LENGTH);
+	HASH_CHECK("sha256", SHA256, SHA256_DIGEST_LENGTH);
+	HASH_CHECK("sha224", SHA224, SHA224_DIGEST_LENGTH);
+	HASH_CHECK("sha1", SHA1, SHA_DIGEST_LENGTH);
+	HASH_CHECK("md5", MD5, MD5_DIGEST_LENGTH);
 freeBuffer:
 	LocalFree(buffer);
 closeHandle:
@@ -67,52 +62,4 @@ static void HexDump(char* output, unsigned long size, unsigned char* input) {
 	}
 
 	output[size * 2] = 0;
-}
-
-static BOOL SHA512Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
-	unsigned char hash[SHA512_DIGEST_LENGTH];
-	SHA512(buffer, size, hash);
-	char output[SHA512_DIGEST_LENGTH * 2 + 1];
-	HexDump(output, SHA512_DIGEST_LENGTH, hash);
-	return !strcmp(output, cJSON_GetStringValue(stringNode));
-}
-
-static BOOL SHA384Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
-	unsigned char hash[SHA384_DIGEST_LENGTH];
-	SHA384(buffer, size, hash);
-	char output[SHA384_DIGEST_LENGTH * 2 + 1];
-	HexDump(output, SHA384_DIGEST_LENGTH, hash);
-	return !strcmp(output, cJSON_GetStringValue(stringNode));
-}
-
-static BOOL SHA256Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
-	unsigned char hash[SHA256_DIGEST_LENGTH];
-	SHA256(buffer, size, hash);
-	char output[SHA256_DIGEST_LENGTH * 2 + 1];
-	HexDump(output, SHA256_DIGEST_LENGTH, hash);
-	return !strcmp(output, cJSON_GetStringValue(stringNode));
-}
-
-static BOOL SHA224Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
-	unsigned char hash[SHA224_DIGEST_LENGTH];
-	SHA224(buffer, size, hash);
-	char output[SHA224_DIGEST_LENGTH * 2 + 1];
-	HexDump(output, SHA224_DIGEST_LENGTH, hash);
-	return !strcmp(output, cJSON_GetStringValue(stringNode));
-}
-
-static BOOL SHA1Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
-	unsigned char hash[SHA_DIGEST_LENGTH];
-	SHA1(buffer, size, hash);
-	char output[SHA_DIGEST_LENGTH * 2 + 1];
-	HexDump(output, SHA_DIGEST_LENGTH, hash);
-	return !strcmp(output, cJSON_GetStringValue(stringNode));
-}
-
-static BOOL MD5Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
-	unsigned char hash[MD5_DIGEST_LENGTH];
-	MD5(buffer, size, hash);
-	char output[MD5_DIGEST_LENGTH * 2 + 1];
-	HexDump(output, MD5_DIGEST_LENGTH, hash);
-	return !strcmp(output, cJSON_GetStringValue(stringNode));
 }
