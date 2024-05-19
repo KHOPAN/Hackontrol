457c8b06c8b20446e179417c5906b8ea8fe9d3d9
KHOPAN <puthipnakchan@gmail.com>
2024-05-19 12:26:34 +0700

    Add SHA384

--- Hackontrol/libdll32/execute_check_hash.c
+++ Hackontrol/libdll32/execute_check_hash.c
@@ -1,8 +1,10 @@
 #include "execute.h"
 #include <openssl/sha.h>
-#include <khopanstring.h>
+
+#define HASH_CHECK(x,y) do{if(cJSON_HasObjectItem(root,x)){cJSON*object=cJSON_GetObjectItem(root,x);if(!cJSON_IsString(object)){goto freeBuffer;}match=y(object,buffer,fileSize.QuadPart);goto freeBuffer;}}while(0)
 
 static BOOL SHA512Check(const cJSON* stringNode, const BYTE* buffer, size_t size);
+static BOOL SHA384Check(const cJSON* stringNode, const BYTE* buffer, size_t size);
 
 BOOL CheckFileHash(cJSON* root, LPWSTR filePath) {
 	HANDLE file = CreateFileW(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -34,11 +36,8 @@ BOOL CheckFileHash(cJSON* root, LPWSTR filePath) {
 		goto freeBuffer;
 	}
 
-	if(cJSON_HasObjectItem(root, "sha512")) {
-		match = SHA512Check(cJSON_GetObjectItem(root, "sha512"), buffer, fileSize.QuadPart);
-		goto freeBuffer;
-	}
-
+	HASH_CHECK("sha512", SHA512Check);
+	HASH_CHECK("sha384", SHA384Check);
 freeBuffer:
 	LocalFree(buffer);
 closeHandle:
@@ -46,22 +45,30 @@ closeHandle:
 	return match;
 }
 
-static BOOL SHA512Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
-	if(!cJSON_IsString(stringNode)) {
-		return FALSE;
-	}
-
-	unsigned char hash[SHA512_DIGEST_LENGTH];
-	SHA512(buffer, size, hash);
-	char hashString[SHA512_DIGEST_LENGTH * 2 + 1];
+static void HexDump(char* output, unsigned long size, unsigned char* input) {
 	const char* hexadecimal = "0123456789abcdef";
 
-	for(unsigned char i = 0; i < SHA512_DIGEST_LENGTH; i++) {
+	for(unsigned char i = 0; i < size; i++) {
 		unsigned char index = i * 2;
-		hashString[index] = hexadecimal[(hash[i] >> 4) & 0xF];
-		hashString[index + 1] = hexadecimal[hash[i] & 0xF];
+		output[index] = hexadecimal[(input[i] >> 4) & 0xF];
+		output[index + 1] = hexadecimal[input[i] & 0xF];
 	}
 
-	hashString[SHA512_DIGEST_LENGTH * 2] = 0;
-	return !strcmp(hashString, cJSON_GetStringValue(stringNode));
+	output[size * 2] = 0;
+}
+
+static BOOL SHA512Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
+	unsigned char hash[SHA512_DIGEST_LENGTH];
+	SHA512(buffer, size, hash);
+	char output[SHA512_DIGEST_LENGTH * 2 + 1];
+	HexDump(output, SHA512_DIGEST_LENGTH, hash);
+	return !strcmp(output, cJSON_GetStringValue(stringNode));
+}
+
+static BOOL SHA384Check(const cJSON* stringNode, const BYTE* buffer, size_t size) {
+	unsigned char hash[SHA384_DIGEST_LENGTH];
+	SHA384(buffer, size, hash);
+	char output[SHA384_DIGEST_LENGTH * 2 + 1];
+	HexDump(output, SHA384_DIGEST_LENGTH, hash);
+	return !strcmp(output, cJSON_GetStringValue(stringNode));
 }
