1d3450f903685002ec111195c990a54a7eb81a8a
KHOPAN <puthipnakchan@gmail.com>
2024-05-21 19:01:15 +0700

    Build the old version

--- Hackontrol/DialogDLL/main.c
+++ Hackontrol/DialogDLL/main.c
@@ -1,5 +1,5 @@
 #include <Windows.h>
 
 __declspec(dllexport) void __stdcall Execute(HWND window, HINSTANCE instance, LPSTR argument, int command) {
-	MessageBoxW(NULL, L"This is the latest version", L"Hackontrol", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+	MessageBoxW(NULL, L"!!OLD VERSION WARNING!!", L"Hackontrol", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
 }
