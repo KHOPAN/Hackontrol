58916ec7c7d809aaf9b083d5c1e26fefbde5029a
KHOPAN <puthipnakchan@gmail.com>
2024-05-21 18:59:56 +0700

    Add latest libdll32 tester

--- Hackontrol/DialogDLL/main.c
+++ Hackontrol/DialogDLL/main.c
@@ -1,17 +1,5 @@
 #include <Windows.h>
 
 __declspec(dllexport) void __stdcall Execute(HWND window, HINSTANCE instance, LPSTR argument, int command) {
-	MessageBoxW(NULL, L"Hello, world!", L"Information", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
-}
-
-BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
-	switch(reason) {
-	case DLL_PROCESS_ATTACH:
-		Execute(NULL, NULL, NULL, 0);
-		return TRUE;
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		return FALSE;
-	}
+	MessageBoxW(NULL, L"This is the latest version", L"Hackontrol", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
 }
