dd6a997b414a26b045255b2d9f5ae5ad20588a8c
KHOPAN <puthipnakchan@gmail.com>
2024-05-23 22:13:48 +0700

    Test environment variable in path name

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -2,9 +2,16 @@
 #include <khopanwin32.h>
 
 int main(int argc, char** argv) {
-	if(!KHWin32ExecuteCommandA("start chrome https://www.youtube.com")) {
-		KHWin32ConsoleErrorW(GetLastError(), L"KHWin32ExecuteCommandW");
+	HANDLE file = CreateFileW(L"%SystemRoot%\\System32\\rundll32.exe", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if(file == INVALID_HANDLE_VALUE) {
+		KHWin32ConsoleErrorW(GetLastError(), L"CreateFileW");
 		return 1;
+	}
+
+closeFile:
+	if(!CloseHandle(file)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"CloseHandle");
 	}
 
 	return 0;
