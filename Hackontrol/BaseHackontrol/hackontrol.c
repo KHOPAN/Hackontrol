236a5170f27975a963bd0b929449e6dd20d8f01f
KHOPAN <puthipnakchan@gmail.com>
2024-05-24 20:17:16 +0700

    Add Base Hackontrol

---
+++ Hackontrol/BaseHackontrol/hackontrol.c
@@ -0,0 +1,5 @@
+#include "hackontrol.h"
+
+LPWSTR GetHackontrolDirectory() {
+	return L"C:\\Windows\\System32";
+}
--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <khopanwin32.h>
+#include <hackontrol.h>
+
+//#define ENVIRONMENT_VARIABLE L"ProgramFiles(x86)"
 
 int main(int argc, char** argv) {
-	HANDLE file = CreateFileW(L"%SystemRoot%\\System32\\rundll32.exe", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	/*DWORD size = GetEnvironmentVariableW(ENVIRONMENT_VARIABLE, NULL, 0);
 
-	if(file == INVALID_HANDLE_VALUE) {
-		KHWin32ConsoleErrorW(GetLastError(), L"CreateFileW");
+	if(!size) {
+		KHWin32ConsoleErrorW(GetLastError(), L"GetEnvironmentVariableW");
 		return 1;
 	}
 
-closeFile:
-	if(!CloseHandle(file)) {
-		KHWin32ConsoleErrorW(GetLastError(), L"CloseHandle");
+	LPWSTR buffer = LocalAlloc(LMEM_FIXED, size * sizeof(WCHAR));
+
+	if(!buffer) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LocalAlloc");
+		return 1;
+	}
+
+	int returnValue = 1;
+
+	if(!GetEnvironmentVariableW(ENVIRONMENT_VARIABLE, buffer, size)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"GetEnvironmentVariableW");
+		goto freeBuffer;
+	}
+
+	printf("%ws\n", buffer);
+	returnValue = 0;
+freeBuffer:
+	if(LocalFree(buffer)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LocalFree");
+		returnValue = 1;
+	}
+
+	return returnValue;*/
+	LPWSTR pathFolderHackontrol = GetHackontrolDirectory();
+
+	if(!pathFolderHackontrol) {
+		KHWin32ConsoleErrorW(GetLastError(), L"GetHackontrolDirectory");
+		return 1;
+	}
+
+	printf("Path: %ws\n", pathFolderHackontrol);
+
+	if(LocalFree(pathFolderHackontrol)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LocalFree");
+		return 1;
 	}
 
 	return 0;
