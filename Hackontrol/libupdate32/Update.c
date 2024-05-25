4ad6186894a1d7e8ceb985f2b08f50d268f4bbe9
KHOPAN <puthipnakchan@gmail.com>
2024-05-25 16:26:13 +0700

    Add dynamic download URL

--- Hackontrol/libupdate32/Update.c
+++ Hackontrol/libupdate32/Update.c
@@ -5,13 +5,45 @@
 #define FREE(x) if(LocalFree(x)) KHWin32DialogErrorW(GetLastError(), L"LocalFree")
 
 #define FUNCTION_LIBDLL32 L"Execute"
-#define URL_LIBDLL32_FILE "https://raw.githubusercontent.com/KHOPAN/Hackontrol/main/system/libdll32.dll"
 
 __declspec(dllexport) void __stdcall Update(HWND window, HINSTANCE instance, LPSTR argument, int command) {
+	LPWSTR wideArgument = KHFormatMessageW(L"%S", argument);
+
+	if(!wideArgument) {
+		KHWin32DialogErrorW(ERROR_FUNCTION_FAILED, L"KHFormatMessageW");
+		return;
+	}
+
+	int count;
+	LPWSTR* arguments = CommandLineToArgvW(wideArgument, &count);
+	FREE(wideArgument);
+
+	if(!arguments) {
+		KHWin32DialogErrorW(GetLastError(), L"CommandLineToArgvW");
+		return;
+	}
+
+	if(count < 2) {
+		MessageBoxW(NULL, L"Invalid argument, expected:\n<pid> <url>", L"libupdate32", MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+		LocalFree(arguments);
+		return;
+	}
+
+	DWORD processIdentifier = (DWORD) _wtoll(arguments[0]);
+	LPSTR downloadUrl = KHFormatMessageA("%ws", arguments[1]);
+	LocalFree(arguments);
+
+	if(!downloadUrl) {
+		KHWin32DialogErrorW(GetLastError(), L"KHFormatMessageA");
+		return;
+	}
+
 	DataStream stream = {0};
 	CURLcode code;
+	BOOL result = HackontrolDownloadData(&stream, downloadUrl, FALSE, &code);
+	LocalFree(downloadUrl);
 
-	if(!HackontrolDownloadData(&stream, URL_LIBDLL32_FILE, FALSE, &code)) {
+	if(!result) {
 		KHCURLDialogErrorW(code, L"HackontrolDownloadData");
 		return;
 	}
@@ -44,7 +76,7 @@ __declspec(dllexport) void __stdcall Update(HWND window, HINSTANCE instance, LPS
 		goto freeStream;
 	}
 
-	BOOL result = KHWin32StartDynamicLibraryW(pathFileLibdll32, FUNCTION_LIBDLL32, NULL);
+	result = KHWin32StartDynamicLibraryW(pathFileLibdll32, FUNCTION_LIBDLL32, NULL);
 	FREE(pathFileLibdll32);
 
 	if(!result) {
