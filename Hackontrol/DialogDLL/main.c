ae8b30e1753fcfbfb7381323fee35f1d5f73fa9a
KHOPAN <puthipnakchan@gmail.com>
2024-05-27 19:42:15 +0700

    Remove DialogDLL

--- Hackontrol/DialogDLL/main.c
+++
@@ -1,5 +0,0 @@
-#include <Windows.h>
-
-__declspec(dllexport) void __stdcall Execute(HWND window, HINSTANCE instance, LPSTR argument, int command) {
-	MessageBoxW(NULL, L"!!OLD VERSION WARNING!!", L"Hackontrol", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
-}
