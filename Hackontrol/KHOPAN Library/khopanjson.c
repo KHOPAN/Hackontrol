32ccf7f432c0c124fb0b42d7b4d44cd73cac7cf8
KHOPAN <puthipnakchan@gmail.com>
2024-05-29 19:47:24 +0700

    Add string, object and array functions

--- Hackontrol/KHOPAN Library/khopanjson.c
+++ Hackontrol/KHOPAN Library/khopanjson.c
@@ -21,3 +21,69 @@ cJSON_bool KHJSONGetBoolean(const cJSON* const root, const char* const field, co
 
 	return cJSON_IsTrue(item);
 }
+
+char* KHJSONGetString(const cJSON* const root, const char* const field, char* const defaultValue) {
+	if(!root || !field) {
+		return defaultValue;
+	}
+
+	if(!cJSON_IsObject(root)) {
+		return defaultValue;
+	}
+
+	if(!cJSON_HasObjectItem(root, field)) {
+		return defaultValue;
+	}
+
+	cJSON* item = cJSON_GetObjectItem(root, field);
+
+	if(!cJSON_IsString(item)) {
+		return defaultValue;
+	}
+
+	return cJSON_GetStringValue(item);
+}
+
+cJSON* KHJSONGetObject(const cJSON* const root, const char* const field, cJSON* const defaultValue) {
+	if(!root || !field) {
+		return defaultValue;
+	}
+
+	if(!cJSON_IsObject(root)) {
+		return defaultValue;
+	}
+
+	if(!cJSON_HasObjectItem(root, field)) {
+		return defaultValue;
+	}
+
+	cJSON* item = cJSON_GetObjectItem(root, field);
+
+	if(!cJSON_IsObject(item)) {
+		return defaultValue;
+	}
+
+	return item;
+}
+
+cJSON* KHJSONGetArray(const cJSON* const root, const char* const field, cJSON* const defaultValue) {
+	if(!root || !field) {
+		return defaultValue;
+	}
+
+	if(!cJSON_IsObject(root)) {
+		return defaultValue;
+	}
+
+	if(!cJSON_HasObjectItem(root, field)) {
+		return defaultValue;
+	}
+
+	cJSON* item = cJSON_GetObjectItem(root, field);
+
+	if(!cJSON_IsArray(item)) {
+		return defaultValue;
+	}
+
+	return item;
+}
--- Hackontrol/KHOPAN Library/khopanjson.h
+++ Hackontrol/KHOPAN Library/khopanjson.h
@@ -6,6 +6,9 @@
 extern "C" {
 #endif
 cJSON_bool KHJSONGetBoolean(const cJSON* const root, const char* const field, const cJSON_bool defaultValue);
+char* KHJSONGetString(const cJSON* const root, const char* const field, char* const defaultValue);
+cJSON* KHJSONGetObject(const cJSON* const root, const char* const field, cJSON* const defaultValue);
+cJSON* KHJSONGetArray(const cJSON* const root, const char* const field, cJSON* const defaultValue);
 #ifdef __cplusplus
 }
 #endif
