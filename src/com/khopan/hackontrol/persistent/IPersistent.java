683fe90a3ba339a700b3daa27129ccec8fabed02
KHOPAN <puthipnakchan@gmail.com>
2024-06-10 22:15:08 +0700

    Delete persistent

--- src/com/khopan/hackontrol/persistent/IPersistent.java
+++
@@ -1,6 +0,0 @@
-package com.khopan.hackontrol.persistent;
-
-public interface IPersistent {
-	void write(IPersistentWriter writer);
-	void read(IPersistentReader reader);
-}
--- src/com/khopan/hackontrol/persistent/PersistentStorage.java
+++
@@ -1,6 +0,0 @@
-package com.khopan.hackontrol.persistent;
-
-public interface PersistentStorage {
-	<T extends IPersistent> void save(String key, T persistent, Class<T> persistentClass);
-	<T extends IPersistent> T load(String key, Class<T> persistentClass);
-}
