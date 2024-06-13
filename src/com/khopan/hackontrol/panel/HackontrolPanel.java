ace023adcb3ac84c2a98a4f095c6dd060334bc9f
KHOPAN <puthipnakchan@gmail.com>
2024-06-13 23:13:41 +0700

    Add hackontrol panel

---
+++ src/com/khopan/hackontrol/panel/HackontrolPanel.java
@@ -0,0 +1,10 @@
+package com.khopan.hackontrol.panel;
+
+public class HackontrolPanel extends Panel {
+	public static final String PANEL_NAME = "hackontrol";
+
+	@Override
+	public String panelName() {
+		return HackontrolPanel.PANEL_NAME;
+	}
+}
--- src/com/khopan/hackontrol/registration/PanelRegistry.java
+++ src/com/khopan/hackontrol/registration/PanelRegistry.java
@@ -1,10 +1,12 @@
 package com.khopan.hackontrol.registration;
 
+import com.khopan.hackontrol.panel.HackontrolPanel;
 import com.khopan.hackontrol.panel.PanelManager;
 
 public class PanelRegistry {
 	private PanelRegistry() {}
 
 	public static void register(PanelManager manager) {
+		manager.add(new HackontrolPanel());
 	}
 }
