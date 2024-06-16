89f2b910a15f61f701a8c421abca1f61d52e6652
KHOPAN <puthipnakchan@gmail.com>
2024-06-16 07:21:41 +0700

    Add control panel

---
+++ src/com/khopan/hackontrol/panel/ControlPanel.java
@@ -0,0 +1,43 @@
+package com.khopan.hackontrol.panel;
+
+import com.khopan.hackontrol.manager.interaction.ButtonManager;
+import com.khopan.hackontrol.manager.interaction.ButtonManager.ButtonType;
+import com.khopan.hackontrol.manager.interaction.Question;
+import com.khopan.hackontrol.manager.interaction.Question.QuestionType;
+import com.khopan.hackontrol.nativelibrary.Kernel;
+import com.khopan.hackontrol.registry.Registration;
+import com.khopan.hackontrol.widget.ControlWidget;
+
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+
+public class ControlPanel extends Panel {
+	private static final String PANEL_NAME = "control";
+
+	private static final Button BUTTON_SLEEP     = ButtonManager.staticButton(ButtonType.SECONDARY, "Sleep",     "sleep");
+	private static final Button BUTTON_HIBERNATE = ButtonManager.staticButton(ButtonType.SUCCESS,   "Hibernate", "hibernate");
+	private static final Button BUTTON_RESTART   = ButtonManager.staticButton(ButtonType.PRIMARY,   "Restart",   "restart");
+	private static final Button BUTTON_SHUTDOWN  = ButtonManager.staticButton(ButtonType.DANGER,    "Shutdown",  "shutdown");
+
+	@Override
+	public String panelName() {
+		return ControlPanel.PANEL_NAME;
+	}
+
+	@Override
+	public void registeration() {
+		this.register(Registration.BUTTON, ControlPanel.BUTTON_SLEEP,     context -> Question.positive(context.reply(), "Are you sure you want to sleep?",     QuestionType.YES_NO, Kernel :: sleep));
+		this.register(Registration.BUTTON, ControlPanel.BUTTON_HIBERNATE, context -> Question.positive(context.reply(), "Are you sure you want to hibernate?", QuestionType.YES_NO, Kernel :: hibernate));
+		this.register(Registration.BUTTON, ControlPanel.BUTTON_RESTART,   context -> Question.positive(context.reply(), "Are you sure you want to restart?",   QuestionType.YES_NO, Kernel :: restart));
+		this.register(Registration.BUTTON, ControlPanel.BUTTON_SHUTDOWN,  context -> Question.positive(context.reply(), "Are you sure you want to shutdown?",  QuestionType.YES_NO, Kernel :: shutdown));
+	}
+
+	@Override
+	public ControlWidget[] controlWidget() {
+		return new ControlWidget[] {
+				ControlWidget.newBuilder()
+				.text("**Power**")
+				.actionRow(ControlPanel.BUTTON_SLEEP, ControlPanel.BUTTON_HIBERNATE, ControlPanel.BUTTON_RESTART, ControlPanel.BUTTON_SHUTDOWN)
+				.build()
+		};
+	}
+}
--- src/com/khopan/hackontrol/registration/PanelRegistry.java
+++ src/com/khopan/hackontrol/registration/PanelRegistry.java
@@ -1,5 +1,6 @@
 package com.khopan.hackontrol.registration;
 
+import com.khopan.hackontrol.panel.ControlPanel;
 import com.khopan.hackontrol.panel.HackontrolPanel;
 import com.khopan.hackontrol.panel.PanelManager;
 
@@ -8,5 +9,6 @@ public class PanelRegistry {
 
 	public static void register(PanelManager manager) {
 		manager.register(new HackontrolPanel());
+		manager.register(new ControlPanel());
 	}
 }
