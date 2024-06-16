e7fd6500eaf2c8c3e05b1701390f680ebf574942
KHOPAN <puthipnakchan@gmail.com>
2024-06-16 19:02:01 +0700

    Remove named panel

--- src/com/khopan/hackontrol/registration/PanelRegistry.java
+++ src/com/khopan/hackontrol/registration/PanelRegistry.java
@@ -2,7 +2,6 @@ package com.khopan.hackontrol.registration;
 
 import com.khopan.hackontrol.panel.ControlPanel;
 import com.khopan.hackontrol.panel.HackontrolPanel;
-import com.khopan.hackontrol.panel.Panel;
 import com.khopan.hackontrol.panel.PanelManager;
 
 public class PanelRegistry {
@@ -10,27 +9,6 @@ public class PanelRegistry {
 
 	public static void register(PanelManager manager) {
 		manager.register(new HackontrolPanel());
-		manager.register(new NamedPanel("audio"));
-		manager.register(new NamedPanel("camera"));
-		manager.register(new NamedPanel("command"));
 		manager.register(new ControlPanel());
-		manager.register(new NamedPanel("dialog"));
-		manager.register(new NamedPanel("file"));
-		manager.register(new NamedPanel("keylogger"));
-		manager.register(new NamedPanel("process"));
-		manager.register(new NamedPanel("screenshot"));
-	}
-
-	private static class NamedPanel extends Panel {
-		private final String name;
-
-		private NamedPanel(String name) {
-			this.name = name;
-		}
-
-		@Override
-		public String panelName() {
-			return this.name;
-		}
 	}
 }
