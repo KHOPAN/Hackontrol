f7e562cda4a5d6f9455a7a8f2fb8de0e25277960
KHOPAN <puthipnakchan@gmail.com>
2024-06-22 11:55:31 +0700

    Load JNI_CreateJavaVM function

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,4 +1,7 @@
 #include <khopanwin32.h>
+#include <jni.h>
+
+typedef jint(__stdcall* CreateJavaVMFunction) (JavaVM** virtualMachine, void** environment, void* arguments);
 
 int main(int argc, char** argv) {
 	HMODULE libraryJava = LoadLibraryW(L"C:\\Program Files\\Java\\jdk-21\\bin\\server\\jvm.dll");
@@ -8,10 +11,20 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	CreateJavaVMFunction createJavaVM = (CreateJavaVMFunction) GetProcAddress(libraryJava, "JNI_CreateJavaVM");
+	int returnValue = 1;
+
+	if(!createJavaVM) {
+		KHWin32ConsoleErrorW(GetLastError(), L"GetProcAddress");
+		goto freeLibraryJava;
+	}
+
+	returnValue = 0;
+freeLibraryJava:
 	if(!FreeLibrary(libraryJava)) {
 		KHWin32ConsoleErrorW(GetLastError(), L"LoadLibraryW");
 		return 1;
 	}
 
-	return 0;
+	return returnValue;
 }
