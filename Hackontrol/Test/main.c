7129790998593cfe77c094acf3b1bc6ec8343545
KHOPAN <puthipnakchan@gmail.com>
2024-06-22 11:48:38 +0700

    Test load JVM DLL

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include <khopanwin32.h>
 
 int main(int argc, char** argv) {
-	printf("Hello, world!\n");
+	HMODULE libraryJava = LoadLibraryW(L"C:\\Program Files\\Java\\jdk-21\\bin\\server\\jvm.dll");
+
+	if(!libraryJava) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LoadLibraryW");
+		return 1;
+	}
+
+	if(!FreeLibrary(libraryJava)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LoadLibraryW");
+		return 1;
+	}
+
 	return 0;
 }
