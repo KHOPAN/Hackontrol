cc85497acba81c445d2d224c71b986df1c5f6a05
KHOPAN <puthipnakchan@gmail.com>
2024-06-26 22:50:00 +0700

    Add list root button

--- src/com/khopan/hackontrol/panel/FilePanel.java
+++ src/com/khopan/hackontrol/panel/FilePanel.java
@@ -1,9 +1,16 @@
 package com.khopan.hackontrol.panel;
 
+import com.khopan.hackontrol.manager.interaction.ButtonManager;
+import com.khopan.hackontrol.manager.interaction.ButtonManager.ButtonType;
+import com.khopan.hackontrol.registry.Registration;
 import com.khopan.hackontrol.widget.ControlWidget;
+
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 public class FilePanel extends Panel {
 	private static final String PANEL_NAME = "file";
+
+	private static final Button BUTTON_LIST_ROOT = ButtonManager.staticButton(ButtonType.SUCCESS, "List Root", "listRoot");
 
 	@Override
 	public String panelName() {
@@ -12,13 +19,15 @@ public class FilePanel extends Panel {
 
 	@Override
 	public void registeration() {
-
+		this.register(Registration.BUTTON, FilePanel.BUTTON_LIST_ROOT, null);
 	}
 
 	@Override
 	public ControlWidget[] controlWidget() {
 		return new ControlWidget[] {
-
+				ControlWidget.newBuilder()
+				.actionRow(FilePanel.BUTTON_LIST_ROOT)
+				.build()
 		};
 	}
 }
