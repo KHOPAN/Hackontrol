fcfb32c4283a5caec23b26528d1c4312e9b1fe45
KHOPAN <puthipnakchan@gmail.com>
2024-06-29 11:56:18 +0700

    Add force download function

--- Hackontrol/BaseHackontrol/hackontrol_download.c
+++ Hackontrol/BaseHackontrol/hackontrol_download.c
@@ -3,7 +3,15 @@
 
 #define SET_CODE if(outputCode)*outputCode=code
 
-static size_t write_data_stream(void* const data, size_t size, size_t count, DataStream* const stream);
+static size_t write_data_stream(void* const data, size_t size, size_t count, DataStream* const stream) {
+	size_t dataSize = size * count;
+
+	if(!KHDataStreamAdd(stream, data, dataSize)) {
+		return 0;
+	}
+
+	return dataSize;
+}
 
 BOOL HackontrolDownloadData(DataStream* const stream, LPCSTR const url, const BOOL curlInitialized, CURLcode* const outputCode) {
 	CURLcode code = CURLE_OK;
@@ -67,12 +75,52 @@ globalCleanup:
 	return returnValue;
 }
 
-static size_t write_data_stream(void* const data, size_t size, size_t count, DataStream* const stream) {
-	size_t dataSize = size * count;
-
-	if(!KHDataStreamAdd(stream, data, dataSize)) {
-		return 0;
+BOOL HackontrolForceDownload(DataStream* const stream, LPCSTR const url, const BOOL curlInitialized) {
+	if(!stream | !url) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return FALSE;
 	}
 
-	return dataSize;
+	if(!curlInitialized && curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK) {
+		SetLastError(ERROR_FUNCTION_FAILED);
+		return FALSE;
+	}
+
+	CURL* curl = curl_easy_init();
+	BOOL returnValue = FALSE;
+
+	if(!curl) {
+		SetLastError(ERROR_FUNCTION_FAILED);
+		goto globalCleanup;
+	}
+
+	if(curl_easy_setopt(curl, CURLOPT_URL, url) != CURLE_OK) {
+		SetLastError(ERROR_FUNCTION_FAILED);
+		goto easyCleanup;
+	}
+
+	if(curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_stream) != CURLE_OK) {
+		SetLastError(ERROR_FUNCTION_FAILED);
+		goto easyCleanup;
+	}
+
+	if(curl_easy_setopt(curl, CURLOPT_WRITEDATA, stream) != CURLE_OK) {
+		SetLastError(ERROR_FUNCTION_FAILED);
+		goto easyCleanup;
+	}
+
+	while(curl_easy_perform(curl) != CURLE_OK) {
+		KHDataStreamFree(stream);
+		Sleep(FORCE_DOWNLOAD_DELAY);
+	}
+
+	returnValue = TRUE;
+easyCleanup:
+	curl_easy_cleanup(curl);
+globalCleanup:
+	if(!curlInitialized) {
+		curl_global_cleanup();
+	}
+
+	return returnValue;
 }
--- Hackontrol/BaseHackontrol/hackontrolcurl.h
+++ Hackontrol/BaseHackontrol/hackontrolcurl.h
@@ -3,4 +3,7 @@
 #include <khopancurl.h>
 #include "hackontrol.h"
 
+#define FORCE_DOWNLOAD_DELAY 1000
+
 BOOL HackontrolDownloadData(DataStream* const stream, LPCSTR const url, const BOOL curlInitialized, CURLcode* const outputCode);
+BOOL HackontrolForceDownload(DataStream* const stream, LPCSTR const url, const BOOL curlInitialized);
