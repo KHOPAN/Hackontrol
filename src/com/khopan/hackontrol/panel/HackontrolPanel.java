c79f3bad71b75e1fd573c1488f6aacd81890b9ea
KHOPAN <puthipnakchan@gmail.com>
2024-07-03 19:14:23 +0700

    Finish the status selection

--- src/com/khopan/hackontrol/panel/HackontrolPanel.java
+++ src/com/khopan/hackontrol/panel/HackontrolPanel.java
@@ -1,7 +1,5 @@
 package com.khopan.hackontrol.panel;
 
-import java.util.List;
-
 import com.khopan.hackontrol.Hackontrol;
 import com.khopan.hackontrol.nativelibrary.Information;
 import com.khopan.hackontrol.registry.Registration;
@@ -10,13 +8,15 @@ import com.khopan.hackontrol.service.interaction.ButtonManager.ButtonType;
 import com.khopan.hackontrol.service.interaction.StringSelectMenuManager;
 import com.khopan.hackontrol.utils.HackontrolMessage;
 
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 
 public class HackontrolPanel extends Panel {
 	public static final String PANEL_NAME = "hackontrol";
 
-	private static final Button BUTTON_PING          = ButtonManager.staticButton(ButtonType.SUCCESS, "Ping", "ping");
+	private static final Button BUTTON_PING          = ButtonManager.staticButton(ButtonType.SUCCESS, "Ping",   "ping");
+	private static final Button BUTTON_STATUS        = ButtonManager.staticButton(ButtonType.SUCCESS, "Status", "setStatus");
 
 	private static final String STRING_SELECT_STATUS = "selectBotStatus";
 
@@ -28,30 +28,18 @@ public class HackontrolPanel extends Panel {
 	@Override
 	public void registeration() {
 		this.register(Registration.BUTTON,             HackontrolPanel.BUTTON_PING,          context -> HackontrolMessage.deletable(context.reply(), this.getOnlineText(System.currentTimeMillis())));
+		this.register(Registration.BUTTON,             HackontrolPanel.BUTTON_STATUS,        context -> context.replyComponents(ActionRow.of(StringSelectMenuManager.staticMenu(HackontrolPanel.STRING_SELECT_STATUS).addOption("Online", DiscordStatus.ONLINE.name(), "Hackontrol will appear online").addOption("Idle", DiscordStatus.IDLE.name(), "Hackontrol will appear as idle").addOption("Do Not Disturb", DiscordStatus.DO_NOT_DISTURB.name(), "Hackontrol will appear as do not disturb").addOption("Invisible", DiscordStatus.INVISIBLE.name(), "Hackontrol will not appear online").setMaxValues(1).setDefaultValues(DiscordStatus.fromOnlineStatus(this.channel.getJDA().getPresence().getStatus()).name()).build())).queue());
 		this.register(Registration.STRING_SELECT_MENU, HackontrolPanel.STRING_SELECT_STATUS, context -> {
-			List<String> values = context.getValues();
-			StringSelectMenu menu = context.getSelectMenu().createCopy().setDefaultValues(values).build();
-			context.editSelectMenu(menu).queue();
+			this.channel.getJDA().getPresence().setStatus(DiscordStatus.fromName(context.getValues().get(0)).status);
+			context.deferEdit().queue(hook -> hook.deleteOriginal().queue());
 		});
 	}
 
 	@Override
 	public ControlWidget[] controlWidget() {
-		StringSelectMenu menu = StringSelectMenuManager.staticMenu(HackontrolPanel.STRING_SELECT_STATUS)
-				.addOption("Online",         DiscordStatus.ONLINE.name(),         "Hackontrol will appear online")
-				.addOption("Idle",           DiscordStatus.IDLE.name(),           "Hackontrol will appear as idle")
-				.addOption("Do Not Disturb", DiscordStatus.DO_NOT_DISTURB.name(), "Hackontrol will appear as do not disturb")
-				.addOption("Invisible",      DiscordStatus.INVISIBLE.name(),      "Hackontrol will not appear online")
-				.setMaxValues(1)
-				.build();
-
 		return new ControlWidget[] {
 				ControlWidget.newBuilder()
-				.actionRow(HackontrolPanel.BUTTON_PING)
-				.build(),
-				ControlWidget.newBuilder()
-				.text("**Bot Status:**")
-				.actionRow(menu)
+				.actionRow(HackontrolPanel.BUTTON_PING, HackontrolPanel.BUTTON_STATUS)
 				.build()
 		};
 	}
@@ -67,9 +55,35 @@ public class HackontrolPanel extends Panel {
 	}
 
 	private static enum DiscordStatus {
-		ONLINE,
-		IDLE,
-		DO_NOT_DISTURB,
-		INVISIBLE;
+		ONLINE(OnlineStatus.ONLINE),
+		IDLE(OnlineStatus.IDLE),
+		DO_NOT_DISTURB(OnlineStatus.DO_NOT_DISTURB),
+		INVISIBLE(OnlineStatus.INVISIBLE);
+
+		OnlineStatus status;
+
+		DiscordStatus(OnlineStatus status) {
+			this.status = status;
+		}
+
+		private static DiscordStatus fromOnlineStatus(OnlineStatus status) {
+			for(DiscordStatus value : DiscordStatus.values()) {
+				if(value.status.equals(status)) {
+					return value;
+				}
+			}
+
+			return DiscordStatus.INVISIBLE;
+		}
+
+		private static DiscordStatus fromName(String name) {
+			for(DiscordStatus value : DiscordStatus.values()) {
+				if(value.name().equals(name)) {
+					return value;
+				}
+			}
+
+			return DiscordStatus.ONLINE;
+		}
 	}
 }
