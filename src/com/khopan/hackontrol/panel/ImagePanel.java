5a4b10dfd0c27069da6389c462e92b7eec676ee1
KHOPAN <puthipnakchan@gmail.com>
2024-07-06 07:10:47 +0700

    Add image panel

---
+++ src/com/khopan/hackontrol/panel/ImagePanel.java
@@ -0,0 +1,22 @@
+package com.khopan.hackontrol.panel;
+
+public class ImagePanel extends Panel {
+	private static final String PANEL_NAME = "image";
+
+	@Override
+	public String panelName() {
+		return ImagePanel.PANEL_NAME;
+	}
+
+	@Override
+	public void registeration() {
+
+	}
+
+	@Override
+	public ControlWidget[] controlWidget() {
+		return new ControlWidget[] {
+
+		};
+	}
+}
--- src/com/khopan/hackontrol/registration/PanelRegistry.java
+++ src/com/khopan/hackontrol/registration/PanelRegistry.java
@@ -6,6 +6,7 @@ import com.khopan.hackontrol.panel.ControlPanel;
 import com.khopan.hackontrol.panel.DialogPanel;
 import com.khopan.hackontrol.panel.FilePanel;
 import com.khopan.hackontrol.panel.HackontrolPanel;
+import com.khopan.hackontrol.panel.ImagePanel;
 import com.khopan.hackontrol.panel.KeyLoggerPanel;
 import com.khopan.hackontrol.panel.PanelManager;
 import com.khopan.hackontrol.panel.ScreenshotPanel;
@@ -20,6 +21,7 @@ public class PanelRegistry {
 		manager.register(new ControlPanel());
 		manager.register(new DialogPanel());
 		manager.register(new FilePanel());
+		manager.register(new ImagePanel());
 		manager.register(new KeyLoggerPanel());
 		manager.register(new ScreenshotPanel());
 	}
