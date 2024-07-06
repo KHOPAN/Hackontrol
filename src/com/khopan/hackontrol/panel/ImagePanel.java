ee84a20614fd5e9176da54bd3db64f6cf8f71bcb
KHOPAN <puthipnakchan@gmail.com>
2024-07-06 07:15:07 +0700

    Add upload button

--- src/com/khopan/hackontrol/panel/ImagePanel.java
+++ src/com/khopan/hackontrol/panel/ImagePanel.java
@@ -1,7 +1,15 @@
 package com.khopan.hackontrol.panel;
 
+import com.khopan.hackontrol.registry.Registration;
+import com.khopan.hackontrol.service.interaction.ButtonManager;
+import com.khopan.hackontrol.service.interaction.ButtonManager.ButtonType;
+
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+
 public class ImagePanel extends Panel {
 	private static final String PANEL_NAME = "image";
+
+	private static final Button BUTTON_UPLOAD = ButtonManager.staticButton(ButtonType.SUCCESS, "Upload", "imageUpload");
 
 	@Override
 	public String panelName() {
@@ -10,13 +18,15 @@ public class ImagePanel extends Panel {
 
 	@Override
 	public void registeration() {
-
+		this.register(Registration.BUTTON, ImagePanel.BUTTON_UPLOAD, context -> context.deferEdit().queue());
 	}
 
 	@Override
 	public ControlWidget[] controlWidget() {
 		return new ControlWidget[] {
-
+				ControlWidget.newBuilder()
+				.actionRow(ImagePanel.BUTTON_UPLOAD)
+				.build()
 		};
 	}
 }
