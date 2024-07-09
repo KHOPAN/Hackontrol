2ebfcb8350f36cdac8418dfb79c2d929c3979835
KHOPAN <puthipnakchan@gmail.com>
2024-07-10 06:52:25 +0700

    Add restart functions

---
+++ Hackontrol/libupdate32/Restart.c
@@ -0,0 +1,5 @@
+#include <Windows.h>
+
+__declspec(dllexport) void __stdcall Restart(HWND window, HINSTANCE instance, LPSTR argument, int command) {
+
+}
---
+++ Hackontrol/libupdate32/RestartUpdate.c
@@ -0,0 +1,5 @@
+#include <Windows.h>
+
+__declspec(dllexport) void __stdcall RestartUpdate(HWND window, HINSTANCE instance, LPSTR argument, int command) {
+
+}
