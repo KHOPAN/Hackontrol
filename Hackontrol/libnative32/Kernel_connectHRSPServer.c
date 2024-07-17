f2fb7360787b5d4d77b5a6eeb40b879d97a964bc
KHOPAN <puthipnakchan@gmail.com>
2024-07-17 18:55:34 +0700

    Implement DLL caller

--- Hackontrol/libnative32/Kernel_connectHRSPServer.c
+++ Hackontrol/libnative32/Kernel_connectHRSPServer.c
@@ -1,5 +1,40 @@
+#include <khopanjava.h>
 #include "Kernel.h"
+#include "exception.h"
+
+typedef void(__stdcall* ConnectHRSPServerFunction) (JNIEnv* const environment, LPCSTR hostName, const unsigned int port, const jobject callback);
 
 void Kernel_connectHRSPServer(JNIEnv* const environment, const jclass class, const jstring host, const jint port, const jobject callback) {
+	if(!host) {
+		KHJavaThrowIllegalArgumentExceptionW(environment, L"Host cannot be null");
+		return;
+	}
 
+	if(!callback) {
+		KHJavaThrowIllegalArgumentExceptionW(environment, L"Callback cannot be null");
+		return;
+	}
+
+	HMODULE library = LoadLibraryW(L"D:\\GitHub Repository\\Hackontrol\\Hackontrol\\x64\\Debug\\libhrsp32.dll");
+
+	if(!library) {
+		HackontrolThrowWin32Error(environment, L"LoadLibraryW");
+		return;
+	}
+
+	ConnectHRSPServerFunction function = (ConnectHRSPServerFunction) GetProcAddress(library, "ConnectHRSPServer");
+
+	if(!function) {
+		HackontrolThrowWin32Error(environment, L"GetProcAddress");
+		return;
+	}
+
+	const char* hostName = (*environment)->GetStringUTFChars(environment, host, NULL);
+
+	if(!hostName || !strlen(hostName)) {
+		KHJavaThrowIllegalArgumentExceptionW(environment, L"Null or empty host name");
+		return;
+	}
+
+	function(environment, hostName, port, callback);
 }
