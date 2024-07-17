0f78fc478934a9ab54990e7a6c664d9da3e15d49
KHOPAN <puthipnakchan@gmail.com>
2024-07-17 15:34:06 +0700

    Add protocol header

--- Server/src/com/khopan/hrsp/HRSPServer.java
+++ Server/src/com/khopan/hrsp/HRSPServer.java
@@ -1,12 +1,26 @@
 package com.khopan.hrsp;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class HRSPServer {
 	public static void main(String[] args) throws Throwable {
 		ServerSocket server = new ServerSocket(42485);
 		Socket socket = server.accept();
+		InputStream inputStream = socket.getInputStream();
+		byte[] data = inputStream.readNBytes(16);
+
+		if(!"HRSP 1.0 CONNECT".equals(new String(data, StandardCharsets.UTF_8))) {
+			socket.close();
+			server.close();
+			return;
+		}
+
+		OutputStream outputStream = socket.getOutputStream();
+		outputStream.write("HRSP 1.0 OK".getBytes(StandardCharsets.UTF_8));
 		server.close();
 	}
 }
--- src/com/khopan/hackontrol/hrsp/HRSPClient.java
+++ src/com/khopan/hackontrol/hrsp/HRSPClient.java
@@ -3,6 +3,7 @@ package com.khopan.hackontrol.hrsp;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
 
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
@@ -11,7 +12,15 @@ public class HRSPClient {
 	public HRSPClient(String domainName, int port, Consumer<MessageCreateData> consumer) throws Throwable {
 		Socket socket = new Socket(domainName, port);
 		OutputStream outputStream = socket.getOutputStream();
+		outputStream.write("HRSP 1.0 CONNECT".getBytes(StandardCharsets.UTF_8));
 		InputStream inputStream = socket.getInputStream();
+		byte[] data = inputStream.readNBytes(11);
+
+		if(!"HRSP 1.0 OK".equals(new String(data, StandardCharsets.UTF_8))) {
+			socket.close();
+			return;
+		}
+
 		consumer.accept(MessageCreateData.fromContent("**Connected**"));
 		socket.close();
 	}
