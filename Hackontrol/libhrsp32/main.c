00837bb7d61cf8a4de2714141fee4eb39caa3324
KHOPAN <puthipnakchan@gmail.com>
2024-07-17 18:41:52 +0700

    Add libhrsp32

---
+++ Hackontrol/libhrsp32/main.c
@@ -0,0 +1,6 @@
+#include <Windows.h>
+#include <jni.h>
+
+_declspec(dllexport) void __stdcall InitializeHRSPServer(JNIEnv* const environment, LPCSTR hostName, const unsigned int port, const jobject callback) {
+
+}
