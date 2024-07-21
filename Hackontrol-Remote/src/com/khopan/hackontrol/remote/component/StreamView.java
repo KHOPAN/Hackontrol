e7eaeffb48fd0bc5d5b13cd45350610669df0464
KHOPAN <puthipnakchan@gmail.com>
2024-07-21 08:10:30 +0700

    Implement more efficient pixel setter

--- Hackontrol-Remote/src/com/khopan/hackontrol/remote/component/StreamView.java
+++ Hackontrol-Remote/src/com/khopan/hackontrol/remote/component/StreamView.java
@@ -5,7 +5,7 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
+import java.awt.image.DataBufferInt;
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 
@@ -31,9 +31,8 @@ public class StreamView extends Component implements PacketProcessor {
 	private int y;
 	private int sourceWidth;
 	private int sourceHeight;
-	private int[] receiveBuffer;
 	private BufferedImage sourceImage;
-	private WritableRaster raster;
+	private int[] receiveBuffer;
 
 	public StreamView() {
 		this.indexTable = new int[64];
@@ -66,60 +65,69 @@ public class StreamView extends Component implements PacketProcessor {
 		case Packet.PACKET_TYPE_SCREEN_INFORMATION:
 			this.sourceWidth = ((stream.read() & 0xFF) << 24) | ((stream.read() & 0xFF) << 16) | ((stream.read() & 0xFF) << 8) | (stream.read() & 0xFF);
 			this.sourceHeight = ((stream.read() & 0xFF) << 24) | ((stream.read() & 0xFF) << 16) | ((stream.read() & 0xFF) << 8) | (stream.read() & 0xFF);
-			this.receiveBuffer = new int[this.sourceWidth * this.sourceHeight];
 			this.sourceImage = new BufferedImage(this.sourceWidth, this.sourceHeight, BufferedImage.TYPE_INT_RGB);
-			this.raster = this.sourceImage.getRaster();
+			this.receiveBuffer = ((DataBufferInt) this.sourceImage.getRaster().getDataBuffer()).getData();
 			return true;
 		case Packet.PACKET_TYPE_STREAM_FRAME:
-			int x = ((stream.read() & 0xFF) << 24) | ((stream.read() & 0xFF) << 16) | ((stream.read() & 0xFF) << 8) | (stream.read() & 0xFF);
-			int y = ((stream.read() & 0xFF) << 24) | ((stream.read() & 0xFF) << 16) | ((stream.read() & 0xFF) << 8) | (stream.read() & 0xFF);
+			int startX = ((stream.read() & 0xFF) << 24) | ((stream.read() & 0xFF) << 16) | ((stream.read() & 0xFF) << 8) | (stream.read() & 0xFF);
+			int startY = ((stream.read() & 0xFF) << 24) | ((stream.read() & 0xFF) << 16) | ((stream.read() & 0xFF) << 8) | (stream.read() & 0xFF);
 			int width = ((stream.read() & 0xFF) << 24) | ((stream.read() & 0xFF) << 16) | ((stream.read() & 0xFF) << 8) | (stream.read() & 0xFF);
 			int height = ((stream.read() & 0xFF) << 24) | ((stream.read() & 0xFF) << 16) | ((stream.read() & 0xFF) << 8) | (stream.read() & 0xFF);
 			Arrays.fill(this.indexTable, 0);
 			int red = 0;
 			int green = 0;
 			int blue = 0;
+			int run = 0;
 
-			for(int i = 0; i < width * height; i++) {
-				int chunk = stream.read() & 0xFF;
+			for(int y = startY; y < startY + height; y++) {
+				for(int x = startX; x < startX + width; x++) {
+					int pixelIndex = y * this.sourceWidth + x;
 
-				if(chunk == StreamView.QOI_OP_RGB) {
-					red = stream.read();
-					green = stream.read();
-					blue = stream.read();
-					this.receiveBuffer[i] = this.indexTable[((red & 0xFF) * 3 + (green & 0xFF) * 5 + (blue & 0xFF) * 7 + 0xFF * 11) & 0b111111] = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
-					continue;
-				}
-
-				switch(chunk & StreamView.OP_MASK) {
-				case StreamView.QOI_OP_INDEX:
-					int index = chunk & 0b111111;
-					red = (this.indexTable[index] >> 16) & 0xFF;
-					green = (this.indexTable[index] >> 8) & 0xFF;
-					blue = this.indexTable[index] & 0xFF;
-					break;
-				case StreamView.QOI_OP_DIFF:
-					red += ((chunk >> 4) & 0b11) - 2;
-					green += ((chunk >> 2) & 0b11) - 2;
-					blue += (chunk & 0b11) - 2;
-					break;
-				case StreamView.QOI_OP_LUMA:
-					int next = stream.read() & 0xFF;
-					int differenceGreen = (chunk & 0b111111) - 32;
-					red += differenceGreen - 8 + ((next >> 4) & 0b1111);
-					green += differenceGreen;
-					blue += differenceGreen - 8 + (next & 0b1111);
-					break;
-				case StreamView.QOI_OP_RUN:
-					for(int z = 0; z < (chunk & 0b111111); z++) {
-						this.receiveBuffer[i++] = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+					if(run > 0) {
+						this.receiveBuffer[pixelIndex] = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+						run--;
+						continue;
 					}
-				}
 
-				this.receiveBuffer[i] = this.indexTable[((red & 0xFF) * 3 + (green & 0xFF) * 5 + (blue & 0xFF) * 7 + 0xFF * 11) & 0b111111] = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+					int chunk = stream.read() & 0xFF;
+
+					if(chunk == StreamView.QOI_OP_RGB) {
+						red = stream.read();
+						green = stream.read();
+						blue = stream.read();
+						this.receiveBuffer[pixelIndex] = this.indexTable[((red & 0xFF) * 3 + (green & 0xFF) * 5 + (blue & 0xFF) * 7 + 0xFF * 11) & 0b111111] = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+						continue;
+					}
+
+					switch(chunk & StreamView.OP_MASK) {
+					case StreamView.QOI_OP_INDEX:
+						int index = chunk & 0b111111;
+						red = (this.indexTable[index] >> 16) & 0xFF;
+						green = (this.indexTable[index] >> 8) & 0xFF;
+						blue = this.indexTable[index] & 0xFF;
+						break;
+					case StreamView.QOI_OP_DIFF:
+						red += ((chunk >> 4) & 0b11) - 2;
+						green += ((chunk >> 2) & 0b11) - 2;
+						blue += (chunk & 0b11) - 2;
+						break;
+					case StreamView.QOI_OP_LUMA:
+						int next = stream.read() & 0xFF;
+						int differenceGreen = (chunk & 0b111111) - 32;
+						red += differenceGreen - 8 + ((next >> 4) & 0b1111);
+						green += differenceGreen;
+						blue += differenceGreen - 8 + (next & 0b1111);
+						break;
+					case StreamView.QOI_OP_RUN:
+						this.receiveBuffer[pixelIndex] = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+						run = (chunk & 0b111111);
+						continue;
+					}
+
+					this.receiveBuffer[pixelIndex] = this.indexTable[((red & 0xFF) * 3 + (green & 0xFF) * 5 + (blue & 0xFF) * 7 + 0xFF * 11) & 0b111111] = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+				}
 			}
 
-			this.raster.setDataElements(x, y, width, height, this.receiveBuffer);
 			this.updateImage();
 			return true;
 		}
