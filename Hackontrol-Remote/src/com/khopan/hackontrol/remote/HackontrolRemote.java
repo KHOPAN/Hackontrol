ae7402e2c02cb57b9a8d5bee3dc94b247c8bc210
KHOPAN <puthipnakchan@gmail.com>
2024-07-21 11:44:12 +0700

    Add logger

--- Hackontrol-Remote/src/com/khopan/hackontrol/remote/HackontrolRemote.java
+++ Hackontrol-Remote/src/com/khopan/hackontrol/remote/HackontrolRemote.java
@@ -5,21 +5,26 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.khopan.hackontrol.remote.component.StreamView;
 import com.khopan.hackontrol.remote.network.HRSPConnection;
 import com.khopan.hackontrol.remote.network.MultiPacketProcessor;
 
 public class HackontrolRemote {
+	public static final String NAME = "Hackontrol Remote";
+	public static final Logger LOGGER = LoggerFactory.getLogger(HackontrolRemote.NAME);
+
 	private static HackontrolRemote INSTANCE;
 
 	private HackontrolRemote() {
 		HackontrolRemote.INSTANCE = this;
 		JFrame frame = new JFrame();
-		String name = "Hackontrol Remote";
-		frame.setTitle(name);
+		frame.setTitle(HackontrolRemote.NAME);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
-		StreamView streamView = new StreamView(text -> frame.setTitle(name + " " + text));
+		StreamView streamView = new StreamView(text -> frame.setTitle(HackontrolRemote.NAME + " " + text));
 		frame.add(streamView, BorderLayout.CENTER);
 		frame.setSize(600, 400);
 		frame.setLocationRelativeTo(null);
@@ -29,6 +34,7 @@ public class HackontrolRemote {
 	}
 
 	public static void main(String[] args) {
+		HackontrolRemote.LOGGER.info("Initializing");
 		HackontrolRemote.getInstance();
 	}
 
