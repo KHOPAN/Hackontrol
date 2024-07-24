6b4a77104ec69e325e82c53724899f0ea39e4aed
KHOPAN <puthipnakchan@gmail.com>
2024-07-24 17:10:35 +0700

    Implement a basic connection

--- Hackontrol/hackontrol-remote/main.c
+++ Hackontrol/hackontrol-remote/main.c
@@ -1,4 +1,7 @@
+#include <WS2tcpip.h>
 #include <khopanwin32.h>
+
+#define REMOTE_PORT "42485"
 
 int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance, _In_ LPSTR argument, _In_ int commandLineShow) {
 	WSADATA windowsSocketData;
@@ -9,6 +12,52 @@ int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance,
 		return 1;
 	}
 
+	struct addrinfo hints = {0};
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+	struct addrinfo* result;
+	status = getaddrinfo(NULL, REMOTE_PORT, &hints, &result);
+
+	if(status) {
+		KHWin32DialogErrorW(status, L"getaddrinfo");
+		goto wsaCleanup;
+	}
+
+	SOCKET serverSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+	if(serverSocket == INVALID_SOCKET) {
+		KHWin32DialogErrorW(WSAGetLastError(), L"socket");
+		freeaddrinfo(result);
+		goto wsaCleanup;
+	}
+
+	status = bind(serverSocket, result->ai_addr, (int) result->ai_addrlen);
+	freeaddrinfo(result);
+
+	if(status == SOCKET_ERROR) {
+		KHWin32DialogErrorW(WSAGetLastError(), L"bind");
+		goto closeServerSocket;
+	}
+
+	if(listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+		KHWin32DialogErrorW(WSAGetLastError(), L"listen");
+		goto closeServerSocket;
+	}
+
+	SOCKET clientSocket = accept(serverSocket, NULL, NULL);
+
+	if(clientSocket == INVALID_SOCKET) {
+		KHWin32DialogErrorW(WSAGetLastError(), L"accept");
+		goto closeServerSocket;
+	}
+
+	MessageBoxW(NULL, L"Connected", L"Hackontrol Remote", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+	closesocket(clientSocket);
+closeServerSocket:
+	closesocket(serverSocket);
+wsaCleanup:
 	if(WSACleanup() == SOCKET_ERROR) {
 		KHWin32DialogErrorW(status, L"WSACleanup");
 		return 1;
