befd77da708b51a2500882694c3bd92654469874
KHOPAN <puthipnakchan@gmail.com>
2024-07-29 17:26:23 +0700

    Add HRSP handshake

--- Hackontrol/hackontrol-remote/thread_client.c
+++ Hackontrol/hackontrol-remote/thread_client.c
@@ -13,6 +13,39 @@ DWORD WINAPI ClientThread(_In_ PCLIENT client) {
 	}
 
 	LOG("[Client Thread %ws]: Hello from client thread\n" COMMA client->address);
+	char buffer[17];
+
+	if(recv(client->socket, buffer, sizeof(buffer) - 1, 0) == SOCKET_ERROR) {
+		KHWin32DialogErrorW(WSAGetLastError(), L"recv");
+		goto exit;
+	}
+
+	buffer[16] = 0;
+
+	if(strcmp(buffer, "HRSP 1.0 CONNECT")) {
+		MessageBoxW(NULL, L"The client has requested an invalid request", L"Hackontrol Remote", MB_OK | MB_DEFBUTTON1 | MB_ICONERROR | MB_SYSTEMMODAL);
+		LOG("[Client Thread %ws]: Client has requested an invalid request: %s\n" COMMA client->address COMMA buffer);
+		goto exit;
+	}
+
+	buffer[0] = 'H';
+	buffer[1] = 'R';
+	buffer[2] = 'S';
+	buffer[3] = 'P';
+	buffer[4] = ' ';
+	buffer[5] = '1';
+	buffer[6] = '.';
+	buffer[7] = '0';
+	buffer[8] = ' ';
+	buffer[9] = 'O';
+	buffer[10] = 'K';
+
+	if(send(client->socket, buffer, 11, 0) == SOCKET_ERROR) {
+		KHWin32DialogErrorW(WSAGetLastError(), L"send");
+		goto exit;
+	}
+
+	LOG("[Client Thread %ws]: HRSP handshake completed! Receiving the first packet...\n" COMMA client->address);
 	int returnValue = 1;
 
 	if(!KHArrayAdd(&clientList, client)) {
@@ -21,7 +54,6 @@ DWORD WINAPI ClientThread(_In_ PCLIENT client) {
 	}
 
 	RefreshMainWindowListView();
-	Sleep(5000);
 
 	for(size_t i = 0; i < clientList.elementCount; i++) {
 		PCLIENT instance;
@@ -41,15 +73,14 @@ DWORD WINAPI ClientThread(_In_ PCLIENT client) {
 		}
 
 		RefreshMainWindowListView();
-		goto found;
+		returnValue = 0;
+		goto exit;
 	}
 
 	LOG("[Client Thread]: Error: Client not found in the client list\n");
-found:
-	returnValue = 0;
+exit:
 	closesocket(client->socket);
 	CloseHandle(client->thread);
-exit:
 	LOG("[Client Thread %ws]: Exiting the client thread (Exit code: %d)\n" COMMA client->address COMMA returnValue);
 	LocalFree(client);
 	return returnValue;
