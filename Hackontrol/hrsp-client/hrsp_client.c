da77e72f69371b0c12108cfc8ad09c05c0406538
KHOPAN <puthipnakchan@gmail.com>
2024-08-25 22:43:41 +0700

    Add getaddrinfo

--- Hackontrol/hrsp-client/hrsp_client.c
+++ Hackontrol/hrsp-client/hrsp_client.c
@@ -1,3 +1,4 @@
+#include <WS2tcpip.h>
 #include "hrsp_client.h"
 
 #define REMOTE_ERROR(x,y) do{if(error){error->code=y;error->function=x;}}while(0)
@@ -16,6 +17,18 @@ BOOL HRSPConnectToServer(const LPCSTR serverAddress, const LPCSTR serverPort, co
 		return FALSE;
 	}
 
+	struct addrinfo hints = {0};
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	struct addrinfo* result;
+	status = getaddrinfo(serverAddress, serverPort, &hints, &result);
+
+	if(status) {
+		REMOTE_ERROR(L"getaddrinfo", status);
+		goto cleanup;
+	}
+cleanup:
 	if(WSACleanup() == SOCKET_ERROR) {
 		REMOTE_ERROR(L"WSACleanup", WSAGetLastError());
 		return FALSE;
