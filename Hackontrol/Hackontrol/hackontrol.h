a4f9a1865381a74d438a9ed51cdb09e0c0e4968b
KHOPAN <puthipnakchan@gmail.com>
2024-08-25 10:42:19 +0700

    Remove Hackontrol tester

--- Hackontrol/Hackontrol/hackontrol.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-void startHackontrol(const char* botToken, unsigned long long userIdentifier);
-#ifdef __cplusplus
-}
-#endif
--- Hackontrol/Hackontrol/token.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-LPSTR getBotToken();
-unsigned long long getUserIdentifier();
