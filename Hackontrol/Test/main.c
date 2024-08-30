12648cf81bf4cecf4b0fd6883ed91ae33102d946
KHOPAN <puthipnakchan@gmail.com>
2024-08-30 23:51:01 +0700

    Remove test code

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <khopanwin32.h>
-//#include <ntstatus.h>
 #include <bcrypt.h>
 
 int main(int argc, char** argv) {
-	KHWIN32_ERROR(ERROR_OUTOFMEMORY, L"main");
 	BCRYPT_ALG_HANDLE handle;
-	NTSTATUS status = BCryptOpenAlgorithmProvider(&handle, L"WRONG", NULL, 0);
+	NTSTATUS status = BCryptOpenAlgorithmProvider(&handle, BCRYPT_RSA_ALGORITHM, NULL, 0);
 
 	if(!BCRYPT_SUCCESS(status)) {
-		//printf("Error: %d\n", status);BCRYPT_RSA_ALGORITHM
 		KHNTSTATUS_ERROR(status, L"BCryptOpenAlgorithmProvider");
 		return 1;
 	}
