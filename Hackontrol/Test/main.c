4769d35b6fc7c37cd6ea88e110f35f99d3da5132
KHOPAN <puthipnakchan@gmail.com>
2024-08-31 14:10:28 +0700

    Remove KHWin32GetWindowsDirectoryW test

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <khopanwin32.h>
-//#include <bcrypt.h>
+#include <bcrypt.h>
 
 int main(int argc, char** argv) {
-	LPWSTR pathFolderWindows = KHWin32GetWindowsDirectoryW();
-
-	if(!pathFolderWindows) {
-		KHWIN32_LAST_ERROR(L"KHWin32GetWindowsDirectoryW");
-		return 1;
-	}
-
-	printf("Path: %ws\n", pathFolderWindows);
-	LocalFree(pathFolderWindows);
-	/*BCRYPT_ALG_HANDLE handle;
+	BCRYPT_ALG_HANDLE handle;
 	NTSTATUS status = BCryptOpenAlgorithmProvider(&handle, BCRYPT_RSA_ALGORITHM, NULL, 0);
 
 	if(!BCRYPT_SUCCESS(status)) {
@@ -25,7 +16,7 @@ int main(int argc, char** argv) {
 	if(!BCRYPT_SUCCESS(status)) {
 		KHNTSTATUS_ERROR(status, L"BCryptCloseAlgorithmProvider");
 		return 1;
-	}*/
+	}
 
 	return 0;
 }
