2d0908211d310d9b04e8f0fba94520f08ef6b269
KHOPAN <puthipnakchan@gmail.com>
2024-08-31 22:31:20 +0700

    Update frame_decoder.c

--- Hackontrol/hackontrol-remote/frame_decoder.c
+++ Hackontrol/hackontrol-remote/frame_decoder.c
@@ -1,5 +1,4 @@
 #include "frame_decoder.h"
-#include "logger.h"
 
 #define SUBTRACT do{if(colorDifference){window->stream.pixels[pixelIndex]-=blue;window->stream.pixels[pixelIndex+1]-=green;window->stream.pixels[pixelIndex+2]-=red;}else{window->stream.pixels[pixelIndex]=blue;window->stream.pixels[pixelIndex+1]=green;window->stream.pixels[pixelIndex+2]=red;}}while(0)
 
@@ -41,30 +40,36 @@ void DecodeHRSPFrame(const BYTE* data, const size_t size, const PWINDOWDATA wind
 		goto releaseMutex;
 	}
 
-	if(boundaryDifference && colorDifference) {
-		if(size - 9 < width * height * 3) {
-			goto releaseMutex;
-		}
+	int x;
+	int y;
+	int pixelIndex;
+	int dataIndex;
 
-		for(int y = 0; y < height; y++) {
-			for(int x = 0; x < width; x++) {
-				int pixelIndex = (y * width + x) * 4;
-				int dataIndex = ((height - y - 1) * width + x) * 3;
-				window->stream.pixels[pixelIndex] = data[dataIndex + 11];
-				window->stream.pixels[pixelIndex + 1] = data[dataIndex + 10];
-				window->stream.pixels[pixelIndex + 2] = data[dataIndex + 9];
-			}
-		}
-
-		goto invalidateWindow;
+	if(!boundaryDifference || !colorDifference) {
+		goto exitRawPixel;
 	}
 
+	if(size - 9 < width * height * 3) {
+		goto releaseMutex;
+	}
+
+	for(y = 0; y < height; y++) {
+		for(x = 0; x < width; x++) {
+			pixelIndex = (y * width + x) * 4;
+			dataIndex = ((height - y - 1) * width + x) * 3;
+			window->stream.pixels[pixelIndex] = data[dataIndex + 11];
+			window->stream.pixels[pixelIndex + 1] = data[dataIndex + 10];
+			window->stream.pixels[pixelIndex + 2] = data[dataIndex + 9];
+		}
+	}
+
+	goto invalidateWindow;
+exitRawPixel:
 	if(boundaryDifference && size - 9 < 16) {
 		goto releaseMutex;
 	}
 
 	int startX = boundaryDifference ? (data[9] << 24) | (data[10] << 16) | (data[11] << 8) | data[12] : 0;
-	int startY = boundaryDifference ? (data[13] << 24) | (data[14] << 16) | (data[15] << 8) | data[16] : 0;
 	int endX = boundaryDifference ? (data[17] << 24) | (data[18] << 16) | (data[19] << 8) | data[20] : width - 1;
 	int endY = boundaryDifference ? (data[21] << 24) | (data[22] << 16) | (data[23] << 8) | data[24] : height - 1;
 	BYTE seenRed[64];
@@ -73,15 +78,15 @@ void DecodeHRSPFrame(const BYTE* data, const size_t size, const PWINDOWDATA wind
 	memset(seenRed, 0, sizeof(seenRed));
 	memset(seenGreen, 0, sizeof(seenGreen));
 	memset(seenBlue, 0, sizeof(seenBlue));
+	size_t pointer = boundaryDifference ? 25 : 9;
 	int red = 0;
 	int green = 0;
 	int blue = 0;
 	int run = 0;
-	size_t pointer = boundaryDifference ? 25 : 9;
 
-	for(int y = startY; y <= endY; y++) {
-		for(int x = startX; x <= endX; x++) {
-			int pixelIndex = ((height - y - 1) * width + x) * 4;
+	for(y = boundaryDifference ? (data[13] << 24) | (data[14] << 16) | (data[15] << 8) | data[16] : 0; y <= endY; y++) {
+		for(x = startX; x <= endX; x++) {
+			pixelIndex = ((height - y - 1) * width + x) * 4;
 
 			if(run > 0) {
 				SUBTRACT;
@@ -93,53 +98,52 @@ void DecodeHRSPFrame(const BYTE* data, const size_t size, const PWINDOWDATA wind
 				goto releaseMutex;
 			}
 
-			BYTE chunk = data[pointer++];
-			int index;
+			startX = data[pointer++];
 
-			if(chunk == QOI_OP_RGB) {
+			if(startX == QOI_OP_RGB) {
 				if(size - pointer < 3) goto releaseMutex;
 				red = data[pointer++];
 				green = data[pointer++];
 				blue = data[pointer++];
-				index = (red * 3 + green * 5 + blue * 7 + 0xFF * 11) & 0b111111;
-				seenRed[index] = red;
-				seenGreen[index] = green;
-				seenBlue[index] = blue;
+				dataIndex = (red * 3 + green * 5 + blue * 7 + 0xFF * 11) & 0b111111;
+				seenRed[dataIndex] = red;
+				seenGreen[dataIndex] = green;
+				seenBlue[dataIndex] = blue;
 				SUBTRACT;
 				continue;
 			}
 
-			switch(chunk & OP_MASK) {
+			switch(startX & OP_MASK) {
 			case QOI_OP_INDEX: {
-				index = chunk & 0b111111;
-				red = seenRed[index];
-				green = seenGreen[index];
-				blue = seenBlue[index];
+				dataIndex = startX & 0b111111;
+				red = seenRed[dataIndex];
+				green = seenGreen[dataIndex];
+				blue = seenBlue[dataIndex];
 				break;
 			}
 			case QOI_OP_DIFF:
-				red += ((chunk >> 4) & 0b11) - 2;
-				green += ((chunk >> 2) & 0b11) - 2;
-				blue += (chunk & 0b11) - 2;
+				red += ((startX >> 4) & 0b11) - 2;
+				green += ((startX >> 2) & 0b11) - 2;
+				blue += (startX & 0b11) - 2;
 				break;
 			case QOI_OP_LUMA:
 				if(size - pointer < 1) goto releaseMutex;
-				index = data[pointer++];
-				int differenceGreen = (chunk & 0b111111) - 32;
-				red += differenceGreen - 8 + ((index >> 4) & 0b1111);
-				green += differenceGreen;
-				blue += differenceGreen - 8 + (index & 0b1111);
+				dataIndex = data[pointer++];
+				startX = (startX & 0b111111) - 32;
+				red += startX - 8 + ((dataIndex >> 4) & 0b1111);
+				green += startX;
+				blue += startX - 8 + (dataIndex & 0b1111);
 				break;
 			case QOI_OP_RUN:
 				SUBTRACT;
-				run = (chunk & 0b111111);
+				run = (startX & 0b111111);
 				continue;
 			}
 
-			index = (red * 3 + green * 5 + blue * 7 + 0xFF * 11) & 0b111111;
-			seenRed[index] = red;
-			seenGreen[index] = green;
-			seenBlue[index] = blue;
+			dataIndex = (red * 3 + green * 5 + blue * 7 + 0xFF * 11) & 0b111111;
+			seenRed[dataIndex] = red;
+			seenGreen[dataIndex] = green;
+			seenBlue[dataIndex] = blue;
 			SUBTRACT;
 		}
 	}
