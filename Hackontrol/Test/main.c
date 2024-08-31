163ac081bb2a4923e8b3fd9d23faeb2bcc70abc6
KHOPAN <puthipnakchan@gmail.com>
2024-08-31 15:01:50 +0700

    Getting the encrypted size

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -3,6 +3,7 @@
 #include <bcrypt.h>
 
 int main(int argc, char** argv) {
+	BYTE inputData[] = "Hello, world! This is example text";
 	BCRYPT_ALG_HANDLE algorithm;
 	NTSTATUS status = BCryptOpenAlgorithmProvider(&algorithm, BCRYPT_RSA_ALGORITHM, NULL, 0);
 
@@ -13,19 +14,38 @@ int main(int argc, char** argv) {
 
 	BCRYPT_KEY_HANDLE key;
 	status = BCryptGenerateKeyPair(algorithm, &key, 2048, 0);
+	int returnValue = 1;
 
 	if(!BCRYPT_SUCCESS(status)) {
 		KHNTSTATUS_ERROR_CONSOLE(status, L"BCryptGenerateKeyPair");
-		return 1;
+		goto closeAlgorithmProvider;
 	}
 
+	status = BCryptFinalizeKeyPair(key, 0);
+
+	if(!BCRYPT_SUCCESS(status)) {
+		KHNTSTATUS_ERROR_CONSOLE(status, L"BCryptFinalizeKeyPair");
+		goto destroyKey;
+	}
+
+	ULONG encryptedSize;
+	status = BCryptEncrypt(key, inputData, sizeof(inputData) - 1, NULL, NULL, 0, NULL, 0, &encryptedSize, 0);
+
+	if(!BCRYPT_SUCCESS(status)) {
+		KHNTSTATUS_ERROR_CONSOLE(status, L"BCryptEncrypt");
+		goto destroyKey;
+	}
+
+	printf("Encrypted size: %lu\n", encryptedSize);
+	returnValue = 0;
+destroyKey:
 	status = BCryptDestroyKey(key);
 
 	if(!BCRYPT_SUCCESS(status)) {
 		KHNTSTATUS_ERROR_CONSOLE(status, L"BCryptDestroyKey");
-		return 1;
+		returnValue = 1;
 	}
-
+closeAlgorithmProvider:
 	status = BCryptCloseAlgorithmProvider(algorithm, 0);
 
 	if(!BCRYPT_SUCCESS(status)) {
@@ -33,5 +53,5 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	return 0;
+	return returnValue;
 }
