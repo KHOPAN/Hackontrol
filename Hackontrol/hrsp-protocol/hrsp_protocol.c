e1f8ee3c6d96e1b7c09c6eba2c420c8f93498330
KHOPAN <puthipnakchan@gmail.com>
2024-09-08 00:22:03 +0700

    Add HRSP handshake functions

---
+++ Hackontrol/hrsp-protocol/hrsp_protocol.c
@@ -0,0 +1,1 @@
+#include "hrsp_protocol.h"
--- Hackontrol/hrsp-protocol/hrsp_protocol.h
+++ Hackontrol/hrsp-protocol/hrsp_protocol.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void HRSPClientHandshake();
+void HRSPServerHandshake();
+
+#ifdef __cplusplus
+}
+#endif
