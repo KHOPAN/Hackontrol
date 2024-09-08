f750966826585d88ac10e532cfe47ac62bea7e11
KHOPAN <puthipnakchan@gmail.com>
2024-09-08 23:53:06 +0700

    Update remote client

--- Hackontrol/remote-client/main.c
+++ Hackontrol/remote-client/main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#include <khopanwin32.h>
-#include "hrsp_client.h"
+#include <hrsp_client.h>
 
 int main(int argc, char** argv) {
-	HRSPCLIENTSTRUCT client = {0};
 	HRSPCLIENTERROR error;
 
-	if(!HRSPConnectToServer("localhost", "42485", &client, &error)) {
-		if(error.remoteError) {
-			printf("Custom Error: %d\n", error.codeRemote);
-			return 1;
-		}
-
-		KHWIN32_ERROR_CONSOLE(error.code, error.function);
+	if(!HRSPClientConnectToServer("localhost", "42485", &error)) {
+		printf("Function: %ws Error code: %u\n", error.function, error.code);
 		return 1;
 	}
 
