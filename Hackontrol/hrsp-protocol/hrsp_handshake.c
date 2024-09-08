5d3f50cc74266276008ada1ab55a1dd3ec136500
KHOPAN <puthipnakchan@gmail.com>
2024-09-08 12:17:14 +0700

    Add error struct

--- Hackontrol/hrsp-protocol/hrsp_handshake.c
+++ Hackontrol/hrsp-protocol/hrsp_handshake.c
@@ -1,9 +1,22 @@
 #include "hrsp_protocol.h"
 
-BOOL HRSPClientHandshake() {
-	return FALSE;
+#define SETERROR(x) if(error){error->errorCode=1;}
+
+BOOL HRSPClientHandshake(const SOCKET socket, const PHRSPPROTOCOLERROR error) {
+	if(!socket) {
+		if(error) {
+			error->win32Error = FALSE;
+			error->functionName = L"HRSPClientHandshake";
+			error->errorCode = HRSP_ERROR_INVALID_FUNCTION_PARAMETER;
+			error->win32ErrorCode = 0;
+		}
+
+		return FALSE;
+	}
+
+	return TRUE;
 }
 
-BOOL HRSPServerHandshake() {
+BOOL HRSPServerHandshake(const SOCKET socket, const PHRSPPROTOCOLERROR error) {
 	return FALSE;
 }
--- Hackontrol/hrsp-protocol/hrsp_protocol.h
+++ Hackontrol/hrsp-protocol/hrsp_protocol.h
@@ -2,12 +2,23 @@
 
 #include <Windows.h>
 
+typedef enum {
+	HRSP_ERROR_INVALID_FUNCTION_PARAMETER = 1
+} HRSPPROTOCOLERRORCODE;
+
+typedef struct {
+	BOOL win32Error;
+	LPCWSTR functionName;
+	HRSPPROTOCOLERRORCODE errorCode;
+	DWORD win32ErrorCode;
+} HRSPPROTOCOLERROR, *PHRSPPROTOCOLERROR;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-BOOL HRSPClientHandshake();
-BOOL HRSPServerHandshake();
+BOOL HRSPClientHandshake(const SOCKET socket, const PHRSPPROTOCOLERROR error);
+BOOL HRSPServerHandshake(const SOCKET socket, const PHRSPPROTOCOLERROR error);
 
 #ifdef __cplusplus
 }
