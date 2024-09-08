9a29f9adc6b6bbf931b3e2e1e56d9235268e7370
KHOPAN <puthipnakchan@gmail.com>
2024-09-08 12:44:42 +0700

    Add send call

--- Hackontrol/hrsp-protocol/error_code.c
+++ Hackontrol/hrsp-protocol/error_code.c
@@ -2,6 +2,7 @@
 
 LPCWSTR HRSPGetErrorCode(const HRSPPROTOCOLERRORCODE code) {
 	switch(code) {
+	case HRSP_ERROR_SUCCESS:                    return L"An operation succeeded with no error";
 	case HRSP_ERROR_INVALID_FUNCTION_PARAMETER: return L"Invalid function parameter";
 	}
 
--- Hackontrol/hrsp-protocol/hrsp_handshake.c
+++ Hackontrol/hrsp-protocol/hrsp_handshake.c
@@ -1,10 +1,16 @@
 #include "hrsp_protocol.h"
 
 #define SETERROR_HRSP(function, code) if(error){error->win32Error=FALSE;error->functionName=function;error->errorCode=code;error->win32ErrorCode=0;}
+#define SETERROR_WIN32(function, code) if(error){error->win32Error=TRUE;error->functionName=function;error->errorCode=HRSP_ERROR_SUCCESS;error->win32ErrorCode=code;}
 
 BOOL HRSPClientHandshake(const SOCKET socket, const PHRSPPROTOCOLERROR error) {
 	if(!socket) {
 		SETERROR_HRSP(L"HRSPClientHandshake", HRSP_ERROR_INVALID_FUNCTION_PARAMETER);
+		return FALSE;
+	}
+
+	if(send(socket, NULL, 0, 0) == SOCKET_ERROR) {
+		SETERROR_WIN32(L"send", WSAGetLastError());
 		return FALSE;
 	}
 
