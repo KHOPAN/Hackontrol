87cf94ed2a769aedb44925538bd4a602818e2b9a
KHOPAN <puthipnakchan@gmail.com>
2024-09-11 07:03:06 +0700

    Update hrsp_packet.c

--- Hackontrol/hrsp-protocol/hrsp_packet.c
+++ Hackontrol/hrsp-protocol/hrsp_packet.c
@@ -1,20 +1,20 @@
-#include "hrsp_protocol.h"
+#include "hrsp_packet.h"
 
-#define SETERROR_HRSP(functionName, errorCode) if(error){error->win32=FALSE;error->function=functionName;error->code=errorCode;}
-#define SETERROR_WIN32(functionName, errorCode) if(error){error->win32=TRUE;error->function=functionName;error->code=errorCode;}
+#define ERROR_HRSP(functionName, errorCode) if(error){error->win32=FALSE;error->function=functionName;error->code=errorCode;}
+#define ERROR_WIN32(functionName, errorCode) if(error){error->win32=TRUE;error->function=functionName;error->code=errorCode;}
 
-BOOL HRSPSendPacket(const SOCKET socket, const PHRSPPROTOCOLDATA data, const PHRSPPROTOCOLPACKET packet, const PHRSPPROTOCOLERROR error) {
+BOOL HRSPSendPacket(const SOCKET socket, const PHRSPDATA data, const PHRSPPACKET packet, const PHRSPERROR error) {
 	if(!socket || !data || !packet) {
-		SETERROR_HRSP(L"HRSPSendPacket", HRSP_ERROR_INVALID_FUNCTION_PARAMETER);
+		ERROR_HRSP(L"HRSPSendPacket", HRSP_ERROR_INVALID_FUNCTION_PARAMETER);
 		return FALSE;
 	}
 
 	return TRUE;
 }
 
-BOOL HRSPReceivePacket(const SOCKET socket, const PHRSPPROTOCOLDATA data, const PHRSPPROTOCOLPACKET packet, const PHRSPPROTOCOLERROR error) {
+BOOL HRSPReceivePacket(const SOCKET socket, const PHRSPDATA data, const PHRSPPACKET packet, const PHRSPERROR error) {
 	if(!socket || !data || !packet) {
-		SETERROR_HRSP(L"HRSPReceivePacket", HRSP_ERROR_INVALID_FUNCTION_PARAMETER);
+		ERROR_HRSP(L"HRSPReceivePacket", HRSP_ERROR_INVALID_FUNCTION_PARAMETER);
 		return FALSE;
 	}
 
