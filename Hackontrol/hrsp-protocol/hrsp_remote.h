30b512a2e25715770f07e8d88f8ed95d86296ec7
KHOPAN <puthipnakchan@gmail.com>
2024-09-11 21:22:51 +0700

    Add shared terminate packet

--- Hackontrol/hrsp-protocol/hrsp_remote.h
+++ Hackontrol/hrsp-protocol/hrsp_remote.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#define HRSP_REMOTE_TERMINATE_PACKET 0x01
+
 // Client -> Server
-#define HRSP_REMOTE_CLIENT_INFORMATION_PACKET 0x01
+#define HRSP_REMOTE_CLIENT_INFORMATION_PACKET 0x02
 
 // Server -> Client
