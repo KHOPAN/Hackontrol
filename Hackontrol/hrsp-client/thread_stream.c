20bf8c4d7ce3aeec55a335e3354425c067da8a77
KHOPAN <puthipnakchan@gmail.com>
2024-09-14 20:05:34 +0700

    Allocate buffers for streaming

--- Hackontrol/hrsp-client/thread_stream.c
+++ Hackontrol/hrsp-client/thread_stream.c
@@ -7,9 +7,16 @@ DWORD WINAPI HRSPClientStreamThread(_In_ PHRSPCLIENTSTREAMPARAMETER parameter) {
 		return 1;
 	}
 
+	UINT width;
+	UINT height;
+	UINT oldWidth = 0;
+	UINT oldHeight = 0;
+	ULONG baseSize;
+	ULONG bufferSize;
 	PBYTE screenshotBuffer = NULL;
 	PBYTE qoiBuffer = NULL;
 	PBYTE previousBuffer = NULL;
+	BOOL hasError = FALSE;
 	DWORD returnValue = 1;
 
 	while(parameter->running) {
@@ -21,11 +28,76 @@ DWORD WINAPI HRSPClientStreamThread(_In_ PHRSPCLIENTSTREAMPARAMETER parameter) {
 		if(!(parameter->sensitive.flags & 1)) {
 			goto releaseMutex;
 		}
+
+		width = GetSystemMetrics(SM_CXSCREEN);
+		height = GetSystemMetrics(SM_CYSCREEN);
+
+		if(width == oldWidth && height == oldHeight) {
+			goto sameSize;
+		}
+
+		oldWidth = width;
+		oldHeight = height;
+
+		if(previousBuffer && LocalFree(previousBuffer)) {
+			ERROR_WIN32(L"LocalFree", GetLastError());
+			previousBuffer = NULL;
+			hasError = TRUE;
+			goto releaseMutex;
+		}
+
+		previousBuffer = NULL;
+
+		if(qoiBuffer && LocalFree(qoiBuffer)) {
+			ERROR_WIN32(L"LocalFree", GetLastError());
+			qoiBuffer = NULL;
+			hasError = TRUE;
+			goto releaseMutex;
+		}
+
+		qoiBuffer = NULL;
+
+		if(screenshotBuffer && LocalFree(screenshotBuffer)) {
+			ERROR_WIN32(L"LocalFree", GetLastError());
+			screenshotBuffer = NULL;
+			hasError = TRUE;
+			goto releaseMutex;
+		}
+
+		screenshotBuffer = NULL;
+		baseSize = width * height;
+		bufferSize = baseSize * 4;
+		screenshotBuffer = LocalAlloc(LMEM_FIXED, bufferSize);
+
+		if(!screenshotBuffer) {
+			ERROR_WIN32(L"LocalAlloc", GetLastError());
+			hasError = TRUE;
+			goto releaseMutex;
+		}
+
+		qoiBuffer = LocalAlloc(LMEM_FIXED, bufferSize);
+
+		if(!qoiBuffer) {
+			ERROR_WIN32(L"LocalAlloc", GetLastError());
+			hasError = TRUE;
+			goto releaseMutex;
+		}
+
+		previousBuffer = LocalAlloc(LMEM_FIXED, baseSize * 3);
+
+		if(!previousBuffer) {
+			ERROR_WIN32(L"LocalAlloc", GetLastError());
+			hasError = TRUE;
+			goto releaseMutex;
+		}
+	sameSize:
 	releaseMutex:
 		if(!ReleaseMutex(parameter->sensitive.mutex)) {
 			ERROR_WIN32(L"ReleaseMutex", GetLastError());
 			goto freeBuffers;
 		}
+
+		if(hasError) goto freeBuffers;
 	}
 
 	/*int width = GetSystemMetrics(SM_CXSCREEN);
