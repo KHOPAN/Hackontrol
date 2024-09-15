c22d36cd5b85c35825d335cc1457bad6241f0da2
KHOPAN <puthipnakchan@gmail.com>
2024-09-15 20:51:53 +0700

    Test connected callback

--- Hackontrol/remote-client/main.c
+++ Hackontrol/remote-client/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <hrsp_client.h>
 
+static void __stdcall connected(LPVOID parameter) {
+	printf("Connected\n");
+}
+
 int main(int argc, char** argv) {
+	HRSPCLIENTINPUT input = {0};
+	input.callbackConnected = connected;
 	HRSPCLIENTERROR error;
 
-	if(!HRSPClientConnectToServer(NULL, NULL, &error)) {
+	if(!HRSPClientConnectToServer(NULL, NULL, &input, &error)) {
 		LPWSTR message = HRSPClientGetErrorMessage(L"HRSPClientConnectToServer", &error);
 
 		if(message) {
