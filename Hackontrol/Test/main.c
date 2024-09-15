d49ffd471bd19ed6e4aab55b4d6124f31fb6a64a
KHOPAN <puthipnakchan@gmail.com>
2024-09-16 00:17:05 +0700

    Add GetAdapterDisplayMode call

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <d3d9.h>
 #include <khopanwin32.h>
 
@@ -9,6 +10,18 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	D3DDISPLAYMODE mode;
+	HRESULT result = direct->lpVtbl->GetAdapterDisplayMode(direct, D3DADAPTER_DEFAULT, &mode);
+	int returnValue = 1;
+
+	if(FAILED(result)) {
+		KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3D9::GetAdapterDisplayMode");
+		goto releaseDirect;
+	}
+
+	printf("Width: %u Height: %u\n", mode.Width, mode.Height);
+	returnValue = 0;
+releaseDirect:
 	direct->lpVtbl->Release(direct);
-	return 0;
+	return returnValue;
 }
