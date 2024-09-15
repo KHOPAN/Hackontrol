0c0d054bf651b3b34d82ed30ef29a196658700b5
KHOPAN <puthipnakchan@gmail.com>
2024-09-16 06:28:16 +0700

    Add CreateDevice call

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -20,6 +20,21 @@ int main(int argc, char** argv) {
 	}
 
 	printf("Width: %u Height: %u\n", mode.Width, mode.Height);
+	D3DPRESENT_PARAMETERS parameters = {0};
+	parameters.Windowed = TRUE;
+	parameters.BackBufferCount = 1;
+	parameters.BackBufferWidth = mode.Width;
+	parameters.BackBufferHeight = mode.Height;
+	parameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	IDirect3DDevice9* device;
+	result = direct->lpVtbl->CreateDevice(direct, D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, NULL, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &parameters, &device);
+
+	if(FAILED(result)) {
+		KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3D9::CreateDevice");
+		goto releaseDirect;
+	}
+
+	device->lpVtbl->Release(device);
 	returnValue = 0;
 releaseDirect:
 	direct->lpVtbl->Release(direct);
