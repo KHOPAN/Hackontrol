2d6fa3c7eee457d8bddd89bedb74ba228190637f
KHOPAN <puthipnakchan@gmail.com>
2024-09-15 07:50:47 +0700

    Add QOI encoding

--- Hackontrol/hrsp-client/thread_stream.c
+++ Hackontrol/hrsp-client/thread_stream.c
@@ -13,7 +13,7 @@
 #pragma warning(disable: 6385)
 #pragma warning(disable: 6386)
 
-static BOOL sendFrame(const PHRSPCLIENTSTREAMPARAMETER parameter, const int width, const int height, const PBYTE screenshotBuffer, const PBYTE qoiBuffer, const PBYTE previousBuffer) {
+static BOOL sendFramsse(const PHRSPCLIENTSTREAMPARAMETER parameter, const int width, const int height, const PBYTE screenshotBuffer, const PBYTE qoiBuffer, const PBYTE previousBuffer) {
 	HRSPPACKET packet;
 	BOOL boundaryDifference = (parameter->sensitive.flags >> 1) & 1;
 	BOOL colorDifference = (parameter->sensitive.flags >> 2) & 1;
@@ -213,6 +213,102 @@ cleanup:
 	return returnValue;
 }
 
+static void qoiEncode(const UINT startX, const UINT startY, const UINT endX, const UINT endY, const UINT width, const UINT height, const PBYTE buffer, const PBYTE previousBuffer, size_t* const pointer, const BOOL colorDifference) {
+	BYTE seenRed[64];
+	BYTE seenGreen[64];
+	BYTE seenBlue[64];
+	UINT x;
+
+	for(x = 0; x < 64; x++) {
+		seenRed[x] = 0;
+		seenGreen[x] = 0;
+		seenBlue[x] = 0;
+	}
+
+	BYTE previousRed = 0;
+	BYTE previousGreen = 0;
+	BYTE previousBlue = 0;
+	BYTE runLength = 0;
+
+	for(UINT y = startY; y <= endY; y++) {
+		for(x = startX; x <= endX; x++) {
+			ULONG baseIndex = (height - y - 1) * width + x;
+			ULONG screenshotIndex = baseIndex * 4;
+			ULONG previousIndex = baseIndex * 3;
+			BYTE red = buffer[screenshotIndex + 2];
+			BYTE green = buffer[screenshotIndex + 1];
+			BYTE blue = buffer[screenshotIndex];
+
+			if(colorDifference) {
+				red = previousBuffer[previousIndex] - buffer[screenshotIndex + 2];
+				green = previousBuffer[previousIndex + 1] - buffer[screenshotIndex + 1];
+				blue = previousBuffer[previousIndex + 2] - buffer[screenshotIndex];
+				previousBuffer[previousIndex] = buffer[screenshotIndex + 2];
+				previousBuffer[previousIndex + 1] = buffer[screenshotIndex + 1];
+				previousBuffer[previousIndex + 2] = buffer[screenshotIndex];
+			}
+
+			if(red == previousRed && green == previousGreen && blue == previousBlue) {
+				runLength++;
+
+				if(runLength == 62) {
+					buffer[(*pointer)++] = QOI_OP_RUN | (runLength - 1);
+					runLength = 0;
+				}
+
+				continue;
+			}
+
+			if(runLength > 0) {
+				buffer[(*pointer)++] = QOI_OP_RUN | ((runLength - 1) & 0b111111);
+				runLength = 0;
+			}
+
+			BYTE indexPosition = (red * 3 + green * 5 + blue * 7 + 0xFF * 11) & 0b111111;
+
+			if(red == seenRed[indexPosition] && green == seenGreen[indexPosition] && blue == seenBlue[indexPosition]) {
+				buffer[(*pointer)++] = QOI_OP_INDEX | indexPosition;
+				previousRed = red;
+				previousGreen = green;
+				previousBlue = blue;
+				continue;
+			}
+
+			seenRed[indexPosition] = red;
+			seenGreen[indexPosition] = green;
+			seenBlue[indexPosition] = blue;
+			char differenceRed = red - previousRed;
+			char differenceGreen = green - previousGreen;
+			char differenceBlue = blue - previousBlue;
+
+			if(differenceRed > -3 && differenceRed < 2 && differenceGreen > -3 && differenceGreen < 2 && differenceBlue > -3 && differenceBlue < 2) {
+				buffer[(*pointer)++] = QOI_OP_DIFF | ((differenceRed + 2) << 4) | ((differenceGreen + 2) << 2) | (differenceBlue + 2);
+			} else {
+				char relativeRed = differenceRed - differenceGreen;
+				char relativeBlue = differenceBlue - differenceGreen;
+
+				if(relativeRed > -9 && relativeRed < 8 && differenceGreen > -33 && differenceGreen < 32 && relativeBlue > -9 && relativeBlue < 8) {
+					buffer[(*pointer)++] = QOI_OP_LUMA | (differenceGreen + 32);
+					buffer[(*pointer)++] = ((relativeRed + 8) << 4) | (relativeBlue + 8);
+				} else {
+					buffer[(*pointer)++] = QOI_OP_RGB;
+					buffer[(*pointer)++] = red;
+					buffer[(*pointer)++] = green;
+					buffer[(*pointer)++] = blue;
+				}
+			}
+
+			previousRed = red;
+			previousGreen = green;
+			previousBlue = blue;
+		}
+	}
+
+	if(runLength > 0) {
+		buffer[(*pointer)++] = QOI_OP_RUN | ((runLength - 1) & 0b111111);
+	}
+}
+
 DWORD WINAPI HRSPClientStreamThread(_In_ PHRSPCLIENTSTREAMPARAMETER parameter) {
 	if(!parameter) {
 		return 1;
@@ -221,6 +317,7 @@ DWORD WINAPI HRSPClientStreamThread(_In_ PHRSPCLIENTSTREAMPARAMETER parameter) {
 	UINT oldWidth = 0;
 	UINT oldHeight = 0;
 	size_t offsetEncoded = 0;
+	size_t offsetPrevious = 0;
 	PBYTE buffer = NULL;
 	HDC context;
 
@@ -265,7 +362,8 @@ DWORD WINAPI HRSPClientStreamThread(_In_ PHRSPCLIENTSTREAMPARAMETER parameter) {
 		}
 
 		offsetEncoded = width * height * 4;
-		buffer = LocalAlloc(LMEM_FIXED, offsetEncoded * 2);
+		offsetPrevious = offsetEncoded * 2;
+		buffer = LocalAlloc(LMEM_FIXED, width * height * 11);
 
 		if(!buffer) {
 			ERROR_WIN32(L"LocalAlloc", GetLastError());
@@ -296,7 +394,7 @@ DWORD WINAPI HRSPClientStreamThread(_In_ PHRSPCLIENTSTREAMPARAMETER parameter) {
 		DeleteDC(memoryContext);
 		ReleaseDC(NULL, context);
 		size_t pointer = offsetEncoded;
-		buffer[pointer++] = 0b11;// ((colorDifference & 1) << 1) | (boundaryDifference & 1);
+		buffer[pointer++] = ((colorDifference & 1) << 1) | (boundaryDifference & 1);
 		buffer[pointer++] = (width >> 24) & 0xFF;
 		buffer[pointer++] = (width >> 16) & 0xFF;
 		buffer[pointer++] = (width >> 8) & 0xFF;
@@ -305,19 +403,27 @@ DWORD WINAPI HRSPClientStreamThread(_In_ PHRSPCLIENTSTREAMPARAMETER parameter) {
 		buffer[pointer++] = (height >> 16) & 0xFF;
 		buffer[pointer++] = (height >> 8) & 0xFF;
 		buffer[pointer++] = height & 0xFF;
+		PBYTE previousBuffer = buffer + offsetPrevious;
+		HRSPPACKET packet;
 
+		if(colorDifference && boundaryDifference) {
+			goto formatRaw;
+		}
+
+		qoiEncode(boundaryDifference ? width - 1 : 0, boundaryDifference ? height - 1 : 0, boundaryDifference ? 0 : width - 1, boundaryDifference ? 0 : height - 1, width, height, buffer, previousBuffer, &pointer, colorDifference);
+		goto sendFrame;
+	formatRaw:
 		for(UINT y = 0; y < height; y++) {
 			for(UINT x = 0; x < width; x++) {
 				ULONG baseIndex = (height - y - 1) * width + x;
 				ULONG screenshotIndex = baseIndex * 4;
 				ULONG previousIndex = baseIndex * 3;
-				buffer[pointer++] = /*previousBuffer[previousIndex] =*/ buffer[screenshotIndex + 2];
-				buffer[pointer++] = /*previousBuffer[previousIndex + 1] =*/ buffer[screenshotIndex + 1];
-				buffer[pointer++] = /*previousBuffer[previousIndex + 2] =*/ buffer[screenshotIndex];
+				buffer[pointer++] = previousBuffer[previousIndex] = buffer[screenshotIndex + 2];
+				buffer[pointer++] = previousBuffer[previousIndex + 1] = buffer[screenshotIndex + 1];
+				buffer[pointer++] = previousBuffer[previousIndex + 2] = buffer[screenshotIndex];
 			}
 		}
-
-		HRSPPACKET packet;
+	sendFrame:
 		packet.size = (int) (pointer - offsetEncoded);
 		packet.type = HRSP_REMOTE_CLIENT_STREAM_FRAME_PACKET;
 		packet.data = buffer + offsetEncoded;
