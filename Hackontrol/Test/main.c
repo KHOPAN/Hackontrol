7db24c10a1c6ed438d670bdc442c09beb4d8996e
KHOPAN <puthipnakchan@gmail.com>
2024-09-15 21:39:58 +0700

    Test Direct3D

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,3 +1,14 @@
+#include <d3d9.h>
+#include <khopanwin32.h>
+
 int main(int argc, char** argv) {
+	IDirect3D9* direct = Direct3DCreate9(D3D_SDK_VERSION);
+
+	if(!direct) {
+		KHWIN32_ERROR_CONSOLE(ERROR_FUNCTION_FAILED, L"Direct3DCreate9");
+		return 1;
+	}
+
+	direct->lpVtbl->Release(direct);
 	return 0;
 }
