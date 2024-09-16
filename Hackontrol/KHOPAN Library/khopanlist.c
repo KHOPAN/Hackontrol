7da15e5680ac6320d7fe5edb40ee645ea668f7b6
KHOPAN <puthipnakchan@gmail.com>
2024-09-17 06:54:48 +0700

    Implement parameter checker

--- Hackontrol/KHOPAN Library/khopanlist.c
+++ Hackontrol/KHOPAN Library/khopanlist.c
@@ -1,29 +1,79 @@
 #include "khopanlist.h"
 
 BOOL KHOPANArrayInitialize(const PARRAYLIST list, size_t size) {
+	if(!list || !size) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return FALSE;
+	}
+
+	SetLastError(ERROR_SUCCESS);
 	return TRUE;
 }
 
 BOOL KHOPANArrayAdd(const PARRAYLIST list, const PBYTE data) {
+	if(!list || !data) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return FALSE;
+	}
+
+	SetLastError(ERROR_SUCCESS);
 	return TRUE;
 }
 
 BOOL KHOPANArrayRemove(const PARRAYLIST list, const size_t index) {
+	if(!list) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return FALSE;
+	}
+
+	SetLastError(ERROR_SUCCESS);
 	return TRUE;
 }
 
 BOOL KHOPANArrayGet(const PARRAYLIST list, const size_t index, const PBYTE* data) {
+	if(!data) {
+		goto success;
+	}
+
+	if(!list) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return FALSE;
+	}
+success:
+	SetLastError(ERROR_SUCCESS);
 	return TRUE;
 }
 
 BOOL KHOPANArrayFree(const PARRAYLIST list) {
+	if(!list) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return FALSE;
+	}
+
+	SetLastError(ERROR_SUCCESS);
 	return TRUE;
 }
 
 BOOL KHOPANStreamAdd(const PDATASTREAM stream, const PBYTE data, const size_t size) {
+	if(!data || !size) {
+		goto success;
+	}
+
+	if(!stream) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return FALSE;
+	}
+success:
+	SetLastError(ERROR_SUCCESS);
 	return TRUE;
 }
 
 BOOL KHOPANStreamFree(const PDATASTREAM stream) {
+	if(!stream) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return FALSE;
+	}
+
+	SetLastError(ERROR_SUCCESS);
 	return TRUE;
 }
