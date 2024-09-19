5ec43ec1b5f20b2aa9b23da2e059bdd1e94060cf
KHOPAN <puthipnakchan@gmail.com>
2024-09-20 00:07:29 +0700

    Remove test code

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,31 +1,3 @@
-#include <libkhopanlist.h>
-#include <libkhopan.h>
-#include <libkhopancurl.h>
-#include <libkhopanjava.h>
-
 int main(int argc, char** argv) {
-	/*KHOPANArrayInitialize(NULL, 0);
-	KHOPANArrayAdd(NULL, NULL);
-	KHOPANArrayRemove(NULL, 0);
-	KHOPANArrayGet(NULL, 0, NULL);
-	KHOPANArrayFree(NULL);
-	KHOPANStreamAdd(NULL, NULL, 0);
-	KHOPANStreamFree(NULL);
-	KHOPANEnablePrivilege(NULL);
-	KHOPANExecuteCommand(NULL, FALSE);
-	KHOPANExecuteDynamicLibrary(NULL, NULL, NULL);
-	KHOPANExecuteProcess(NULL, NULL, FALSE);
-	KHOPANExecuteRundll32Function(NULL, NULL, NULL, FALSE);
-	KHOPANFileGetCmd();
-	KHOPANFileGetRundll32();
-	KHOPANFolderGetWindows();
-	KHOPANFormatMessage(NULL);
-	KHOPANInternalGetErrorMessage(0, NULL, FALSE);
-	KHOPANStringDuplicate(NULL);
-	KHOPANCURLInternalGetErrorMessage(0, NULL);
-	KHOPANJavaStandardOutput(NULL, NULL);
-	KHOPANJavaStandardError(NULL, NULL);
-	KHOPANJavaThrow(NULL, NULL, NULL);*/
-	KHOPANERRORMESSAGE_CURL(CURLE_REMOTE_DISK_FULL, L"TestFunction");
 	return 0;
 }
