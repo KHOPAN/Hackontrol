30a9626c1e5910549e91f92603cedd1b8e4f138a
KHOPAN <puthipnakchan@gmail.com>
2024-09-19 22:21:11 +0700

    Check for unresolved external symbols

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,125 +1,30 @@
-//#include <stdio.h>
-//#include <d3d9.h>
+#include <libkhopanlist.h>
 #include <libkhopan.h>
+#include <libkhopancurl.h>
+#include <libkhopanjava.h>
 
 int main(int argc, char** argv) {
-	/*LPWSTR fileRundll32 = KHOPANFileGetRundll32();
-
-	if(!fileRundll32) {
-		KHOPANLASTERRORCONSOLE_WIN32(L"KHOPANFileGetRundll32");
-		return 1;
-	}
-
-	printf("Path: %ws\n", fileRundll32);
-	LocalFree(fileRundll32);*/
-
-	if(!KHOPANExecuteCommand(L"netstat", FALSE)) {
-		KHOPANLASTERRORCONSOLE_WIN32(L"KHOPANExecuteCommand");
-		return 1;
-	}
-
+	KHOPANArrayInitialize(NULL, 0);
+	KHOPANArrayAdd(NULL, NULL);
+	KHOPANArrayRemove(NULL, 0);
+	KHOPANArrayGet(NULL, 0, NULL);
+	KHOPANArrayFree(NULL);
+	KHOPANStreamAdd(NULL, NULL, 0);
+	KHOPANStreamFree(NULL);
+	KHOPANEnablePrivilege(NULL);
+	KHOPANExecuteCommand(NULL, FALSE);
+	KHOPANExecuteDynamicLibrary(NULL, NULL, NULL);
+	KHOPANExecuteProcess(NULL, NULL, FALSE);
+	KHOPANExecuteRundll32Function(NULL, NULL, NULL, FALSE);
+	KHOPANFileGetCmd();
+	KHOPANFileGetRundll32();
+	KHOPANFolderGetWindows();
+	KHOPANFormatMessage(NULL);
+	KHOPANInternalGetErrorMessage(0, NULL, FALSE);
+	KHOPANStringDuplicate(NULL);
+	KHOPANCURLInternalGetErrorMessage(0, NULL);
+	KHOPANJavaStandardOutput(NULL, NULL);
+	KHOPANJavaStandardError(NULL, NULL);
+	KHOPANJavaThrow(NULL, NULL, NULL);
 	return 0;
-	/*IDirect3D9* direct = Direct3DCreate9(D3D_SDK_VERSION);
-
-	if(!direct) {
-		KHWIN32_ERROR_CONSOLE(ERROR_FUNCTION_FAILED, L"Direct3DCreate9");
-		return 1;
-	}
-
-	D3DDISPLAYMODE mode;
-	HRESULT result = direct->lpVtbl->GetAdapterDisplayMode(direct, D3DADAPTER_DEFAULT, &mode);
-	int returnValue = 1;
-
-	if(FAILED(result)) {
-		KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3D9::GetAdapterDisplayMode");
-		goto releaseDirect;
-	}
-
-	printf("Width: %u Height: %u\n", mode.Width, mode.Height);
-	D3DPRESENT_PARAMETERS parameters = {0};
-	parameters.Windowed = TRUE;
-	parameters.BackBufferCount = 1;
-	parameters.BackBufferWidth = mode.Width;
-	parameters.BackBufferHeight = mode.Height;
-	parameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	IDirect3DDevice9* device;
-	result = direct->lpVtbl->CreateDevice(direct, D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, NULL, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &parameters, &device);
-
-	if(FAILED(result)) {
-		KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3D9::CreateDevice");
-		goto releaseDirect;
-	}
-
-	IDirect3DSurface9* surface;
-	result = device->lpVtbl->CreateOffscreenPlainSurface(device, mode.Width, mode.Height, D3DFMT_A8R8G8B8, D3DPOOL_SYSTEMMEM, &surface, NULL);
-
-	if(FAILED(result)) {
-		KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3DDevice9::CreateOffscreenPlainSurface");
-		goto releaseDevice;
-	}
-
-	D3DLOCKED_RECT bounds;
-	result = surface->lpVtbl->LockRect(surface, &bounds, NULL, 0);
-
-	if(FAILED(result)) {
-		KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3DSurface9::LockRect");
-		goto releaseSurface;
-	}
-
-	printf("Pitch: %d\n", bounds.Pitch);
-	result = surface->lpVtbl->UnlockRect(surface);
-
-	if(FAILED(result)) {
-		KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3DSurface9::UnlockRect");
-		goto releaseSurface;
-	}
-
-	PBYTE buffer = LocalAlloc(LMEM_FIXED, bounds.Pitch * mode.Height);
-
-	if(!buffer) {
-		KHWIN32_ERROR_CONSOLE(GetLastError(), L"LocalAlloc");
-		goto releaseSurface;
-	}
-
-	while(TRUE) {
-		ULONGLONG time = GetTickCount64();
-		result = device->lpVtbl->GetFrontBufferData(device, 0, surface);
-
-		if(FAILED(result)) {
-			KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3DDevice9::GetFrontBufferData");
-			goto freeBuffer;
-		}
-
-		result = surface->lpVtbl->LockRect(surface, &bounds, NULL, 0);
-
-		if(FAILED(result)) {
-			KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3DSurface9::LockRect");
-			goto freeBuffer;
-		}
-
-		for(size_t i = 0; i < bounds.Pitch * mode.Height; i++) {
-			buffer[i] = ((PBYTE) bounds.pBits)[i];
-		}
-
-		result = surface->lpVtbl->UnlockRect(surface);
-
-		if(FAILED(result)) {
-			KHWIN32_ERROR_CONSOLE(KHHRESULT_DECODE(result), L"IDirect3DSurface9::UnlockRect");
-			goto freeBuffer;
-		}
-
-		time = GetTickCount64() - time;
-		printf("Elapsed: %llums (%f FPS)\n", time, 1000.0 / time);
-	}
-
-	returnValue = 0;
-freeBuffer:
-	LocalFree(buffer);
-releaseSurface:
-	surface->lpVtbl->Release(surface);
-releaseDevice:
-	device->lpVtbl->Release(device);
-releaseDirect:
-	direct->lpVtbl->Release(direct);
-	return returnValue;*/
 }
