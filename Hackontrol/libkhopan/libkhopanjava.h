dfe01704d9803630128a8679e295ddc4c330f1ed
KHOPAN <puthipnakchan@gmail.com>
2024-09-19 22:06:40 +0700

    Add print functions

--- Hackontrol/libkhopan/libkhopanjava.h
+++ Hackontrol/libkhopan/libkhopanjava.h
@@ -3,9 +3,15 @@
 #include <Windows.h>
 #include <jni.h>
 
+#define KHOPANTHROW_RUNTIME_EXCEPTION(environment, message)          KHOPANJavaThrow(environment,"java/lang/RuntimeException",message)
+#define KHOPANTHROW_ILLEGAL_ARGUMENT_EXCEPTION(environment, message) KHOPANJavaThrow(environment,"java/lang/IllegalArgumentException",message)
+#define KHOPANTHROW_INTERNAL_ERROR(environment, message)             KHOPANJavaThrow(environment,"java/lang/InternalError",message)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+void KHOPANJavaStandardOutput(JNIEnv* environment, const LPCWSTR message);
+void KHOPANJavaStandardError(JNIEnv* environment, const LPCWSTR message);
 void KHOPANJavaThrow(JNIEnv* environment, const LPCSTR class, const LPCWSTR message);
 #ifdef __cplusplus
 }
