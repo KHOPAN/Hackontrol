e8696edb167dba793f117ebd93416482564c1ebe
KHOPAN <puthipnakchan@gmail.com>
2024-09-20 23:46:48 +0700

    Test library-hackontrol

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -1,3 +1,20 @@
+#include <libhackontrolcurl.h>
+#include <libkhopancurl.h>
+
 int main(int argc, char** argv) {
+	DATASTREAM stream = {0};
+	CURLcode code = HackontrolDownload("www.google.com", &stream, FALSE, FALSE);
+
+	if(code != CURLE_OK) {
+		KHOPANERRORMESSAGE_CURL(code, L"HackontrolDownload");
+		return 1;
+	}
+
+	for(size_t i = 0; i < stream.size; i++) {
+		putchar(stream.data[i]);
+	}
+
+	KHOPANStreamFree(&stream);
+	_flushall();
 	return 0;
 }
