5212027f9a1d0f299b3a1a9e70dfbc1c54875ee9
KHOPAN <puthipnakchan@gmail.com>
2024-09-20 23:06:13 +0700

    Remove unused files

--- Hackontrol/library-hackontrol/hackontrolcurl.h
+++ Hackontrol/library-hackontrol/hackontrolcurl.h
@@ -1,13 +1,8 @@
 #pragma once
 
-//#include <khopancurl.h>
-//#include "hackontrol.h"
-//#include <Windows.h>
 #include <libkhopanlist.h>
 #include <libkhopancurl.h>
 
 #define DELAY_FORCE_DOWNLOAD 1000
 
 CURLcode HackontrolDownload(const LPCSTR location, const PDATASTREAM stream, const BOOL initialized, const BOOL force);
-//BOOL HackontrolDownloadData(DataStream* const stream, LPCSTR const url, const BOOL curlInitialized, CURLcode* const outputCode);
-//BOOL HackontrolForceDownload(DataStream* const stream, LPCSTR const url, const BOOL curlInitialized);
--- Hackontrol/library-hackontrol/hackontroljava.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-#include <jni.h>
-
-void HackontrolThrowWin32Error(JNIEnv* const environment, const LPWSTR functionName);
