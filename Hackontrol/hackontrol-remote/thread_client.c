132f5c59d626b2daf25a2b2ba9bc078f8a1799de
KHOPAN <puthipnakchan@gmail.com>
2024-10-02 18:03:17 +0700

    Add list item

--- Hackontrol/hackontrol-remote/thread_client.c
+++ Hackontrol/hackontrol-remote/thread_client.c
@@ -1,9 +1,13 @@
+#include <libkhopanlist.h>
 #include <hrsp_handshake.h>
 #include <hrsp_packet.h>
 #include <hrsp_remote.h>
 #include "remote.h"
 
 #define ERROR_HRSP(function) message=HRSPGetErrorMessage(function,&protocolError);if(message){LOG("[Client %ws]: %ws",client->address,message);LocalFree(message);}
+
+extern LINKEDLIST clientList;
+extern HANDLE clientListMutex;
 
 DWORD WINAPI ThreadClient(_In_ PCLIENT client) {
 	if(!client) {
@@ -37,10 +41,31 @@ DWORD WINAPI ThreadClient(_In_ PCLIENT client) {
 	client->name = KHOPANFormatMessage(L"%S", packet.data);
 	HRSPFreePacket(&packet, NULL);
 	LOG("[Client %ws]: Username: '%ws'\n", client->address, client->name);
+
+	if(WaitForSingleObject(clientListMutex, INFINITE) == WAIT_FAILED) {
+		KHOPANLASTERRORMESSAGE_WIN32(L"WaitForSingleObject");
+		goto freeName;
+	}
+
+	PLINKEDLISTITEM item;
+
+	if(!KHOPANLinkedAdd(&clientList, (PBYTE) client, &item)) {
+		KHOPANLASTERRORMESSAGE_WIN32(L"KHOPANLinkedAdd");
+		ReleaseMutex(clientListMutex);
+		goto freeName;
+	}
+
+	KHOPAN_DEALLOCATE(client);
+	client = (PCLIENT) item->data;
+	ReleaseMutex(clientListMutex);
+	WindowMainRefresh();
 	codeExit = 0;
+freeName:
+	if(client->name) {
+		LocalFree(client->name);
+	}
 functionExit:
 	LOG("[Client %ws]: Exit with code: %d\n", client->address, codeExit);
 	CloseHandle(client->thread);
-	LocalFree(client);
 	return codeExit;
 }
