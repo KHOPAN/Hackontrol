5a8e06e0a7116fe327141429e627e737fe222c89
KHOPAN <puthipnakchan@gmail.com>
2024-10-18 14:54:34 +0700

    Add open stream button back

--- Hackontrol/hackontrol-remote/window_session_tab_stream.c
+++ Hackontrol/hackontrol-remote/window_session_tab_stream.c
@@ -1,11 +1,8 @@
 #include "window_session_tabs.h"
 
-void __stdcall WindowSessionTabStream(const PTABINITIALIZER tab) {
-	tab->name = L"Stream";
-	tab->windowClass.lpszClassName = L"HackontrolRemoteSessionTabStream";
-}
+#define CLASS_NAME L"HackontrolRemoteSessionTabStream"
 
-/*extern HFONT font;
+extern HFONT font;
 
 typedef struct {
 	PCLIENT client;
@@ -14,7 +11,7 @@ typedef struct {
 	HWND button;
 } TABSTREAMDATA, *PTABSTREAMDATA;
 
-HWND __stdcall WindowSessionTabStream(const HWND parent, const PCLIENT client) {
+static HWND __stdcall clientInitialize(const HWND parent, const PCLIENT client) {
 	PTABSTREAMDATA data = KHOPAN_ALLOCATE(sizeof(TABSTREAMDATA));
 
 	if(KHOPAN_ALLOCATE_FAILED(data)) {
@@ -23,7 +20,7 @@ HWND __stdcall WindowSessionTabStream(const HWND parent, const PCLIENT client) {
 	}
 
 	data->client = client;
-	HWND window = CreateWindowExW(0L, REMOTE_CLASS_TAB_STREAM, L"", WS_CHILD, 0, 0, 0, 0, parent, NULL, NULL, data);
+	HWND window = CreateWindowExW(0L, CLASS_NAME, L"", WS_CHILD, 0, 0, 0, 0, parent, NULL, NULL, data);
 
 	if(!window) {
 		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
@@ -45,19 +42,26 @@ HWND __stdcall WindowSessionTabStream(const HWND parent, const PCLIENT client) {
 	return window;
 }
 
-LRESULT CALLBACK WindowSessionTabStreamProcedure(_In_ HWND window, _In_ UINT message, _In_ WPARAM wparam, _In_ LPARAM lparam) {
+static LRESULT CALLBACK windowProcedure(_In_ HWND window, _In_ UINT message, _In_ WPARAM wparam, _In_ LPARAM lparam) {
 	USERDATA(PTABSTREAMDATA, data, window, message, wparam, lparam);
 	RECT bounds;
 
 	switch(message) {
-	case WM_CLOSE:
-		DestroyWindow(window);
-		return 0;
 	case WM_SIZE:
 		GetClientRect(window, &bounds);
 		SetWindowPos(data->button, NULL, (bounds.right - bounds.left - data->buttonWidth) / 2, (bounds.bottom - bounds.top - data->buttonHeight) / 2, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 		return 0;
+	case WM_DESTROY:
+		KHOPAN_DEALLOCATE(data);
+		return 0;
 	}
 
 	return DefWindowProcW(window, message, wparam, lparam);
-}*/
+}
+
+void __stdcall WindowSessionTabStream(const PTABINITIALIZER tab) {
+	tab->name = L"Stream";
+	tab->clientInitialize = clientInitialize;
+	tab->windowClass.lpfnWndProc = windowProcedure;
+	tab->windowClass.lpszClassName = CLASS_NAME;
+}
