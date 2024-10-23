1dc29c3897dffa50a8fc4719f2aadc5e1efecdee
KHOPAN <puthipnakchan@gmail.com>
2024-10-23 20:47:48 +0700

    Add list view to audio tab

--- Hackontrol/hackontrol-remote/window_session_tab_audio.c
+++ Hackontrol/hackontrol-remote/window_session_tab_audio.c
@@ -1,5 +1,18 @@
 #include "window_session_tabs.h"
+#include <CommCtrl.h>
+
+static HWND __stdcall clientInitialize(const PCLIENT client, const PULONGLONG data, const HWND parent) {
+	HWND window = CreateWindowExW(WS_EX_CLIENTEDGE, WC_LISTVIEW, L"", WS_CHILD, 0, 0, 0, 0, parent, NULL, NULL, data);
+
+	if(!window) {
+		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
+		return NULL;
+	}
+
+	return window;
+}
 
 void __stdcall WindowSessionTabAudio(const PTABINITIALIZER tab) {
 	tab->name = L"Audio";
+	tab->clientInitialize = clientInitialize;
 }
