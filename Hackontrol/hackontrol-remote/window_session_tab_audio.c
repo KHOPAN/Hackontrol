aec65d1d9f1094e830c90761df97445106ef9bb2
KHOPAN <puthipnakchan@gmail.com>
2024-10-25 18:45:54 +0700

    Update window_session_tab_audio.c

--- Hackontrol/hackontrol-remote/window_session_tab_audio.c
+++ Hackontrol/hackontrol-remote/window_session_tab_audio.c
@@ -1,42 +1,101 @@
 #include "window_session_tabs.h"
 #include <CommCtrl.h>
 
+#define CLASS_NAME L"HackontrolRemoteSessionTabAudio"
+
 extern HFONT font;
 
-static HWND __stdcall clientInitialize(const PCLIENT client, const PULONGLONG data, const HWND parent) {
-	HWND window = CreateWindowExW(WS_EX_CLIENTEDGE, WC_LISTVIEW, L"", LVS_REPORT | LVS_SINGLESEL | WS_TABSTOP | WS_CHILD | WS_VSCROLL, 0, 0, 0, 0, parent, NULL, NULL, data);
+typedef struct {
+	HWND border;
+	HWND list;
+} TABAUDIODATA, *PTABAUDIODATA;
 
-	if(!window) {
-		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
+static HWND __stdcall clientInitialize(const PCLIENT client, const PULONGLONG customData, const HWND parent) {
+	PTABAUDIODATA data = KHOPAN_ALLOCATE(sizeof(TABAUDIODATA));
+
+	if(KHOPAN_ALLOCATE_FAILED(data)) {
+		KHOPANERRORCONSOLE_WIN32(KHOPAN_ALLOCATE_ERROR, KHOPAN_ALLOCATE_FUNCTION);
 		return NULL;
 	}
 
-	SendMessageW(window, LVM_SETEXTENDEDLISTVIEWSTYLE, 0, LVS_EX_FULLROWSELECT);
+	HWND window = CreateWindowExW(WS_EX_CONTROLPARENT, CLASS_NAME, L"", WS_CHILD, 0, 0, 0, 0, parent, NULL, NULL, data);
+
+	if(!window) {
+		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
+		KHOPAN_DEALLOCATE(data);
+		return NULL;
+	}
+
+	data->border = CreateWindowExW(WS_EX_NOPARENTNOTIFY, L"Button", L"Audio Devices", BS_GROUPBOX | WS_CHILD | WS_VISIBLE, 0, 0, 0, 0, window, NULL, NULL, NULL);
+
+	if(!data->border) {
+		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
+		DestroyWindow(window);
+		KHOPAN_DEALLOCATE(data);
+		return NULL;
+	}
+
+	SendMessageW(data->border, WM_SETFONT, (WPARAM) font, TRUE);
+	data->list = CreateWindowExW(WS_EX_CLIENTEDGE, WC_LISTVIEW, L"", LVS_REPORT | LVS_SINGLESEL | WS_TABSTOP | WS_CHILD | WS_VISIBLE | WS_VSCROLL, 3, 17, 0, 0, window, NULL, NULL, NULL);
+
+	if(!data->list) {
+		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
+		DestroyWindow(window);
+		KHOPAN_DEALLOCATE(data);
+		return NULL;
+	}
+
+	SendMessageW(data->list, WM_SETFONT, (WPARAM) font, TRUE);
+	SendMessageW(data->list, LVM_SETEXTENDEDLISTVIEWSTYLE, 0, LVS_EX_FULLROWSELECT);
 	LVCOLUMNW column = {0};
 	column.mask = LVCF_FMT | LVCF_WIDTH | LVCF_TEXT | LVCF_SUBITEM;
 	column.cx = (int) (((double) GetSystemMetrics(SM_CXSCREEN)) * 0.13250366);
 	column.fmt = LVCFMT_LEFT;
 	column.pszText = L"Name";
 
-	if(SendMessageW(window, LVM_INSERTCOLUMN, 0, (LPARAM) &column) == -1) {
+	if(SendMessageW(data->list, LVM_INSERTCOLUMN, 0, (LPARAM) &column) == -1) {
 		KHOPANLASTERRORMESSAGE_WIN32(L"ListView_InsertColumn");
+		DestroyWindow(window);
+		KHOPAN_DEALLOCATE(data);
 		return NULL;
 	}
 
-	SendMessageW(window, WM_SETFONT, (WPARAM) font, TRUE);
 	LVITEMW listItem = {0};
 	listItem.mask = LVIF_TEXT;
 	listItem.iSubItem = 0;
 	listItem.pszText = L"Third";
-	SendMessageW(window, LVM_INSERTITEM, 0, (LPARAM) &listItem);
+	SendMessageW(data->list, LVM_INSERTITEM, 0, (LPARAM) &listItem);
 	listItem.pszText = L"Second";
-	SendMessageW(window, LVM_INSERTITEM, 0, (LPARAM) &listItem);
+	SendMessageW(data->list, LVM_INSERTITEM, 0, (LPARAM) &listItem);
 	listItem.pszText = L"First";
-	SendMessageW(window, LVM_INSERTITEM, 0, (LPARAM) &listItem);
+	SendMessageW(data->list, LVM_INSERTITEM, 0, (LPARAM) &listItem);
 	return window;
+}
+
+static LRESULT CALLBACK procedure(_In_ HWND window, _In_ UINT message, _In_ WPARAM wparam, _In_ LPARAM lparam) {
+	USERDATA(PTABAUDIODATA, data, window, message, wparam, lparam);
+	RECT bounds;
+
+	switch(message) {
+	case WM_DESTROY:
+		KHOPAN_DEALLOCATE(data);
+		return 0;
+	case WM_SIZE:
+		GetClientRect(window, &bounds);
+		SetWindowPos(data->border, HWND_TOP, 0, 0, bounds.right - bounds.left - 2, bounds.bottom - bounds.top, SWP_NOMOVE);
+		SetWindowPos(data->list, HWND_TOP, 0, 0, bounds.right - bounds.left - 8, bounds.bottom - bounds.top - 21, SWP_NOMOVE);
+		return 0;
+	case WM_CTLCOLORSTATIC:
+		SetDCBrushColor((HDC) wparam, 0xF9F9F9);
+		return (LRESULT) GetStockObject(DC_BRUSH);
+	}
+
+	return DefWindowProcW(window, message, wparam, lparam);
 }
 
 void __stdcall WindowSessionTabAudio(const PTABINITIALIZER tab) {
 	tab->name = L"Audio";
 	tab->clientInitialize = clientInitialize;
+	tab->windowClass.lpfnWndProc = procedure;
+	tab->windowClass.lpszClassName = CLASS_NAME;
 }
