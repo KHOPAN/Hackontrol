a78b16ef8af7421fbb8fc5b7ace57abf119bca71
KHOPAN <puthipnakchan@gmail.com>
2024-11-03 13:07:27 +0700

    Delete batch-connect.bat

--- remote-release/batch-connect.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-start /b ../Hackontrol/x64/Debug/remote-client.exe Adams
-start /b ../Hackontrol/x64/Debug/remote-client.exe Baker
-start /b ../Hackontrol/x64/Debug/remote-client.exe Clark
-start /b ../Hackontrol/x64/Debug/remote-client.exe Davis
-start /b ../Hackontrol/x64/Debug/remote-client.exe Evans
-start /b ../Hackontrol/x64/Debug/remote-client.exe Frank
-start /b ../Hackontrol/x64/Debug/remote-client.exe Ghosh
-start /b ../Hackontrol/x64/Debug/remote-client.exe Hills
