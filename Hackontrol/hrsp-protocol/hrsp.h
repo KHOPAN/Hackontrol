e972c8639425b268b94c222e1b9efef9da5fc57f
KHOPAN <puthipnakchan@gmail.com>
2024-11-06 22:20:43 +0700

    Update hrsp.h

--- Hackontrol/hrsp-protocol/hrsp.h
+++ Hackontrol/hrsp-protocol/hrsp.h
@@ -7,30 +7,34 @@
 #define HRSP_PROTOCOL_PORT          42485
 #define HRSP_PROTOCOL_PORT_STRING   L"42485"
 
+#define HRSP_FACILITY_WIN32 0x00
+#define HRSP_FACILITY_HRSP  0x01
+
 typedef enum {
-	HRSP_ERROR_SUCCESS = 0,
-	HRSP_ERROR_UNKNOWN_ERROR,
-	HRSP_ERROR_INVALID_FUNCTION_PARAMETER,
+	HRSP_ERROR_SUCCESS,
+	HRSP_ERROR_UNDEFINED,
+	HRSP_ERROR_FUNCTION_FAILED,
+	HRSP_ERROR_INVALID_PARAMETER,
 	HRSP_ERROR_INVALID_MAGIC,
 	HRSP_ERROR_UNSUPPORTED_VERSION,
 	HRSP_ERROR_CONNECTION_CLOSED
 } HRSPERRORCODE;
 
 typedef struct {
-	BOOL win32;
-	DWORD code;
+	UINT facility;
+	ULONG code;
 	LPCWSTR function;
 } HRSPERROR, *PHRSPERROR;
 
-typedef struct {
-	BYTE placeholder;
-} HRSPDATA, *PHRSPDATA;
+//typedef struct {
+//	BYTE placeholder;
+//} HRSPDATA, *PHRSPDATA;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-LPWSTR HRSPGetErrorMessage(const LPCWSTR function, const PHRSPERROR error);
 LPCWSTR HRSPGetErrorCode(const HRSPERRORCODE code);
+LPWSTR HRSPGetErrorMessage(const LPCWSTR function, const PHRSPERROR error);
 #ifdef __cplusplus
 }
 #endif
