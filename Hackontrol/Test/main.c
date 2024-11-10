2f0dcc05c8ccb3209953b597ca4e53661fd8a0e5
KHOPAN <puthipnakchan@gmail.com>
2024-11-10 15:15:01 +0700

    Increase the key size

--- Hackontrol/Test/main.c
+++ Hackontrol/Test/main.c
@@ -10,30 +10,20 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	PBYTE buffer[16];
-	buffer[0] = 0;
-	buffer[1] = 0;
-	buffer[2] = 0;
-	buffer[3] = 0;
-	buffer[4] = 0;
-	buffer[5] = 0;
-	buffer[6] = 0;
-	buffer[7] = 0;
-	buffer[8] = 0;
-	buffer[9] = 0;
-	buffer[10] = 0;
-	buffer[11] = 0;
-	buffer[12] = 0;
-	buffer[13] = 0;
-	buffer[14] = 0;
-	buffer[15] = 0;
-	BCRYPT_KEY_HANDLE key;
-	status = BCryptGenerateSymmetricKey(provider, &key, NULL, 0, (PUCHAR) buffer, 16, 0);
+	PBYTE buffer = KHOPAN_ALLOCATE(32);
 	int codeExit = 1;
+
+	if(!buffer) {
+		KHOPANERRORMESSAGE_WIN32(ERROR_FUNCTION_FAILED, L"KHOPAN_ALLOCATE");
+		goto closeProvider;
+	}
+
+	BCRYPT_KEY_HANDLE key;
+	status = BCryptGenerateSymmetricKey(provider, &key, NULL, 0, (PUCHAR) buffer, 32, 0);
 
 	if(!BCRYPT_SUCCESS(status)) {
 		KHOPANERRORMESSAGE_NTSTATUS(status, L"BCryptGenerateSymmetricKey");
-		goto closeProvider;
+		goto freeBuffer;
 	}
 
 	codeExit = 0;
@@ -44,6 +34,8 @@ destroyKey:
 		KHOPANERRORMESSAGE_NTSTATUS(status, L"BCryptDestroyKey");
 		codeExit = 1;
 	}
+freeBuffer:
+	KHOPAN_DEALLOCATE(buffer);
 closeProvider:
 	status = BCryptCloseAlgorithmProvider(provider, 0);
 
