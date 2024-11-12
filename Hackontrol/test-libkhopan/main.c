77ea4f13c15940dea95b375059665a4b9ea01c88
KHOPAN <puthipnakchan@gmail.com>
2024-11-12 22:38:49 +0700

    Test the array

--- Hackontrol/test-libkhopan/main.c
+++ Hackontrol/test-libkhopan/main.c
@@ -30,6 +30,41 @@ freeStream:
 	return codeExit;
 }
 
+int arrayTest() {
+	ARRAYLIST list;
+	KHOPANERROR error;
+	BYTE data[16];
+	memset(data, 5, sizeof(data));
+
+	if(!KHOPANArrayInitialize(&list, sizeof(data), &error)) {
+		KHOPANERRORMESSAGE_KHOPAN(error);
+		return 1;
+	}
+
+	int codeExit = 0;
+
+	for(UINT i = 0; i < 100; i++) {
+		if(!KHOPANArrayAdd(&list, data, &error)) {
+			KHOPANERRORMESSAGE_KHOPAN(error);
+			goto freeStream;
+		}
+	}
+
+	if(!KHOPANArrayRemove(&list, 5, &error)) {
+		KHOPANERRORMESSAGE_KHOPAN(error);
+		goto freeStream;
+	}
+
+	codeExit = 0;
+freeStream:
+	if(!KHOPANArrayFree(&list, &error)) {
+		KHOPANERRORMESSAGE_KHOPAN(error);
+		codeExit = 1;
+	}
+
+	return codeExit;
+}
+
 int main(int argc, char** argv) {
-	return streamTest();
+	return arrayTest();
 }
