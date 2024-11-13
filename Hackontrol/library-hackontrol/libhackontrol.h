598f457684ed1aa684aff1730a6b7e1259c983ec
KHOPAN <puthipnakchan@gmail.com>
2024-11-13 21:57:16 +0700

    Delete library-hackontrol

--- Hackontrol/library-hackontrol/libhackontrol.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-#define FILE_LIBDLL32 L"libdll32.dll"
-
-LPWSTR HackontrolGetHomeDirectory();
-BOOL HackontrolWriteFile(const LPCWSTR file, const PBYTE data, const size_t size);
-BOOL HackontrolCreateDirectory(const LPCWSTR folder);
