9199217d6f5960ef121ac124929f6cb85bfd4e3f
KHOPAN <puthipnakchan@gmail.com>
2024-11-30 19:23:23 +0700

    Add tabs to stream tab

--- Hackontrol/hackontrol-remote/window_session_stream.c
+++ Hackontrol/hackontrol-remote/window_session_stream.c
@@ -1,5 +1,107 @@
 #include "window_session.h"
+#include <CommCtrl.h>
+
+#define CLASS_NAME L"HackontrolRemoteSessionTabAudio"
+
+extern HFONT font;
+
+typedef struct {
+	PCLIENT client;
+	HWND border;
+	HWND list;
+} TABSTREAMDATA, *PTABSTREAMDATA;
+
+static HWND __stdcall clientInitialize(const PCLIENT client, const PULONGLONG customData, const HWND parent) {
+	PTABSTREAMDATA data = KHOPAN_ALLOCATE(sizeof(TABSTREAMDATA));
+
+	if(!data) {
+		KHOPANERRORCONSOLE_COMMON(ERROR_COMMON_ALLOCATION_FAILED, L"KHOPAN_ALLOCATE");
+		return NULL;
+	}
+
+	data->client = client;
+	HWND window = CreateWindowExW(WS_EX_CONTROLPARENT, CLASS_NAME, L"", WS_CHILD, 0, 0, 0, 0, parent, NULL, NULL, data);
+
+	if(!window) {
+		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
+		goto freeData;
+	}
+
+	data->border = CreateWindowExW(WS_EX_NOPARENTNOTIFY, L"Button", L"Stream Sources", BS_GROUPBOX | WS_CHILD | WS_VISIBLE, 0, 0, 0, 0, window, NULL, NULL, NULL);
+
+	if(!data->border) {
+		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
+		goto destroyWindow;
+	}
+
+	SendMessageW(data->border, WM_SETFONT, (WPARAM) font, TRUE);
+	data->list = CreateWindowExW(WS_EX_CLIENTEDGE, WC_LISTVIEW, L"", LVS_REPORT | LVS_SINGLESEL | WS_TABSTOP | WS_CHILD | WS_VISIBLE | WS_VSCROLL, 3, 17, 0, 0, window, NULL, NULL, NULL);
+
+	if(!data->list) {
+		KHOPANLASTERRORCONSOLE_WIN32(L"CreateWindowExW");
+		goto destroyWindow;
+	}
+
+	SendMessageW(data->list, WM_SETFONT, (WPARAM) font, TRUE);
+	SendMessageW(data->list, LVM_SETEXTENDEDLISTVIEWSTYLE, 0, LVS_EX_DOUBLEBUFFER | LVS_EX_FULLROWSELECT);
+	LVCOLUMNW column = {0};
+	column.mask = LVCF_FMT | LVCF_SUBITEM | LVCF_TEXT | LVCF_WIDTH;
+	column.cx = (int) (((double) GetSystemMetrics(SM_CXSCREEN)) * 0.13250366);
+	column.fmt = LVCFMT_LEFT;
+	column.pszText = L"Name";
+
+	if(SendMessageW(data->list, LVM_INSERTCOLUMN, 0, (LPARAM) &column) == -1) {
+		KHOPANLASTERRORMESSAGE_WIN32(L"ListView_InsertColumn");
+		goto destroyWindow;
+	}
+
+	column.pszText = L"Type";
+
+	if(SendMessageW(data->list, LVM_INSERTCOLUMN, 1, (LPARAM) &column) == -1) {
+		KHOPANLASTERRORMESSAGE_WIN32(L"ListView_InsertColumn");
+		goto destroyWindow;
+	}
+
+	LVITEMW item = {0};
+	item.mask = LVIF_TEXT;
+	item.pszText = L"DISPLAY1";
+	SendMessageW(data->list, LVM_INSERTITEM, 0, (LPARAM) &item);
+	item.iSubItem = 1;
+	item.pszText = L"Monitor (Primary)";
+	SendMessageW(data->list, LVM_SETITEM, 0, (LPARAM) &item);
+	*customData = (ULONGLONG) data;
+	return window;
+destroyWindow:
+	DestroyWindow(window);
+freeData:
+	KHOPAN_DEALLOCATE(data);
+	return NULL;
+}
+
+static LRESULT CALLBACK procedure(_In_ HWND window, _In_ UINT message, _In_ WPARAM wparam, _In_ LPARAM lparam) {
+	USERDATA(PTABSTREAMDATA, data, window, message, wparam, lparam);
+	RECT bounds;
+
+	switch(message) {
+	case WM_CTLCOLORSTATIC:
+		SetDCBrushColor((HDC) wparam, 0xF9F9F9);
+		return (LRESULT) GetStockObject(DC_BRUSH);
+	case WM_DESTROY:
+		KHOPAN_DEALLOCATE(data);
+		return 0;
+	case WM_SIZE:
+		GetClientRect(window, &bounds);
+		SetWindowPos(data->border, HWND_TOP, 0, 0, bounds.right - bounds.left - 2, bounds.bottom - bounds.top, SWP_NOMOVE);
+		SetWindowPos(data->list, HWND_TOP, 0, 0, bounds.right - bounds.left - 8, bounds.bottom - bounds.top - 21, SWP_NOMOVE);
+		return 0;
+	}
+
+	return DefWindowProcW(window, message, wparam, lparam);
+}
 
 void __stdcall WindowSessionTabStream(const PTABINITIALIZER tab) {
 	tab->name = L"Stream";
+	tab->clientInitialize = clientInitialize;
+	tab->windowClass.lpfnWndProc = procedure;
+	tab->windowClass.lpszClassName = CLASS_NAME;
 }
