916dc804f02121cc0c5ee1a10964ee601eb1bd11
KHOPAN <puthipnakchan@gmail.com>
2024-12-02 19:17:10 +0700

    Get the camera device count

--- Hackontrol/hrsp-client/stream.c
+++ Hackontrol/hrsp-client/stream.c
@@ -1,5 +1,28 @@
+#include <mfapi.h>
+#include <mfidl.h>
 #include "hrsp_client_internal.h"
 
 void StreamRequestDevice(const SOCKET socket) {
+	IMFAttributes* attributes;
 
+	if(FAILED(MFCreateAttributes(&attributes, 1))) {
+		return;
+	}
+
+	if(FAILED(attributes->lpVtbl->SetGUID(attributes, &MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE, &MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID))) {
+		attributes->lpVtbl->Release(attributes);
+		return;
+	}
+
+	IMFActivate** activates;
+	UINT32 count;
+	HRESULT result = MFEnumDeviceSources(attributes, &activates, &count);
+	attributes->lpVtbl->Release(attributes);
+
+	if(FAILED(result)) {
+		return;
+	}
+
+	printf("Count: %lu\n", count);
+	CoTaskMemFree(activates);
 }
