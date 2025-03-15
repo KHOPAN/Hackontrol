43d66dc00a2da8223dbaa5526aa959ae799a6acc
KHOPAN <puthipnakchan@gmail.com>
2025-03-15 13:42:19 +0700

    Add CURL calls

--- Hackontrol/hackontrol-installer/main.c
+++ Hackontrol/hackontrol-installer/main.c
@@ -1,4 +1,13 @@
+#include <curl/curl.h>
+
 int main(int argc, char** argv) {
+	CURLcode code = curl_global_init(CURL_GLOBAL_ALL);
+
+	if(code != CURLE_OK) {
+		return 1;
+	}
+
+	curl_global_cleanup();
 	return 0;
 }
 
