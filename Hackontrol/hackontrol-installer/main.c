39f148695db17a23777e6a2fa03dc141af2f8818
KHOPAN <puthipnakchan@gmail.com>
2025-04-21 12:00:00 +0700

    Refactor curlError()

--- Hackontrol/hackontrol-installer/main.c
+++ Hackontrol/hackontrol-installer/main.c
@@ -5,13 +5,48 @@ static const LPCWSTR programName = L"Hackontrol Installer";
 
 static HANDLE processHeap;
 
-int main(int argc, char** argv) {
-	processHeap = GetProcessHeap();
+static void curlError(const LPCWSTR function, const CURLcode code, const LPCSTR errorBuffer) {
+	static const LPCWSTR format = L"CURL error ocurred.\n%ws() failed. Error code: 0x%04X Message:\n%S";
+	const char* message = errorBuffer ? errorBuffer : curl_easy_strerror(code);
+	int length = _scwprintf(format, function, code, message);
 
-	if(!processHeap) {
-		MessageBoxW(NULL, L"GetProcessHeap() failed.", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+	if(length < 1) {
+		MessageBoxW(NULL, L"_scwprintf() failed in curlError()", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+		return;
 	}
 
+	LPWSTR buffer = HeapAlloc(processHeap, 0, sizeof(WCHAR) * (length + 1));
+
+	if(!buffer) {
+		MessageBoxW(NULL, L"HeapAlloc() failed in curlError()", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+		return;
+	}
+
+	if((length = swprintf_s(buffer, length + 1, format, function, code, message)) < 1) {
+		HeapFree(processHeap, 0, buffer);
+		MessageBoxW(NULL, L"swprintf_s() failed in curlError()", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+		return;
+	}
+
+	buffer[length] = 0;
+	MessageBoxW(NULL, buffer, programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+	HeapFree(processHeap, 0, buffer);
+}
+
+int main(int argc, char** argv) {
+	if(!(processHeap = GetProcessHeap())) {
+		MessageBoxW(NULL, L"GetProcessHeap() failed.", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+		return 1;
+	}
+
+	CURLcode code = curl_global_init(CURL_GLOBAL_ALL);
+
+	if(code != CURLE_OK) {
+		curlError(L"curl_global_init", code, NULL);
+		return 1;
+	}
+globalCleanup:
+	curl_global_cleanup();
 	return 0;
 }
 
@@ -19,31 +54,6 @@ int main(int argc, char** argv) {
 	PBYTE data;
 	size_t size;
 } DATABUFFER, *PDATABUFFER;
-
-static void curlError(const LPCWSTR function, const CURLcode code, const LPCSTR errorBuffer) {
-	static const LPCWSTR format = L"CURL error ocurred.\n%ws() failed. Error code: 0x%04X Message:\n%S";
-	const char* message = errorBuffer ? errorBuffer : curl_easy_strerror(code);
-	int length = _scwprintf(format, function, code, message);
-
-	if(length < 1) {
-		return;
-	}
-
-	LPWSTR buffer = HeapAlloc(processHeap, 0, sizeof(WCHAR) * (length + 1));
-
-	if(!buffer) {
-		return;
-	}
-
-	if((length = swprintf_s(buffer, length + 1, format, function, code, message)) < 1) {
-		HeapFree(processHeap, 0, buffer);
-		return;
-	}
-
-	buffer[length] = 0;
-	MessageBoxW(NULL, buffer, L"Hackontrol Installer", MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
-	HeapFree(processHeap, 0, buffer);
-}
 
 static size_t curlWriteCallback(const char* const data, size_t size, size_t count, const PDATABUFFER pointer) {
 	size *= count;
@@ -71,13 +81,6 @@ static size_t curlWriteCallback(const char* const data, size_t size, size_t coun
 }
 
 int main(int argc, char** argv) {
-	CURLcode code = curl_global_init(CURL_GLOBAL_ALL);
-
-	if(code != CURLE_OK) {
-		curlError(L"curl_global_init", code, NULL);
-		return 1;
-	}
-
 	CURL* curl = curl_easy_init();
 	int codeExit = 1;
 
@@ -139,7 +142,5 @@ int main(int argc, char** argv) {
 	codeExit = 0;
 easyCleanup:
 	curl_easy_cleanup(curl);
-globalCleanup:
-	curl_global_cleanup();
 	return codeExit;
 }*/
