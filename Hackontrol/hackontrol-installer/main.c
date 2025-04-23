b7aac5aed54fd178a9b9914afd6a2d1dde9cd934
KHOPAN <puthipnakchan@gmail.com>
2025-04-23 12:00:00 +0700

    Finish the setup

--- Hackontrol/hackontrol-installer/main.c
+++ Hackontrol/hackontrol-installer/main.c
@@ -7,8 +7,13 @@ static const LPCWSTR programName = L"Hackontrol Installer";
 
 static HANDLE processHeap;
 
+typedef struct {
+	PBYTE data;
+	size_t size;
+} DATABUFFER, *PDATABUFFER;
+
 static void curlError(const LPCWSTR function, const CURLcode code, const LPCSTR errorBuffer) {
-	static const LPCWSTR format = L"CURL error ocurred.\n%ws() failed. Error code: 0x%04X Message:\n%S";
+	static const LPCWSTR format = L"CURL error ocurred.\n%ws failed. Error code: 0x%04X Message:\n%S";
 	const char* message = errorBuffer ? errorBuffer : curl_easy_strerror(code);
 	int length = _scwprintf(format, function, code, message);
 
@@ -35,64 +40,7 @@ static void curlError(const LPCWSTR function, const CURLcode code, const LPCSTR 
 	HeapFree(processHeap, 0, buffer);
 }
 
-int main(int argc, char** argv) {
-	if(!(processHeap = GetProcessHeap())) {
-		MessageBoxW(NULL, L"GetProcessHeap() failed.", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
-		return 1;
-	}
-
-	CURLcode code = curl_global_init(CURL_GLOBAL_ALL);
-
-	if(code != CURLE_OK) {
-		curlError(L"curl_global_init", code, NULL);
-		return 1;
-	}
-
-	CURL* curl = curl_easy_init();
-	int codeExit = 1;
-
-	if(!curl) {
-		MessageBoxW(NULL, L"CURL error ocurred.\ncurl_easy_init() failed.", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
-		goto globalCleanup;
-	}
-
-	char errorBuffer[CURL_ERROR_SIZE + 1];
-
-	for(codeExit = 0; codeExit <= CURL_ERROR_SIZE; codeExit++) {
-		errorBuffer[codeExit] = 0;
-	}
-
-	codeExit = 1;
-
-	if((code = curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuffer)) != CURLE_OK) {
-		curlError(L"curl_easy_setopt", code, NULL);
-		goto easyCleanup;
-	}
-
-	if((code = curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0)) != CURLE_OK) {
-		curlError(L"curl_easy_setopt", code, NULL);
-		goto easyCleanup;
-	}
-
-	if((code = curl_easy_setopt(curl, CURLOPT_URL, LATEST_JSON)) != CURLE_OK) {
-		curlError(L"curl_easy_setopt", code, NULL);
-		goto easyCleanup;
-	}
-
-	codeExit = 0;
-easyCleanup:
-	curl_easy_cleanup(curl);
-globalCleanup:
-	curl_global_cleanup();
-	return codeExit;
-}
-
-/*typedef struct {
-	PBYTE data;
-	size_t size;
-} DATABUFFER, *PDATABUFFER;
-
-static size_t curlWriteCallback(const char* const data, size_t size, size_t count, const PDATABUFFER pointer) {
+static size_t curlWriteBuffer(const char* const data, size_t size, size_t count, const PDATABUFFER pointer) {
 	size *= count;
 	PBYTE buffer = HeapAlloc(processHeap, 0, pointer->size + size);
 
@@ -118,18 +66,70 @@ static size_t curlWriteCallback(const char* const data, size_t size, size_t coun
 }
 
 int main(int argc, char** argv) {
+	if(!(processHeap = GetProcessHeap())) {
+		MessageBoxW(NULL, L"GetProcessHeap() failed.", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+		return 1;
+	}
+
+	CURLcode code = curl_global_init(CURL_GLOBAL_ALL);
+
+	if(code != CURLE_OK) {
+		curlError(L"curl_global_init()", code, NULL);
+		return 1;
+	}
+
+	CURL* curl = curl_easy_init();
+	int codeExit = 1;
+
+	if(!curl) {
+		MessageBoxW(NULL, L"CURL error ocurred.\ncurl_easy_init() failed.", programName, MB_OK | MB_ICONERROR | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+		goto globalCleanup;
+	}
+
+	char errorBuffer[CURL_ERROR_SIZE + 1];
+
+	for(codeExit = 0; codeExit <= CURL_ERROR_SIZE; codeExit++) {
+		errorBuffer[codeExit] = 0;
+	}
+
+	codeExit = 1;
+
+	if((code = curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuffer)) != CURLE_OK) {
+		curlError(L"curl_easy_setopt(CURLOPT_ERRORBUFFER)", code, NULL);
+		goto easyCleanup;
+	}
+
+	if((code = curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0)) != CURLE_OK) {
+		curlError(L"curl_easy_setopt(CURLOPT_SSL_VERIFYPEER)", code, NULL);
+		goto easyCleanup;
+	}
+
+	if((code = curl_easy_setopt(curl, CURLOPT_URL, LATEST_JSON)) != CURLE_OK) {
+		curlError(L"curl_easy_setopt(CURLOPT_URL)", code, NULL);
+		goto easyCleanup;
+	}
+
 	DATABUFFER buffer = {0};
 
 	if((code = curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer)) != CURLE_OK) {
-		curlError(L"curl_easy_setopt", code, NULL);
+		curlError(L"curl_easy_setopt(CURLOPT_WRITEDATA)", code, NULL);
 		goto easyCleanup;
 	}
 
-	if((code = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriteCallback)) != CURLE_OK) {
-		curlError(L"curl_easy_setopt", code, NULL);
+	if((code = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriteBuffer)) != CURLE_OK) {
+		curlError(L"curl_easy_setopt(CURLOPT_WRITEFUNCTION)", code, NULL);
 		goto easyCleanup;
 	}
 
+	codeExit = 0;
+easyCleanup:
+	curl_easy_cleanup(curl);
+globalCleanup:
+	curl_global_cleanup();
+	return codeExit;
+}
+
+/*int main(int argc, char** argv) {
 	if((code = curl_easy_perform(curl)) != CURLE_OK) {
 		if(buffer.data) {
 			HeapFree(processHeap, 0, buffer.data);
