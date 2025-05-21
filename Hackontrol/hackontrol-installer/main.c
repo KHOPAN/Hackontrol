eedcc423fedb7a5c2572133592d17ffd4dde3661
KHOPAN <puthipnakchan@gmail.com>
2025-05-21 12:00:00 +0700

    Add WNDCLASSEXW initialization

--- Hackontrol/hackontrol-installer/main.c
+++ Hackontrol/hackontrol-installer/main.c
@@ -1,5 +1,27 @@
 #include <Windows.h>
 
+#define CLASS_NAME L"HackontrolInstallerWindow"
+
 int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance, _In_ LPSTR commandLine, _In_ int showWindow) {
+	WNDCLASSEXW windowClass = {0};
+	windowClass.cbSize;
+	windowClass.style;
+	windowClass.lpfnWndProc;
+	windowClass.cbClsExtra;
+	windowClass.cbWndExtra;
+	windowClass.hInstance;
+	windowClass.hIcon;
+	windowClass.hCursor;
+	windowClass.hbrBackground;
+	windowClass.lpszMenuName;
+	windowClass.lpszClassName = CLASS_NAME;
+	windowClass.hIconSm;
+	ATOM atom = RegisterClassExW(&windowClass);
+
+	if(!atom) {
+		return 1;
+	}
+
+	UnregisterClassW(atom, instance);
 	return 0;
 }
